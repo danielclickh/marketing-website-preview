b10c9226234cdb4fabe9f73c19ff33c9bda96d63
rfraposa <richraposa@gmail.com>
2021-11-01 15:43:12 -0600

    Update lrs.js

--- assets/js/lrs.js
+++ assets/js/lrs.js
@@ -162,6 +162,7 @@ function lesson_attempted(lesson_name) {
     }
 
     //Let's see if the page is gated
+    console.log(document.getElementById("gated").value);
     if(document.getElementById("gated").value && isGated()) {
         //We need their email address before showing any instructions
         document.querySelectorAll('details').forEach(item => {
