4c8552ffe8481497836bdf2769f954c5c029909a
rfraposa <richraposa@gmail.com>
2021-10-31 18:55:45 -0600

    Update lrs.js

--- assets/js/lrs.js
+++ assets/js/lrs.js
@@ -19,32 +19,32 @@
         return false;
     }
 
-    function promptForEmail() {
-            // Handle form submission
-            document.querySelector('.capture-email').addEventListener('submit', function(e) {
-                    e.preventDefault();
-                    // Do something with the form data here...
-                    // Then...
-                    var email = document.getElementById("email").value;
-                    localStorage.setItem('email', email);
+    // function promptForEmail() {
+    //         // Handle form submission
+    //         document.querySelector('.capture-email').addEventListener('submit', function(e) {
+    //                 e.preventDefault();
+    //                 // Do something with the form data here...
+    //                 // Then...
+    //                 var email = document.getElementById("email").value;
+    //                 localStorage.setItem('email', email);
                     
-                    // Save preference to localstorage
-                    gatePrevent();
-                    // Hide the gate immediately
-                    gateHide();
-            });
-    }
+    //                 // Save preference to localstorage
+    //                 gatePrevent();
+    //                 // Hide the gate immediately
+    //                 gateHide();
+    //         });
+    // }
     
-    function gateHide() {
-            // Remove the .gated class from .content which hides the entire .gate div
-            //document.querySelector('.content').classList.remove('gated');
-            document.getElementById("gate").style.display = "none";
-    }
-    function gatePrevent() {
-            // Prevent the gate from being shown from this point forward
-            localStorage.setItem('email-stored', true);
-    }
-    */
+    // function gateHide() {
+    //         // Remove the .gated class from .content which hides the entire .gate div
+    //         //document.querySelector('.content').classList.remove('gated');
+    //         document.getElementById("gate").style.display = "none";
+    // }
+    // function gatePrevent() {
+    //         // Prevent the gate from being shown from this point forward
+    //         localStorage.setItem('email-stored', true);
+    // }
+    
 }
 
 
