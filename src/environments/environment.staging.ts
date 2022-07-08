7df213a4221116e335d92406d2007e3f17248ddc
Nir Peled <peledni@gmail.com>
2022-07-08 11:30:48 -0400

    Updated environments

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  strapiBaseUrl: 'https://clickhouse.com/cms',
+  production: false,
+  strapiBaseUrl: 'https://staging.clickhouse.com/cms',
   workatoApiBaseUrl: 'https://apim.workato.com/clickhouse/marketing-api-v0-1/form',
   workatoToken: 'b5825bafb02a8e854ac7252952d2394f73aa39dc39cc5228d63490ced2926b5a'
 };
