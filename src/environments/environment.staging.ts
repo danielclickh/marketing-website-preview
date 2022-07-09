1ee660362e55aa67e01c4dc8ab1963ed340def97
Nir Peled <peledni@gmail.com>
2022-07-09 15:13:21 -0400

    Changed strapiBaseUrl

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  strapiBaseUrl: '/api',
+  strapiBaseUrl: '', // Keeping it empty because CloudFront redirects from /api to the CMS
   workatoApiBaseUrl: 'https://apim.workato.com/clickhouse/marketing-api-v0-1/form',
   workatoToken: 'b5825bafb02a8e854ac7252952d2394f73aa39dc39cc5228d63490ced2926b5a'
 };
