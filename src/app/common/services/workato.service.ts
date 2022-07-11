108ee1345b921826a2822a93e664d18dd6bfe8b4
Nir Peled <peledni@gmail.com>
2022-07-11 01:05:17 -0400

    Sending marketo cookie when submitting forms

--- src/app/common/services/workato.service.ts
+++ src/app/common/services/workato.service.ts
@@ -1,9 +1,10 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable, PLATFORM_ID} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {firstValueFrom} from "rxjs";
 import {UtmService} from "./utm.service";
 import {SegmentService} from "./segment.service";
+import {isPlatformServer} from "@angular/common";
 
 type WorkatoFormType = 'newsletter' | 'websiteContact' | 'eventRegistration' | 'recordedGatedContent';
 type WorkatoRequest =
@@ -23,6 +24,7 @@ interface BaseWorkatoRequest {
   utm_source?: string;
   firstName?: string;
   lastName?: string;
+  mkto_trk?: string;
 }
 
 interface WorkatoNewsletterRequest extends BaseWorkatoRequest {
@@ -53,7 +55,9 @@ interface WorkatoRecordedGatedContentRequest extends BaseWorkatoRequest {
 export class WorkatoService {
   constructor(private readonly http: HttpClient,
               private readonly utmService: UtmService,
-              private readonly segmentService: SegmentService) {
+              private readonly segmentService: SegmentService,
+              @Inject(PLATFORM_ID) private platformId: object) {
+
   }
 
   async submitNewsletterForm(email: string): Promise<void> {
@@ -84,7 +88,12 @@ export class WorkatoService {
 
   private async submitWorkatoForm(formType: WorkatoFormType, request: WorkatoRequest) {
     const utmParams = this.utmService.getUtmParams();
-    request = {...request, ...utmParams};
+    request = {
+      ...request,
+      ...utmParams,
+      mkto_trk: this.getMarketoCookie()
+    };
+
     request.url = window.location.href;
     const workatoResp: WorkatoResponse = await firstValueFrom(this.http.post(
       `${environment.workatoApiBaseUrl}/${formType}`,
@@ -102,5 +111,25 @@ export class WorkatoService {
       const lastName = request.lastName;
       this.segmentService.identify(email, firstName, lastName, workatoResp.cloudId)
     }
+  }
+
+  private getMarketoCookie(): string | undefined {
+    if (isPlatformServer(this.platformId)) {
+      return undefined;
+    }
+
+    try {
+      const cookieMap = document.cookie
+        .split('; ')
+        .reduce((prev, item) => {
+          const splitItem: string[] = item.split('=');
+          prev[splitItem[0]] = splitItem[1];
+          return prev
+        }, {} as Record<string, string>);
+      return cookieMap['_mkto_trk'];
+    } catch (e) {
+      console.error(e);
+    }
+    return undefined;
   }
 }
