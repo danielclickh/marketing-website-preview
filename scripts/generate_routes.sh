f5e69e242cd16c2876d3cc37b4f8a43f0f4d6937
guykohen <guy@clickhouse.com>
2022-09-21 17:09:29 -0400

    Added --env argument to generate_routes script

--- scripts/generate_routes.sh
+++ scripts/generate_routes.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
+
+ENV="../cms/.env"
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --env=*)
+      ENV="${1#*=}"
+      ;;
+    *)
+      printf "***************************\n"
+      printf "* Error: Invalid argument.*\n"
+      printf "***************************\n"
+      exit 1
+  esac
+  shift
+done
+
 ## Export env vars
-export $(grep -v '^#' ../cms/.env | xargs)
+export $(grep -v '^#' $ENV | xargs)
 ## Query Blog posts
 mysql -u $DATABASE_USERNAME -p$DATABASE_PASSWORD -h $DATABASE_HOST -D $DATABASE_NAME -e "select slug from blog_posts where published_at is not null;" -N > routes.txt
 ## Append prefix /blog/ to all blog post slugs
@@ -11,4 +28,4 @@ awk '$0="/company/events/"$0' routes.txt >> new_routes.txt
 sort new_routes.txt > routes.txt
 rm new_routes.txt
 ## Unset env vars
-unset $(grep -v '^#' ../cms/.env | sed -E 's/(.*)=.*/\1/' | xargs)
+unset $(grep -v '^#' $ENV | sed -E 's/(.*)=.*/\1/' | xargs)
