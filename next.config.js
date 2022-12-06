ae7fe35cdcff4e70140f37532bd4d3e00a42aa23
Justin de Guzman <justin@justindeguzman.net>
2022-12-06 02:31:22 -0800

    add ContentSecurityPolicy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,34 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' https://clickhouselearn.github.io;
+  child-src https://clickhouselearn.github.io;
+  style-src 'self' https://clickhouselearn.github.io;
+  font-src 'self' https://clickhouselearn.github.io;
+`
+
+const securityHeaders = [{
+  key: 'Content-Security-Policy',
+  value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+}]
+
 const nextConfig = {
   assetPrefix: 'https://clickhouselearn.github.io/home/',
   images: {
     unoptimized: true
-  }
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
