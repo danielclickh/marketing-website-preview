226ac4622c8600f3b30cd592007f841e8877328f
VINEETH ASOK KUMAR <me@vineethasok.com>
2023-01-08 00:26:50 +0530

    Add en locale changes

--- next.config.js
+++ next.config.js
@@ -6,11 +6,6 @@ const nextConfig ={
   experimental: {
     appDir: true,
   },
-  i18n: {
-    locales: ['default', 'en'],
-    defaultLocale: 'default',
-    localeDetection: false,
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles'), path.join(__dirname, 'app/**/*.module.scss'), path.join(__dirname, 'components/**/*.module.scss')],
   },
@@ -19,6 +14,10 @@ const nextConfig ={
       {
         source: '/events/:slug',
         destination: '/company/events/:slug',
+      },
+      {
+        source: '/en/:path*',
+        destination: '/:path*',
       },
     ]
   },
