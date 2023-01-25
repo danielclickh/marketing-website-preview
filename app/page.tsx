955b31f2cf77bea5b1aa1031ecdd2c2316d223a3
Fábio Neves <fabio.miguel.neves@gmail.com>
2023-01-25 18:23:03 +0000

    adding sharp package. Adding priority to hero image

--- app/page.tsx
+++ app/page.tsx
@@ -132,6 +132,7 @@ export default async function HomePage() {
               src={mainImage}
               alt='HomePage Image'
               className='heroScreenshot'
+              priority
               unoptimized
             />
           </div>
