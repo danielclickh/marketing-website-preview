ab6666912c1f0a6ad23306dcc8d00db774798900
Fábio Neves <fabio.miguel.neves@gmail.com>
2023-01-26 11:22:34 +0000

    asking to optimize hero

--- app/page.tsx
+++ app/page.tsx
@@ -133,7 +133,6 @@ export default async function HomePage() {
               alt='HomePage Image'
               className='heroScreenshot'
               priority
-              unoptimized
             />
           </div>
         </div>
