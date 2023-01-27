b1706539de72c3c46c841694fcdefe1883e16f72
Kevin Zhang <kevin.zhang@clickhouse.com>
2023-01-27 15:48:22 -0500

    generate rss.xml during build

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -9,6 +9,8 @@ import { BlogPost as BlogPostType } from './types'
 import Markdown from '../../components/Markdown'
 import BlogPost from '../../components/BlogPostList/BlogPost'
 
+import generateRssFeed from '../../lib/api/rss'
+
 interface Props {
   title: string
   description: string
@@ -36,6 +38,7 @@ async function getData(): Promise<Props> {
     ]
   }
   const data = await fetchAll('blog-posts', blogsParams)
+  await generateRssFeed(data)
   const categories = new Set<string>()
   for (let index = 0; index < data.length; index++) {
     categories.add(data[index].category)
