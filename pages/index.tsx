ac3773fe3a2002b96a2d73dd7817b27e6168527b
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-02-07 10:50:20 +0000

    Updated event listing

--- pages/index.tsx
+++ pages/index.tsx
@@ -281,50 +281,7 @@ export default function Home() {
             </div>
           </div>
           <div className='events-container'>
-            <div className='event-item'>
-              <div className='event-item--image'>
-                <Image
-                  alt='image'
-                  src={`${imagePrefix}/clickhouse_workshop.png`}
-                  width={342}
-                  height={196}
-                />
-              </div>
-              <div className='event-item--title'>ClickHouse Workshop</div>
-              <div className='event-item--description'>
-                A deep dive into ClickHouse for developers and architects new to
-                ClickHouse. This free, hands-on, live-training event covers
-                modeling data, ingestion, views, analyzing data, managing data,
-                optimizing ClickHouse, and more.
-              </div>
-              <div className='event-item--details'>
-                <div className='event-item--date'>
-                  <Image
-                    alt='image'
-                    src={`${imagePrefix}/icon-calendar.svg`}
-                    height={20}
-                    width={20}
-                    className='event-item--icon'
-                  />
-                  February 1 &amp; 2, 2023
-                </div>
-                <div className='event-item--time'>
-                  <Image
-                    alt='image'
-                    src={`${imagePrefix}/icon-clock.svg`}
-                    height={20}
-                    width={20}
-                    className='event-item--icon'
-                  />
-                  11:00 AM PST / 2:00 PM EST
-                </div>
-              </div>
-              <Link href='https://clickhouse.com/company/events/2023-02-01-clickhouse-workshop'>
-                <div className='event-item--register-cta'>
-                  Register now -&gt;
-                </div>
-              </Link>
-            </div>
+            
             <div className='event-item'>
               <div className='event-item--image'>
                 <Image
@@ -361,6 +318,50 @@ export default function Home() {
                 </div>
               </div>
               <Link href='https://clickhouse.com/company/events/2023-02-08-clickhouse-onboarding-workshop'>
+                <div className='event-item--register-cta'>
+                  Register now -&gt;
+                </div>
+              </Link>
+            </div>
+            <div className='event-item'>
+              <div className='event-item--image'>
+                <Image
+                  alt='image'
+                  src={`${imagePrefix}/clickhouse_workshop.png`}
+                  width={342}
+                  height={196}
+                />
+              </div>
+              <div className='event-item--title'>ClickHouse Workshop</div>
+              <div className='event-item--description'>
+                A deep dive into ClickHouse for developers and architects new to
+                ClickHouse. This free, hands-on, live-training event covers
+                modeling data, ingestion, views, analyzing data, managing data,
+                optimizing ClickHouse, and more.
+              </div>
+              <div className='event-item--details'>
+                <div className='event-item--date'>
+                  <Image
+                    alt='image'
+                    src={`${imagePrefix}/icon-calendar.svg`}
+                    height={20}
+                    width={20}
+                    className='event-item--icon'
+                  />
+                  February 15 &amp; 16, 2023
+                </div>
+                <div className='event-item--time'>
+                  <Image
+                    alt='image'
+                    src={`${imagePrefix}/icon-clock.svg`}
+                    height={20}
+                    width={20}
+                    className='event-item--icon'
+                  />
+                  1:00 PM GMT / 2:00 PM CET
+                </div>
+              </div>
+              <Link href='https://clickhouse.com/company/events/2023-02-15-clickhouse-workshop'>
                 <div className='event-item--register-cta'>
                   Register now -&gt;
                 </div>
