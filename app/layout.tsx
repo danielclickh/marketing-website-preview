7b9c11b8ad9540eb4e1d785f43de5f4c8e4f1b69
VINEETH ASOK KUMAR <me@vineethasok.com>
2023-02-08 23:48:19 +0530

    Remove cookie icon code script

--- app/layout.tsx
+++ app/layout.tsx
@@ -99,6 +99,25 @@ export default function BaseLayout({ children }: Props) {
           type='text/javascript'
           async
         />
+
+        {/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
+        <Script
+          id='remove-cookie-icon'
+          type='text/javascript'
+          async
+          strategy='beforeInteractive'>
+          {`  
+            window.addEventListener("load", function () {
+              setTimeout(() => {
+                const otIcon = document.querySelector('.ot-optout-icon');
+                if (otIcon) {
+                  otIcon.innerHTML = '';
+                }
+              }, 0);
+            });
+
+          `}
+        </Script>
       </body>
     </html>
   )
