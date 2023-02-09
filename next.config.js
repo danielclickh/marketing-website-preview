63fd8fe2eb8bddc2b040f0decabf56940b9447ee
Fábio Neves <fabio.miguel.neves@gmail.com>
2023-02-09 14:31:17 +0000

    removing no cache header

--- next.config.js
+++ next.config.js
@@ -12,12 +12,6 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/:path",
-        headers: [
-          { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' },
-        ],
-      },
-      {
         // matching all API routes
         source: "/api/:path*",
         headers: [
