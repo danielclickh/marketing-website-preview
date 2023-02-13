eafd1ccfcbd83d56d7cf96a52d938b6ac5267e80
VINEETH ASOK KUMAR <me@vineethasok.com>
2023-02-13 12:30:20 +0530

    Style carousel

--- app/customer-stories/Carousel.tsx
+++ app/customer-stories/Carousel.tsx
@@ -1,16 +1,20 @@
 'use client'
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useState } from 'react'
 import Glider from 'react-glider'
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
 import styles from './CustomerStories.module.scss'
 import 'glider-js/glider.min.css'
 
 const Carousel = ({ children }: { children: ReactNode }) => {
+  const [visibility, setVisibility] = useState(false)
   return (
-    <div className={styles.gliderContainer}>
+    <div className={styles.gliderContainer} data-visible={visibility}>
       <Glider
         hasArrows
         slidesToShow={1}
+        onLoad={() => {
+          setVisibility(true)
+        }}
         draggable
         slidesToScroll={1}
         itemWidth={320}
