8c91642512be84f04ed7bc26e57b32ca8d4d6d84
VINEETH ASOK KUMAR <me@vineethasok.com>
2023-02-14 20:46:53 +0530

    Fix Feature Item text color

--- components/feature_item/feature_item.tsx
+++ components/feature_item/feature_item.tsx
@@ -28,7 +28,10 @@ export function FeatureItem(props: FeatureItemProps) {
         <SuiTitle type='h3' className='mb-1' weight='bold'>
           {title}
         </SuiTitle>
-        <SuiText size='sm' weight='medium' color='c4'>
+        <SuiText
+          size='sm'
+          weight='medium'
+          color={forceDarkMode ? 'c4-dark' : 'c4'}>
           <Markdown>{description}</Markdown>
         </SuiText>
       </div>
