3f559f9541cde2f6bca10fb40c9572b503dbe8ac
Vineeth Asok Kumar <vineeth.asokkumar@clickhouse.com>
2023-02-22 19:56:50 +0530

    Fix incorrect revalidation condition (#110)

--- lib/utils/revalidationConfig.ts
+++ lib/utils/revalidationConfig.ts
@@ -1,5 +1,5 @@
 export const REVALIDATE_SECONDS =
-  process.env.NEXT_IS_PROD === 'true' ? 30 : undefined
+  process.env.NEXT_IS_PROD === 'true' ? undefined : 30
 
 export const NOT_FOUND_FALLBACK =
   process.env.NEXT_IS_PROD === 'true' ? false : 'blocking'
