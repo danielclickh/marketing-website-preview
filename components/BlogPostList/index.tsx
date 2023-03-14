134fe4bc666e6740c7674706701de7f2d7145db9
clickhouse-adrianfraguela <119855513+clickhouse-adrianfraguela@users.noreply.github.com>
2023-03-14 16:17:58 +0000

    Added link to RSS feed in category nav in /blog (#141)
    * Added link to RSS feed in category nav in /blog
    
    * Updates per comments from Vineeth

--- components/BlogPostList/index.tsx
+++ components/BlogPostList/index.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, useRef, useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { BlogPostListProps } from './types'
-
+import { SuiLink } from '../sui'
 import { SuiSearchField, SuiTitle } from '../sui/client'
 import CategorySelector from '../CategorySelector'
 
@@ -111,6 +111,37 @@ function BlogPostList({ categories, children }: BlogPostListProps) {
           Blog categories
         </SuiTitle>
         <CategorySelector options={categoryList} />
+        <div className='pt-2'>
+          <hr />
+          <SuiLink
+            key='blog-categories-nav'
+            href='/rss.xml'
+            target='blank'
+            segmentEvent={{
+              label: 'Blog RSS link',
+              category: 'blog-categories-nav'
+            }}
+            weight='normal'>
+            <div className='flex items-center pt-2'>
+              <span className='text-sm px-4 pr-2 py-2 font-semibold cursor-pointer text-c4'>
+                Grab the RSS feed
+              </span>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-6 h-6 stroke-orange-400'>
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M12.75 19.5v-.75a7.5 7.5 0 00-7.5-7.5H4.5m0-6.75h.75c7.87 0 14.25 6.38 14.25 14.25v.75M6 18.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
+                />
+              </svg>
+            </div>
+          </SuiLink>
+        </div>
       </div>
       <div className='w-full md:w-3/4' ref={ref}>
         {children}
