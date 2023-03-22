3fdb9d0a830567e8b49cdc655ebb894c0df35306
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-03-22 17:36:25 +0000

    Polished follow us component and blog listing page

--- components/FollowUs/index.tsx
+++ components/FollowUs/index.tsx
@@ -3,9 +3,12 @@ function FollowUs() {
   return (
     <div className='bg-primary-300 text-neutral py-12 text-center'>
       <h3 className='text-lg font-bold pb-4'>Follow Us</h3>
-      <div className='flex items-center justify-center space-x-6'>
-        <div>
-          <a href='https://twitter.com/clickhousedb' target='_blank'>
+      <div className='grid grid-cols-3 content-center md:grid-cols-5 md:max-w-lg md:mx-auto'>
+        <div className='mx-auto pb-4'>
+          <a
+            href='https://twitter.com/clickhousedb'
+            target='_blank'
+            className='text-center content-center'>
             <div className='bg-neutral-725 w-16 h-16 rounded-md flex items-center justify-center'>
               <svg viewBox='0 0 1024 1024' className='fill-neutral-0 w-8 h-8'>
                 <path d='M928 254.3c-30.6 13.2-63.9 22.7-98.2 26.4a170.1 170.1 0 0075-94 336.64 336.64 0 01-108.2 41.2A170.1 170.1 0 00672 174c-94.5 0-170.5 76.6-170.5 170.6 0 13.2 1.6 26.4 4.2 39.1-141.5-7.4-267.7-75-351.6-178.5a169.32 169.32 0 00-23.2 86.1c0 59.2 30.1 111.4 76 142.1a172 172 0 01-77.1-21.7v2.1c0 82.9 58.6 151.6 136.7 167.4a180.6 180.6 0 01-44.9 5.8c-11.1 0-21.6-1.1-32.2-2.6C211 652 273.9 701.1 348.8 702.7c-58.6 45.9-132 72.9-211.7 72.9-14.3 0-27.5-.5-41.2-2.1C171.5 822 261.2 850 357.8 850 671.4 850 843 590.2 843 364.7c0-7.4 0-14.8-.5-22.2 33.2-24.3 62.3-54.4 85.5-88.2z' />
@@ -13,7 +16,7 @@ function FollowUs() {
             </div>
           </a>
         </div>
-        <div>
+        <div className='mx-auto'>
           <Link href='/slack' target='_blank'>
             <div className='bg-neutral-725 w-16 h-16 rounded-md flex items-center justify-center'>
               <svg viewBox='0 0 1024 1024' className='fill-neutral-0 w-8 h-8'>
@@ -22,7 +25,7 @@ function FollowUs() {
             </div>
           </Link>
         </div>
-        <div>
+        <div className='mx-auto'>
           <a href='https://github.com/ClickHouse/ClickHouse' target='_blank'>
             <div className='bg-neutral-725 w-16 h-16 rounded-md flex items-center justify-center'>
               <svg viewBox='0 0 16 16' className='fill-neutral-0 w-8 h-8'>
@@ -31,7 +34,7 @@ function FollowUs() {
             </div>
           </a>
         </div>
-        <div>
+        <div className='mx-auto'>
           <a href='https://www.meetup.com/pro/clickhouse/' target='_blank'>
             <div className='bg-neutral-725 w-16 h-16 rounded-md flex items-center justify-center'>
               <svg viewBox='0 0 512 512' className='fill-neutral-0 w-8 h-8'>
@@ -40,7 +43,7 @@ function FollowUs() {
             </div>
           </a>
         </div>
-        <div>
+        <div className='mx-auto'>
           <Link href='/rss.xml' target='_blank'>
             <div className='bg-neutral-725 w-16 h-16 rounded-md flex items-center justify-center'>
               <svg viewBox='0 0 700 1000' className='fill-neutral-0 w-8 h-8'>
