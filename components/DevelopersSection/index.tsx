468c7090e842939974d6139bba0d59bc835bc8d0
VINEETH ASOK KUMAR <me@vineethasok.com>
2023-03-24 13:59:21 +0530

    Fix homepage styling

--- components/DevelopersSection/index.tsx
+++ components/DevelopersSection/index.tsx
@@ -3,33 +3,7 @@ import Image from 'next/image'
 import React from 'react'
 import { CUIButton, CUICard } from '../ClickUI'
 import { SuiTitle } from '../sui'
-
-const developerOptions = [
-  {
-    img: '/images/homepage/developer-image.png',
-    title: 'Susan Wright - Uber',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.',
-    btnText: 'Learn More',
-    href: 'https://clickhouse.cloud'
-  },
-  {
-    img: '/images/homepage/developer-image.png',
-    title: 'Susan Wright - Uber',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.',
-    btnText: 'Learn More',
-    href: 'https://clickhouse.cloud'
-  },
-  {
-    img: '/images/homepage/developer-image.png',
-    title: 'Susan Wright - Uber',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi.',
-    btnText: 'Learn More',
-    href: 'https://clickhouse.cloud'
-  }
-]
+import developerOptions from './developerOptions.json'
 
 function DevelopersSection() {
   return (
@@ -71,7 +45,7 @@ function DevelopersSection() {
           <CUIButton
             type='secondary'
             href='/'
-            iconRight={<ArrowRightIcon height='24' />}>
+            iconRight={<ArrowRightIcon height='16' />}>
             Learn More
           </CUIButton>
         </div>
@@ -80,20 +54,24 @@ function DevelopersSection() {
         {developerOptions.map((developerOption) => (
           <CUICard className=''>
             {developerOption.img && (
-              <CUICard.Header className='w-full'>
+              <CUICard.Header className='w-full h-32'>
                 <Image
                   src={developerOption.img}
                   alt={`Image for ${developerOption.title ?? ''}`}
                   width={385}
-                  height={127}
-                  className='w-full h-fit aspect-[3/1]'
+                  height={128}
+                  className='w-full h-full aspect-video object-cover'
                 />
               </CUICard.Header>
             )}
             {developerOption.description && (
               <CUICard.Body className='flex flex-col items-center justify-center gap-2 px-6 py-8'>
-                <div className='text-center'>{developerOption.title}</div>
-                <div className='text-center'>{developerOption.description}</div>
+                <div className='text-center text-xl font-semibold'>
+                  {developerOption.title}
+                </div>
+                <div className='text-center text-neutral-200 text-sm'>
+                  {developerOption.description}
+                </div>
               </CUICard.Body>
             )}
             <CUICard.Footer className='grid place-items-center w-full px-6 pb-6'>
@@ -101,7 +79,7 @@ function DevelopersSection() {
                 type='secondary'
                 linkClass='w-full inline-grid'
                 href={developerOption.href}
-                iconRight={<ArrowRightIcon height='24' />}>
+                iconRight={<ArrowRightIcon height='16' />}>
                 {developerOption.btnText}
               </CUIButton>
             </CUICard.Footer>
