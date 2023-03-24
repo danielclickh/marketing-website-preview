010ce48aedb58d791ab756d25b4701a0d54ed609
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-03-24 14:55:57 +0000

    updated divider spacing

--- pages/company/careers/index.tsx
+++ pages/company/careers/index.tsx
@@ -153,7 +153,7 @@ export default function CareersPage({
           </div>
         </div>
       </div>
-      <div className='w-full border-t border-neutral-725 max-w-screen-md px-7 mx-auto my-24'></div>
+      <div className='w-full border-t border-neutral-725 max-w-screen-md px-7 mx-auto my-16'></div>
       <div className='w-full pb-12 px-8'>
         <div className='flex flex-col bg-shadow-element yellow-shadow max-w-7xl mx-auto'>
           <h2 className='text-3xl font-semibold font-inter mb-6'>
