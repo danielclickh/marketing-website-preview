535c4f46891890f41ede97c1c4f45e37b74353b9
VINEETH ASOK KUMAR <me@vineethasok.com>
2023-03-24 19:32:51 +0530

    Remove unwanted code

--- components/header/MenuItem.tsx
+++ components/header/MenuItem.tsx
@@ -1,6 +1,4 @@
-import { Fragment, ReactNode, useRef, useState } from 'react'
-import { Popover, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/solid'
+import { ReactNode, useRef, useState } from 'react'
 import styles from './Header.module.scss'
 import {
   autoUpdate,
@@ -72,16 +70,6 @@ const MenuItem = ({
         {...getReferenceProps()}>
         {name}
       </div>
-      {/* 
-      <Transition
-        as={Fragment}
-        show={isOpen}
-        enter='transition ease-out duration-200'
-        enterFrom='opacity-0 translate-y-1'
-        enterTo='opacity-100 translate-y-0'
-        leave='transition ease-in duration-150'
-        leaveFrom='opacity-100 translate-y-0'
-        leaveTo='opacity-0 translate-y-1'> */}
       {isOpen && (
         <>
           <FloatingArrow ref={arrowEl} context={context} />
@@ -105,7 +93,6 @@ const MenuItem = ({
           </div>
         </>
       )}
-      {/* </Transition> */}
     </div>
   )
 }
