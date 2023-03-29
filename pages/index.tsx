383dc1dab7e7288b25427032278d38a73e816bd1
Gareth Jones <gareth@clickhouse.com>
2023-03-29 15:37:34 -0400

    Hero interaction

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,7 @@ import HomePageTerminal from '../components/Terminal/HomePageTerminal'
 import DevelopersSection from '../components/DevelopersSection'
 import { CSSProperties } from 'react'
 import HRSeparator from '../components/HRSeparator'
+import Tilt from 'react-parallax-tilt'
 
 const yellowPositionStyle = {
   '--left-side': 'auto',
@@ -133,93 +134,102 @@ export default function HomePage({
   return (
     <Layout footerData={footerData} seo={seo}>
       <div className='homepage overflow-hidden bg-grid'>
-        <div className='flex flex-col pb-20 lg:pb-44 pt-16 md:pt-24 px-8 2xl:px-0 relative gap-24 justify-center '>
-          <div className='flex flex-col w-full mx-auto max-w-2xl'>
-            <div className='mx-auto md:mr-0 md:mt-8 flex-col items-center justify-center'>
-              <SuiTitle type='h1' className='text-center' color='primary'>
-                Query{' '}
-                <span className='tilted tilted-yellow'>
-                  <span className='tilted-content'>billions</span>
-                </span>{' '}
-                of rows in milliseconds
-              </SuiTitle>
-              <div className='mx-auto flex flex-col items-center max-w-md'>
-                <div className='leading-normal text-neutral-200 text-center my-8'>
-                  ClickHouse is the fastest and most resource efficient
-                  open-source database for real-time apps and analytics.
+        <Tilt
+          tiltEnable={false}
+          glareEnable={true}
+          glareMaxOpacity={0.4}
+          glareColor='rgba(251, 255, 70, 0.08)'
+          glarePosition='all'
+          className='h-full'>
+          <div className='flex flex-col pb-20 lg:pb-44 pt-16 md:pt-24 px-8 2xl:px-0 relative gap-24 justify-center '>
+            <div className='flex flex-col w-full mx-auto max-w-2xl'>
+              <div className='mx-auto md:mr-0 md:mt-8 flex-col items-center justify-center'>
+                <SuiTitle type='h1' className='text-center' color='primary'>
+                  Query{' '}
+                  <span className='tilted tilted-yellow'>
+                    <span className='tilted-content'>billions</span>
+                  </span>{' '}
+                  of rows in milliseconds
+                </SuiTitle>
+                <div className='mx-auto flex flex-col items-center max-w-md'>
+                  <div className='leading-normal text-neutral-200 text-center my-8'>
+                    ClickHouse is the fastest and most resource efficient
+                    open-source database for real-time apps and analytics.
+                  </div>
+
+                  <div className='flex flex-col md:flex-row gap-6 w-full justify-center items-center'>
+                    <CUIButton
+                      type='primary'
+                      size='lg'
+                      weight='semibold'
+                      href={hero.ctaButton.href}
+                      segmentEvent={{
+                        label: hero.ctaButton.text,
+                        category: 'website-hero'
+                      }}
+                      linkClass='w-full max-w-[14rem]'
+                      className='w-full'>
+                      Start free trial
+                    </CUIButton>
+                    <CUIButton
+                      type='secondary'
+                      weight='semibold'
+                      size='lg'
+                      href='/docs'
+                      segmentEvent={{
+                        label: hero.ctaButton.text,
+                        category: 'website-hero'
+                      }}
+                      linkClass='w-full max-w-[14rem]'
+                      className='w-full'>
+                      View documentation
+                    </CUIButton>
+                  </div>
+                  <CUILink
+                    href='/clickhouse'
+                    target='_self'
+                    className='mt-5 text-neutral-200 hover:text-neutral-0 flex items-center gap-2'>
+                    Or download open-source ClickHouse{' '}
+                    <ArrowRightIcon height='16' />
+                  </CUILink>
                 </div>
-                <div className='flex flex-col md:flex-row gap-6 w-full justify-center items-center'>
-                  <CUIButton
-                    type='primary'
-                    size='lg'
-                    weight='semibold'
-                    href={hero.ctaButton.href}
-                    segmentEvent={{
-                      label: hero.ctaButton.text,
-                      category: 'website-hero'
-                    }}
-                    linkClass='w-full max-w-[14rem]'
-                    className='w-full'>
-                    Start free trial
-                  </CUIButton>
-                  <CUIButton
-                    type='secondary'
-                    weight='semibold'
-                    size='lg'
-                    href='/docs'
-                    segmentEvent={{
-                      label: hero.ctaButton.text,
-                      category: 'website-hero'
-                    }}
-                    linkClass='w-full max-w-[14rem]'
-                    className='w-full'>
-                    View documentation
-                  </CUIButton>
-                </div>
-                <CUILink
-                  href='/clickhouse'
-                  target='_self'
-                  className='mt-5 text-neutral-200 hover:text-neutral-0 flex items-center gap-2'>
-                  Or download open-source ClickHouse{' '}
-                  <ArrowRightIcon height='16' />
-                </CUILink>
               </div>
             </div>
           </div>
-        </div>
-        <div className='clip-inverted-triangle'>
-          <div className='section-container max-w-3xl'>
-            <HomePageTerminal />
-          </div>
-        </div>
-        <div className='bg-primary-300 py-8'>
-          <div className='max-w-3xl mx-auto'>
-            <div className='text-center mb-8 text-primary-800 w-fit mx-auto text-xl font-semibold leading-normal'>
-              Trusted by the best developers that work with data at{' '}
-              <span className='tilted tilted-black'>
-                <span className='tilted-content leading-8'>scale</span>
-              </span>
+          <div className='clip-inverted-triangle'>
+            <div className='section-container max-w-3xl'>
+              <HomePageTerminal />
             </div>
+          </div>
+          <div className='bg-primary-300 py-8'>
+            <div className='max-w-3xl mx-auto'>
+              <div className='text-center mb-8 text-primary-800 w-fit mx-auto text-xl font-semibold leading-normal'>
+                Trusted by the best developers that work with data at{' '}
+                <span className='tilted tilted-black'>
+                  <span className='tilted-content leading-8'>scale</span>
+                </span>
+              </div>
 
-            <div className='section-container flex flex-wrap sm:grid sm:grid-cols-5 gap-2 md:gap-x-8 self-center items-center justify-center place-items-center'>
-              {customerStoriesLogos.map((logo, index: number) => (
-                <CUILink
-                  key={logo.href}
-                  href={logo.href}
-                  target={logo.target}
-                  className={`customer-stories-${index} flex rounded-lg justify-center ease-in-out duration-200 cursor-pointer gap-2`}>
-                  <Image
-                    src={logo?.imageSrc}
-                    className='w-auto h-10 rounded object-contain'
-                    alt={logo.alt}
-                    width={logo.width}
-                    height={logo.height}
-                  />
-                </CUILink>
-              ))}
+              <div className='section-container flex flex-wrap sm:grid sm:grid-cols-5 gap-2 md:gap-x-8 self-center items-center justify-center place-items-center'>
+                {customerStoriesLogos.map((logo, index: number) => (
+                  <CUILink
+                    key={logo.href}
+                    href={logo.href}
+                    target={logo.target}
+                    className={`customer-stories-${index} flex rounded-lg justify-center ease-in-out duration-200 cursor-pointer gap-2`}>
+                    <Image
+                      src={logo?.imageSrc}
+                      className='w-auto h-10 rounded object-contain'
+                      alt={logo.alt}
+                      width={logo.width}
+                      height={logo.height}
+                    />
+                  </CUILink>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
+        </Tilt>
       </div>
       <div className='flex w-full text-neutral-0'>
         <div className='flex section-container w-full mx-auto flex-col pt-20 text-center items-center'>
