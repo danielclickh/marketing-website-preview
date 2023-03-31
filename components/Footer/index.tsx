044ef3ee4578201e2fd7710855639210771674cb
VINEETH ASOK KUMAR <me@vineethasok.com>
2023-04-01 01:03:26 +0530

    Add footer link fix

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -75,8 +75,6 @@ export default function Footer({
                 {bottomLink.text}
               </CUILink>
             ))}
-            <a className='optanon-allow-all'>Cookie Policy</a>
-            <a className='optanon-toggle-display'>cookies</a>
             <button
               onClick={() => {
                 const element = document.querySelector(
@@ -85,7 +83,7 @@ export default function Footer({
                 console.log(element)
                 element && element.click()
               }}
-              className={`bg-transparent first:pl-0 bottom-link-${bottomLinks.length}`}>
+              className={`bg-transparent first:pl-0 bottom-link-${bottomLinks.length} hover:underline`}>
               Cookie Policy
             </button>
           </div>
