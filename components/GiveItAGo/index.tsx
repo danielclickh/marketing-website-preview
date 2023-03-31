91b64b891cfd57eab027ac5d825ab7aace66775c
Gareth Jones <gareth@clickhouse.com>
2023-03-31 17:56:01 -0400

    Last ClickHouse typo.

--- components/GiveItAGo/index.tsx
+++ components/GiveItAGo/index.tsx
@@ -23,7 +23,7 @@ const deployData: Array<DeployData> = [
     btnType: 'secondary'
   },
   {
-    title: 'Clickhouse',
+    title: 'ClickHouse',
     img: '/drive.svg',
     btnText: 'Download ClickHouse',
     description:
