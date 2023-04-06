925c102004bd189d182bf165949b9ef203c9638b
VINEETH ASOK KUMAR <me@vineethasok.com>
2023-04-06 17:53:27 +0200

    Fix card alignment

--- pages/clickhouse/index.tsx
+++ pages/clickhouse/index.tsx
@@ -202,7 +202,7 @@ export default function ClickHouseServerPage({
                 times.
               </div>
 
-              <div className='grid grid-cols-1 md:grid-cols-3 gap-10 px-4 md:px-0 mx-auto mt-10 md:mt-0'>
+              <div className='grid grid-cols-1 md:grid-cols-3 gap-10 px-4 md:px-0 mx-auto mt-10 md:mt-0 content-baseline'>
                 <CUICard title='Strives for CPU efficiency' className='p-6'>
                   <p className='font-inconsolata text-primary-300'>
                     Vectorization
