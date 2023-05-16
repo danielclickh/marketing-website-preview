01a5bd2b2f33bd9a9fb0d17a735a24b58c8a3adc
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-05-16 08:47:47 +0100

    fixed quotes logos

--- pages/use-cases/index.tsx
+++ pages/use-cases/index.tsx
@@ -213,11 +213,13 @@ function UseCasesPage({
                   <div
                     key={index}
                     className='animate-fade-in mb-3 w-full break-inside-avoid rounded-lg border border-neutral-700/80 bg-neutral-900/50 object-cover p-4 shadow-card hover:bg-neutral-750'>
-                    <StrapiImage
-                      {...quote.quotes.logo}
-                      className='color-swap ml-1 h-auto w-32'
-                    />
-                    <p className=' pt-3 text-sm font-normal text-white'>
+                    <div className='w-1/2'>
+                      <StrapiImage
+                        {...quote.quotes.logo}
+                        className='color-swap h-16'
+                      />
+                    </div>
+                    <p className='pt-1 text-sm font-normal text-white'>
                       "{quote.quotes.quote}"
                     </p>
                   </div>
@@ -226,10 +228,12 @@ function UseCasesPage({
                 <div
                   key={index}
                   className='animate-fade-in mb-3 w-full break-inside-avoid rounded-lg border border-neutral-700/80 bg-neutral-900/50 object-cover shadow-card hover:bg-neutral-750'>
-                  <StrapiImage
-                    {...quote.quotes.logo}
-                    className='color-swap mt-4 ml-1 h-auto w-32 px-4'
-                  />
+                  <div className='w-1/2'>
+                    <StrapiImage
+                      {...quote.quotes.logo}
+                      className='color-swap h-16'
+                    />
+                  </div>
                   <p className='px-4 pb-6 pt-3 text-sm font-normal text-white'>
                     "{quote.quotes.quote}"
                   </p>
