a4233449b436b6f6f859bfd4f15b4ef0fea442f2
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-05-16 08:58:02 +0100

    testing safari 16.0 fix for hovers

--- pages/use-cases/index.tsx
+++ pages/use-cases/index.tsx
@@ -133,7 +133,7 @@ function UseCasesPage({
                                 <>
                                   <div
                                     id={`buttonPrev-${index}`}
-                                    className='group absolute top-0 left-0 z-40 hidden h-full transform bg-gradient-to-l from-neutral-900/90 via-neutral-800/90 to-transparent pl-2 pr-10 hover:cursor-pointer md:flex'>
+                                    className='group absolute top-0 left-0 z-40 hidden h-full transform bg-gradient-to-l from-neutral-900/90 via-neutral-800/90 to-transparent pl-2 pr-4 hover:cursor-pointer md:flex'>
                                     <span className='sr-only'>Previous</span>
                                     <div className='flex items-center'>
                                       <ChevronLeftIcon
@@ -144,7 +144,7 @@ function UseCasesPage({
                                   </div>
                                   <div
                                     id={`buttonNext-${index}`}
-                                    className='group absolute top-0 right-0 z-40 hidden h-full transform bg-gradient-to-l from-neutral-900/90 via-neutral-800/90 to-transparent pr-2 pl-10 hover:cursor-pointer md:flex'>
+                                    className='group absolute top-0 right-0 z-40 hidden h-full transform bg-gradient-to-l from-neutral-900/90 via-neutral-800/90 to-transparent pr-2 pl-4 hover:cursor-pointer md:flex'>
                                     <span className='sr-only'>Next</span>
                                     <div className='flex items-center'>
                                       <ChevronRightIcon
@@ -183,7 +183,7 @@ function UseCasesPage({
                                         <div className='h-20'>
                                           <StrapiImage
                                             {...client.logo}
-                                            className='h-full w-full object-contain px-4 py-2 grayscale hover:cursor-pointer group-hover:grayscale-0'
+                                            className='color-swap h-full w-full object-contain px-4 py-2 hover:cursor-pointer'
                                           />
                                         </div>
                                       </Link>
