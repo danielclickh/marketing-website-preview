9e4baaa9fb998de96d9a1cc68f153401d54211f3
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-05-23 15:03:05 +0100

    Removed speed lines

--- pages/cloud/index.tsx
+++ pages/cloud/index.tsx
@@ -64,7 +64,7 @@ export default function CloudPage({
   return (
     <>
       <Layout footerData={footerData} seo={seo} headerData={headerData}>
-        <div className='bg-contain bg-center bg-no-repeat pt-10 lg:bg-speed-lines'>
+        <div className='bg-contain bg-center bg-no-repeat pt-10'>
           <div className='relative overflow-x-hidden'>
             <div className='container mx-auto flex max-w-7xl flex-col bg-opacity-10 px-4 pb-16 md:bg-no-repeat md:px-8 md:pb-24 lg:min-h-[630px] 2xl:px-0'>
               <div className='flex'>
