93c42447f98008887329e856643be69068f8bd77
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-05-25 14:24:46 +0100

    optimised badge

--- components/PricingOptions/index.tsx
+++ components/PricingOptions/index.tsx
@@ -149,12 +149,12 @@ function PricingOptions({
                 className='relative mx-auto w-full max-w-sm rounded-lg border border-t-4 border-neutral-700/80 border-t-primary bg-neutral-900/50 shadow-card-xl'
                 key={`plan-${plan.name}`}>
                 {index === 2 && (
-                  <div className='absolute -top-5 -right-6'>
+                  <div className='absolute left-1/2 -translate-y-1/2 -translate-x-1/2 transform lg:-right-20 lg:left-auto'>
                     <span className='relative inline-flex'>
                       <Link
                         href='/company/contact?loc=ent-coming-soon-btn'
                         type='button'
-                        className='inline-flex items-center rounded-md bg-primary-300 px-4 py-2 text-sm font-semibold leading-6 text-neutral-800 shadow ring-1 ring-slate-900/10 transition duration-150 ease-in-out'>
+                        className='inline-flex items-center rounded-md bg-primary-300 px-4 py-1 text-sm font-semibold leading-6 text-neutral-800 shadow ring-1 ring-slate-900/10 transition duration-150 ease-in-out '>
                         Coming soon
                       </Link>
                       <span className='absolute -top-1 -right-1 flex h-3 w-3'>
