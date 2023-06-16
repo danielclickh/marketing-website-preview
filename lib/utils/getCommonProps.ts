ca3146971b2513a72d3e7c565abef7a296e0d94c
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-06-16 14:23:22 +0100

    update fallback star count

--- lib/utils/getCommonProps.ts
+++ lib/utils/getCommonProps.ts
@@ -19,7 +19,7 @@ export async function getCommonProps(): Promise<Props> {
     'https://api.github.com/repos/ClickHouse/ClickHouse'
   )
   const data = await githubData.json()
-  const stars = data?.stargazers_count ?? 28649
+  const stars = data?.stargazers_count ?? 29285
   return {
     footerData,
     platforms: getStartedData.platforms,
