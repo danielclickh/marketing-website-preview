1d6c38b5687b317f3c8b68064bd51430fae88df5
Levi Cole <levi@skape.co>
2023-08-16 13:03:14 +0100

    Replaced snackbar success with on page success message

--- components/ContactForm/index.tsx
+++ components/ContactForm/index.tsx
@@ -2,6 +2,7 @@ import React, { FocusEvent, FormEvent, useRef, useState } from 'react'
 import { submitWorkatoForm } from '../../lib/api/workato'
 import { validateEmail } from '../../lib/form'
 import { ContactFormProps } from '../../types/contact'
+import ReactMarkdown from 'react-markdown'
 
 import {
   SuiButton,
@@ -26,6 +27,7 @@ function ContactForm({
   const [company, setCompany] = useState<string>()
   const submitRef = useRef(false)
   const [useCase, setUseCase] = useState<string>('')
+  const [submissionSuccessful, setSubmissionSuccessful] = useState(false);
 
   const onChange = (
     e:
@@ -103,13 +105,15 @@ function ContactForm({
         })
         await window.analytics.identify(userId, requestData)
       } catch (e) {}
-      openSnackBar(thankYouMessage, 'success')
+      //openSnackBar(thankYouMessage, 'success')
+      setSubmissionSuccessful(true)
       setFirstName(undefined)
       setLastName(undefined)
       setEmail(undefined)
       setCompany(undefined)
       setUseCase('')
     } catch (e: any) {
+      setSubmissionSuccessful(false)
       openSnackBar(e.message, 'error')
     }
 
@@ -117,97 +121,100 @@ function ContactForm({
   }
   return (
     <>
-      <div className='flex space-x-8'>
-        <SuiTextField
-          htmlFor='firstName'
-          name='firstName'
-          label={firstNameLabel}
-          value={firstName ?? ''}
-          onChange={onChange}
-          onBlur={onChange}
-          className='w-full'
-          error={
-            typeof firstName === 'string' && firstName.length === 0
-              ? 'Invalid First Name'
-              : ''
-          }
-          required
-        />
-        <SuiTextField
-          htmlFor='lastName'
-          name='lastName'
-          value={lastName ?? ''}
-          onChange={onChange}
-          onBlur={onChange}
-          label={lastNameLabel}
-          error={
-            typeof lastName === 'string' && lastName.length === 0
-              ? 'Invalid Last Name'
-              : ''
-          }
-          className='w-full'
-          required
-        />
-      </div>
-      <div className='flex w-full'>
-        <SuiTextField
-          htmlFor='email'
-          name='email'
-          type='email'
-          value={email ?? ''}
-          onChange={onChange}
-          onBlur={onChange}
-          label={emailLabel}
-          error={
-            typeof email === 'undefined'
-              ? undefined
-              : email.length === 0
-              ? 'E-mail address cannot be empty'
-              : validateEmail(email)
-              ? ''
-              : 'Invalid E-mail address'
-          }
-          className='w-full'
-          required
-        />
-      </div>
-      <div className='flex'>
-        <SuiTextField
-          htmlFor='company'
-          name='company'
-          value={company ?? ''}
-          onChange={onChange}
-          onBlur={onChange}
-          label={companyLabel}
-          error={
-            typeof company === 'string' && company.length === 0
-              ? 'Company name cannot be empty'
-              : ''
-          }
-          className='w-full'
-          required
-        />
-      </div>
-      <div className='flex'>
-        <SuiTextFieldArea
-          htmlFor='useCase'
-          name='useCase'
-          value={useCase}
-          onChange={onChange}
-          onBlur={onChange}
-          label={messageLabel}
-          className='w-full'
-        />
-      </div>
+      <div className={submissionSuccessful ? 'hidden' : ''}>
+        <div className='flex space-x-8'>
+          <SuiTextField
+            htmlFor='firstName'
+            name='firstName'
+            label={firstNameLabel}
+            value={firstName ?? ''}
+            onChange={onChange}
+            onBlur={onChange}
+            className='w-full'
+            error={
+              typeof firstName === 'string' && firstName.length === 0
+                ? 'Invalid First Name'
+                : ''
+            }
+            required
+          />
+          <SuiTextField
+            htmlFor='lastName'
+            name='lastName'
+            value={lastName ?? ''}
+            onChange={onChange}
+            onBlur={onChange}
+            label={lastNameLabel}
+            error={
+              typeof lastName === 'string' && lastName.length === 0
+                ? 'Invalid Last Name'
+                : ''
+            }
+            className='w-full'
+            required
+          />
+        </div>
+        <div className='flex w-full'>
+          <SuiTextField
+            htmlFor='email'
+            name='email'
+            type='email'
+            value={email ?? ''}
+            onChange={onChange}
+            onBlur={onChange}
+            label={emailLabel}
+            error={
+              typeof email === 'undefined'
+                ? undefined
+                : email.length === 0
+                ? 'E-mail address cannot be empty'
+                : validateEmail(email)
+                ? ''
+                : 'Invalid E-mail address'
+            }
+            className='w-full'
+            required
+          />
+        </div>
+        <div className='flex'>
+          <SuiTextField
+            htmlFor='company'
+            name='company'
+            value={company ?? ''}
+            onChange={onChange}
+            onBlur={onChange}
+            label={companyLabel}
+            error={
+              typeof company === 'string' && company.length === 0
+                ? 'Company name cannot be empty'
+                : ''
+            }
+            className='w-full'
+            required
+          />
+        </div>
+        <div className='flex'>
+          <SuiTextFieldArea
+            htmlFor='useCase'
+            name='useCase'
+            value={useCase}
+            onChange={onChange}
+            onBlur={onChange}
+            label={messageLabel}
+            className='w-full'
+          />
+        </div>
 
-      <div className='mx-auto flex w-full'>
-        <SuiButton
-          type='primary'
-          onClick={onSubmit}
-          className='w-full rounded-md hover:translate-y-0 hover:bg-primary-400 hover:no-underline'>
-          {submitButtonLabel}
-        </SuiButton>
+        <div className='mx-auto flex w-full'>
+          <SuiButton
+            type='primary'
+            onClick={onSubmit}
+            className='w-full rounded-md hover:translate-y-0 hover:bg-primary-400 hover:no-underline'>
+            {submitButtonLabel}
+          </SuiButton>
+        </div>
       </div>
+      {submissionSuccessful && <ReactMarkdown className='text-center' children={thankYouMessage} />}
     </>
   )
 }
