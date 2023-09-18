dca60a25b8108c43d4ca5fc87eabe1902b8a73c8
Levi Cole <levi@skape.co>
2023-09-18 13:07:24 +0100

    Added autoplay to video embeds

--- lib/videos/index.ts
+++ lib/videos/index.ts
@@ -9,7 +9,7 @@ export function getVideos(): Video[] {
       subTitle: "Brooke McKim\nCo-founder and CTO, Vantage",
       description: "Vantage streamlines cloud cost management for businesses. Brooke shares how transitioning to ClickHouse Cloud not only optimized Vantage's operations but also cut their Redshift bill in half.",
       thumbnail: "https://img.youtube.com/vi/8FUfyvoqDTg/maxresdefault.jpg",
-      embed: "<iframe src=\"https://www.youtube-nocookie.com/embed/8FUfyvoqDTg?rel=0\" frameBorder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowFullScreen></iframe>",
+      embed: "<iframe src=\"https://www.youtube-nocookie.com/embed/8FUfyvoqDTg?rel=0&autoplay=1\" frameBorder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowFullScreen></iframe>",
       categories: [
         "Management"
       ]
@@ -20,7 +20,7 @@ export function getVideos(): Video[] {
       subTitle: "Harlow Ward\nCo-founder and CTO, Clearbit",
       description: "Clearbit needed a real-time database to handle their massive clickstream data volume and power their UI. Harlow shares why ClickHouse Cloud ticked all the boxes.",
       thumbnail: "https://img.youtube.com/vi/3mS-RSKfGWQ/maxresdefault.jpg",
-      embed: "<iframe src=\"https://www.youtube-nocookie.com/embed/3mS-RSKfGWQ?rel=0\" frameBorder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowFullScreen></iframe>",
+      embed: "<iframe src=\"https://www.youtube-nocookie.com/embed/3mS-RSKfGWQ?rel=0&autoplay=1\" frameBorder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowFullScreen></iframe>",
       categories: [
         "Database"
       ]
@@ -31,7 +31,7 @@ export function getVideos(): Video[] {
       subTitle: "Jason Wang\nSoftware Engineer, Statsig",
       description: "Statsig is an experimentation and A/B testing company, handling millions of events every second and billions daily. Jason shares why they landed on ClickHouse Cloud for their real-time analytics.",
       thumbnail: "https://img.youtube.com/vi/6V_66oiln00/maxresdefault.jpg",
-      embed: "<iframe src=\"https://www.youtube-nocookie.com/embed/6V_66oiln00?rel=0\" frameBorder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowFullScreen></iframe>",
+      embed: "<iframe src=\"https://www.youtube-nocookie.com/embed/6V_66oiln00?rel=0&autoplay=1\" frameBorder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowFullScreen></iframe>",
       categories: [
         "Testing"
       ]
