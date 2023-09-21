6eac85dcdcd1a305d434c6ff863280b0cc0a3c6c
Leticia Webb <110556185+leticiawebb@users.noreply.github.com>
2023-09-21 12:52:20 -0700

    Implement Securiti.ai banner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -42,23 +42,25 @@ function MyApp({ Component, pageProps }: AppProps) {
           </div>
         </SnackbarContextProvider>
       </main>
-      <SegmentScript />
-      {/* CookiePro Cookies Consent Notice start for clickhouse.com */}
-      <Script
-        id='otSdkStub-script'
-        src='https://cookie-cdn.cookiepro.com/scripttemplates/otSDKStub.js'
-        charSet='UTF-8'
-        type='text/javascript'
-        data-domain-script={`dedccc4b-7ab2-47de-935c-073b23b1d9b7${
-          process.env.NEXT_IS_PROD === 'true' ? '' : '-test'
-        }`}
-        strategy='lazyOnload'
+      {/* Securiti.ai Cookie Blocker - required for GDPR compliance */}
+      <script 
+        type="text/javascript" 
+        src="https://cdn-prod.securiti.ai/consent/auto_blocking/8555e54b-cd0b-45d7-9c1c-e9e088bf774a/e058d040-977c-4594-aa2c-84b844ce5cf0.js"
       />
-      <Script
-        id='cookie-consent'
-        type='text/javascript'>{`function OptanonWrapper() {}`}</Script>
-
-      {/* CookiePro Cookies Consent Notice start for clickhouse.com */}
+      {/* Securiti.ai Cookie Blocker end */}
+      {/* Securiti.ai CSS */}
+      <link 
+        rel='stylesheet' 
+        type='text/css' 
+        href='https://cdn-prod.securiti.ai/consent/cookie-consent.css' 
+      />
+      <link 
+        rel='stylesheet' 
+        type='text/css' 
+        href='https://cdn-prod.securiti.ai/consent/styles/8555e54b-cd0b-45d7-9c1c-e9e088bf774a/e058d040-977c-4594-aa2c-84b844ce5cf0.css' 
+      />
+      {/* Securiti.ai CSS */}
+      <SegmentScript />
       <Script
         id='stripmkttok-script'
         src='https://discover.clickhouse.com/js/stripmkttok.js'
@@ -76,6 +78,20 @@ function MyApp({ Component, pageProps }: AppProps) {
         })(window,document,'script','dataLayer', '${gtmId}');
       `}
       </Script>
+      {/* Securiti.ai Cookie Banner */}
+      {/*-- After adding this script, call setConsentBannerParams({uuid: <unique id> }) to set unique ID for a customer. --*/}
+      <script
+      	defer
+      	data-strict-csp
+      	src='https://cdn-prod.securiti.ai/consent/cookie-consent-sdk-strict-csp.js'
+      	data-tenant-uuid='8555e54b-cd0b-45d7-9c1c-e9e088bf774a'
+      	data-domain-uuid='e058d040-977c-4594-aa2c-84b844ce5cf0'
+      	data-backend-url='https://app.securiti.ai'
+        id='securiti-ai-script'
+        type='text/javascript'
+        strategy='afterInteractive'
+      />
+      {/* Securiti.ai Cookie Banner */}
     </>
   )
 }
