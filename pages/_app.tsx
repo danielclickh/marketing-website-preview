dfab6ea0d8accabc32c07b49263717b767290d10
Leticia Webb <110556185+leticiawebb@users.noreply.github.com>
2023-09-25 12:12:43 -0700

    Test removing external CSS and blocker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,14 +35,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <base href='/' />
         <meta content='width=device-width, initial-scale=1' name='viewport' />
         <link href='favicon.ico' rel='icon' type='image/x-icon' />
-        <link 
-            rel='stylesheet' type='text/css' 
-          href='https://cdn-prod.securiti.ai/consent/cookie-consent.css' 
-        />
-        <link 
-          rel='stylesheet' 
-          type='text/css' href='https://cdn-prod.securiti.ai/consent/styles/8555e54b-cd0b-45d7-9c1c-e9e088bf774a/e058d040-977c-4594-aa2c-84b844ce5cf0.css' 
-        />
       </Head>
       <main className={`${inter.variable} font-inter ${inconsolata.variable}`}>
         <SnackbarContextProvider>
