e6c9ad0ab1d5c39081b1f42a55412de76875ff9f
Leticia Webb <110556185+leticiawebb@users.noreply.github.com>
2023-09-25 13:13:27 -0700

    Updating config to prod settings for Securiti.ai

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,6 +32,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+	{/* Securiti.ai cookie blocker */}
+	<script 
+	  type="text/javascript" 
+	  src="https://cdn-prod.securiti.ai/consent/auto_blocking/8555e54b-cd0b-45d7-9c1c-e9e088bf774a/e058d040-977c-4594-aa2c-84b844ce5cf0.js"
+        />
+	{/* Securiti.ai cookie blocker */}
         <base href='/' />
         <meta content='width=device-width, initial-scale=1' name='viewport' />
         <link href='favicon.ico' rel='icon' type='image/x-icon' />
@@ -63,13 +69,13 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Script>
       {/* Securiti.ai Cookie Banner */}
       <script
-	      defer
-	      data-strict-csp
-	      src="https://cdn-prod.securiti.ai/consent/cookie-consent-sdk-strict-csp.js"
-	      data-tenant-uuid="8555e54b-cd0b-45d7-9c1c-e9e088bf774a"
-	      data-domain-uuid="6e04a5fb-5986-4252-81d6-b9ea27685442"
-	      data-backend-url="https://app.securiti.ai">
-      </script>
+        defer
+	data-strict-csp
+	src="https://cdn-prod.securiti.ai/consent/cookie-consent-sdk-strict-csp.js"
+		data-tenant-uuid="8555e54b-cd0b-45d7-9c1c-e9e088bf774a"
+		data-domain-uuid="e058d040-977c-4594-aa2c-84b844ce5cf0"
+		data-backend-url="https://app.securiti.ai"
+      />
       {/* Securiti.ai Cookie Banner */}
     </>
   )
