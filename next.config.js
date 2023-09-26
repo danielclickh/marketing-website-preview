4521c7392c744796794f28f438f5f19d4baa7f4e
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-09-26 09:09:24 +0100

    update slack link

--- next.config.js
+++ next.config.js
@@ -17,9 +17,9 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'img.youtube.com',
         port: '',
-        pathname: '/**',
-      },
-    ],
+        pathname: '/**'
+      }
+    ]
   },
   async headers() {
     return [
@@ -87,7 +87,7 @@ const nextConfig = {
       {
         source: '/slack',
         destination:
-          'https://join.slack.com/t/clickhousedb/shared_invite/zt-21dqsxg8y-85z8Y1OZyHHluhN~d7_~ew',
+          'https://join.slack.com/t/clickhousedb/shared_invite/zt-23y85sfck-crUbdd4RupnjOBxfFIQv5w',
         permanent: true
       },
       {
