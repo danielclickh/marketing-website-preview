9b9bd8155368bd2312c17bd3e4e81152689dfd43
Levi Cole <levi@skape.co>
2023-09-27 15:16:43 +0100

    Added 4 hidden seo pages

---
+++ components/SeoPage/index.tsx
@@ -0,0 +1,100 @@
+import Link from "next/link";
+import React, { HTMLAttributes } from "react";
+import { NewsLetterData } from "../../types/homepage";
+import CopyUrlButton from "../CopyUrlButton";
+import FollowUs from "../FollowUs";
+import HRSeparator from "../HRSeparator";
+import Markdown from "../Markdown";
+import NewsLetterForm from "../NewsLetter/NewsLetterForm";
+import SocialButton from "../SocialButton";
+import { SuiButton, SuiPanel, SuiText } from "../sui";
+
+export interface SeoPageProps extends HTMLAttributes<any> {
+  title: string,
+  children: string,
+  newsLetterData: NewsLetterData,
+  moreLikeThis?: Array<{
+    link: string,
+    title: string
+  }>,
+}
+
+
+export function SeoPage({
+  title,
+  children,
+  moreLikeThis = [],
+  newsLetterData
+}: SeoPageProps) {
+  return (
+    <main>
+      <div className='container mx-auto max-w-6xl px-6 2xl:px-0 my-20'>
+      <div className='lg:flex flex-row items-start gap-6'>
+        <div className='flex-grow flex-shrink'>
+          <h1 className='mb-20 font-basier text-4xl font-bold text-neutral-100'>
+            <span className='leading-snug'>{title}</span>
+          </h1>
+          <Markdown className='rich-text-content leading-6' allowHeaderLink>
+              {children}
+          </Markdown>
+          <HRSeparator className='my-8' />
+          <div className='mb-10 flex flex-col items-center justify-between gap-4 md:flex-row'>
+            <div className='flex'>
+              <SuiText size='sm' weight='medium' color='primary'>
+                Share this resource
+              </SuiText>
+            </div>
+            <div className='flex flex-wrap justify-center gap-4 text-neutral-0'>
+              <CopyUrlButton />
+              {['y_combinator', 'twitter', 'facebook', 'linkedin'].map(
+                (social) => (
+                  <SocialButton key={social} type={social} title={title} />
+                )
+              )}
+            </div>
+          </div>
+        </div>
+        <aside className='w-full lg:max-w-sm flex-shrink-0 flex-grow-0 align-top'>
+          {moreLikeThis?.length > 0 && (
+            <div className='mb-8'>
+              <h3 className='text-lg font-bold mb-6'>More like this</h3>
+              {moreLikeThis.map(item => (
+                <Link href={item.link} className='block w-full mb-4'>
+                  <SuiButton
+                    type='empty'
+                    color='primary'
+                    className='font-base border border-neutral-700 hover:translate-y-0 hover:border-primary-400/40 block w-full hover:no-underline'>
+                    <div className='flex flex-row gap-6 items-center w-full'>
+                      <div className='text-left flex-grow flex-shrink basis-0'>
+                        {item.title}
+                      </div>
+                      <div className='flex-grow-0 flex-shrink-0'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                          <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                        </svg>
+                      </div>
+                    </div>
+                  </SuiButton>
+                </Link>
+              ))}
+              <HRSeparator className='mt-8' />
+            </div>
+          )}
+          <SuiPanel color='bg-c4/10' padding='lg' isRounded className='mt-6'>
+            <h3 className='text-lg font-bold mb-2'>{newsLetterData.title}</h3>
+            <SuiText size='sm' weight='medium' color='secondary' className='mb-6'>
+              {newsLetterData.description}
+            </SuiText>
+            <NewsLetterForm
+              emailLabel={newsLetterData.emailLabel}
+              submitButtonLabel={newsLetterData.submitButtonLabel} />
+          </SuiPanel>
+        </aside>
+      </div>
+    </div>
+      <div className='mt-20'>
+        <FollowUs />
+      </div>
+    </main>
+  )
+}
---
+++ pages/high-performance-databases.tsx
@@ -0,0 +1,114 @@
+import { InferGetStaticPropsType } from 'next'
+import React from 'react'
+import Layout from '../components/Layout'
+import { getNewsLetterData } from '../components/NewsLetter/getNewsLetterData'
+import { SeoPage } from '../components/SeoPage'
+import { getCommonProps } from '../lib/utils/getCommonProps'
+import { REVALIDATE_SECONDS } from '../lib/utils/revalidationConfig'
+
+export async function getStaticProps() {
+  const newsLetterData = await getNewsLetterData()
+  return {
+    props: {
+      seo: {
+        title: 'High-Performance Database Defined | ClickHouse',
+        description: 'Discover the definition of a high-performance database and learn why ClickHouse sets the standard in speed, efficiency, and scalability for data at scale.',
+        path: '/high-performance-databases'
+      },
+      newsLetterData,
+      ...(await getCommonProps()),
+    },
+    revalidate: REVALIDATE_SECONDS
+  }
+}
+
+export default function Page({
+  seo,
+  headerData,
+  footerData,
+  newsLetterData
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <Layout footerData={footerData} seo={seo} headerData={headerData}>
+      <SeoPage
+        title='High-Performance Database Defined'
+        moreLikeThis={[
+          {
+            link: '/data-warehousing-explained',
+            title: 'Data Warehousing Explained'
+          },
+          {
+            link: '/distributed-databases',
+            title: 'Distributed Database Systems Explained'
+          },
+          // {
+          //   link: '/high-performance-databases',
+          //   title: 'High-Performance Database Defined'
+          // },
+          {
+            link: '/what-is-an-open-source-database',
+            title: 'What Is an Open-Source Database?'
+          }
+        ]}
+        newsLetterData={newsLetterData}>
+{`
+**High-Performance Database Defined**
+
+In our fast-paced digital era, it’s crucial for businesses to keep up with rapidly expanding data volumes and their inevitable complexity. An essential tool enabling this efficiency is none other than a high-performance database. The term might sound a bit technical and perhaps intimidating. If you’re unfamiliar with it or interested in learning more, we’re here to break it down for you.
+
+High-performance databases, as the name suggests, are finely tuned engines of information retrieval and storage. These databases are specifically designed to process complex queries and handle vast amounts of data at high speed, making them indispensable for time-critical applications across sectors.
+
+In the following sections, we will talk about all that goes into these databases. We will also highlight how businesses can take advantage of ClickHouse for all their high-performance database needs, thus giving you the blueprint for growth and better decision-making.
+
+**Elements that Drive Performance**
+
+High-performance databases aren’t created by a stroke of luck or magic but through the careful consideration of several key elements. These databases are characterized by their speed, reliability, and scalability.
+
+**Speed**: At its core, a high-performance database should be able to process large amounts of data quickly and efficiently. This speed is derived from optimized data structures, indexing techniques, and advanced algorithms for query processing.
+
+**Reliability**: Data isn’t worth much if it can’t be trusted. Therefore, high-performance databases should maintain the integrity of data, provide backups, and ensure recovery options to minimize data loss risks.
+
+**Scalability**: As your business grows, your database should grow with it. High-performance databases are built to scale, both in terms of data storage and query processing capabilities.
+
+**Technology at Play**
+
+There’s a lot going on behind the scenes in a high-performance database, and the heart of the operation is the technology that powers it. Several key technologies are:
+
+**In-Memory Computing**: By storing data in the main memory rather than the disk, these databases significantly speed up data processing times.
+
+**Parallel Processing**: This is the concept of dividing tasks into smaller parts and processing them concurrently, leading to faster data retrieval and processing.
+
+**Column-Oriented Storage**: Unlike traditional row-based storage, column-oriented storage allows for quicker data access and analysis, which is particularly beneficial for analytical queries.
+
+**The Impact of High-Performance Databases on Businesses**
+
+Businesses have a lot to gain from using high-performance databases. Here are a few benefits to note:
+
+**Real-Time Decision-Making**: High-performance databases facilitate real-time analytics, empowering businesses to make informed decisions instantly.
+
+**Customer Satisfaction**: By providing fast and reliable services, businesses can significantly improve customer experience and satisfaction.
+
+**Operational Efficiency**: High-performance databases can handle heavy data loads, reducing system lag and increasing operational efficiency.
+
+**High-Performance Databases: Moving into the Future**
+
+Given their immense potential, high-performance databases are becoming an integral part of our future digital landscape. These databases are particularly important in emerging fields like machine learning, AI, and real-time analytics, which require rapid processing of vast amounts of data.
+
+However, with growing data privacy concerns, the onus is on high-performance database providers to ensure robust security measures. This blend of speed and security will define the next generation of high-performance databases.
+
+High-performance databases, in all their glory, are truly game-changers. They are not just a tool for data storage and retrieval but a critical component of business strategy, directly impacting decision-making, customer satisfaction, and operational efficiency. As technology evolves, we can only expect these databases to get faster, smarter, and more secure, redefining our data-driven future.
+
+**Advancements in High-Performance Databases: The Role of ClickHouse**
+
+With advancements in technology, databases have evolved to handle the growing demands for high performance. One such innovation is ClickHouse, an open-source, column-oriented DBMS designed for high-performance applications.
+
+ClickHouse stands out for its real-time query processing capabilities, which are ideal for analytical tasks. It operates on a column-oriented database structure, which is better suited for OLAP (Online Analytical Processing) scenarios. As a result, it processes queries faster, up to a hundred times quicker than traditional row-oriented databases.
+
+Furthermore, ClickHouse supports a wide variety of data sources, making it adaptable to different business needs. Its scalable design also allows it to handle data volume growth efficiently.
+
+A high-performance database is a vital tool for managing and leveraging the vast amounts of data generated in today’s digital age. With solutions like ClickHouse, businesses can harness their data’s power to drive strategic decision-making and growth.
+            `.trim()}
+      </SeoPage>
+    </Layout>
+  )
+}
