c8623c6dea6d6e830b4791fa705bdf185c309175
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-09-28 12:25:26 +0100

    remove altogether to test hydration error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -37,10 +37,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link href='favicon.ico' rel='icon' type='image/x-icon' />
       </Head>
       {/* Securiti.ai cookie blocker */}
-      <Script
+      {/* <Script
         src='https://cdn-prod.securiti.ai/consent/auto_blocking/8555e54b-cd0b-45d7-9c1c-e9e088bf774a/e058d040-977c-4594-aa2c-84b844ce5cf0.js'
         strategy='beforeInteractive'
-      />
+      /> */}
       {/* Securiti.ai cookie blocker */}
       <main className={`${inter.variable} font-inter ${inconsolata.variable}`}>
         <SnackbarContextProvider>
@@ -69,7 +69,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       `}
       </Script>
       {/* Securiti.ai Cookie Banner */}
-      <Script
+      {/* <Script
         defer
         data-strict-csp
         src='https://cdn-prod.securiti.ai/consent/cookie-consent-sdk-strict-csp.js'
@@ -83,7 +83,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           cookieSettingsButton?.classList.remove('hidden')
           cookieSettingsButton?.classList.add('cmp-revoke-consent')
         }}
-      />
+      /> */}
       {/* Securiti.ai Cookie Banner */}
     </>
   )
