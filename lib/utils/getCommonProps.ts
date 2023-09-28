faa434bb524c25521b76911759862ce2e3a5148b
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-09-28 20:41:34 +0100

    updated video titles

--- lib/utils/getCommonProps.ts
+++ lib/utils/getCommonProps.ts
@@ -20,7 +20,7 @@ export async function getCommonProps(): Promise<Props> {
   // )
   // const data = await githubData.json()
 
-  const stars = 30969
+  const stars = 30975
   return {
     footerData,
     platforms: getStartedData.platforms,
