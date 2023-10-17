b180c24b8af22c2a342d56bdc3022042c1052d98
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-10-17 15:06:44 +0100

    switching to prod mode to test in cookie sub domain

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -86,7 +86,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Script
           defer
           data-strict-csp
-          data-securiti-staging-mode='true'
           data-skip-css='false'
           src='https://cdn-prod.securiti.ai/consent/cookie-consent-sdk-loader-strict-csp.js'
           data-tenant-uuid='8555e54b-cd0b-45d7-9c1c-e9e088bf774a'
