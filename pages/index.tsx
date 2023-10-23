55c21cbc2a4e95e05abe47c0cf874da91a016a78
Gareth Jones <gareth@clickhouse.com>
2023-10-23 13:25:53 -0400

    Increasing subtitle size to 20px.

--- pages/index.tsx
+++ pages/index.tsx
@@ -125,7 +125,7 @@ export default function HomePage({
                 of rows in milliseconds
               </SuiTitle>
               <div className='mx-auto flex max-w-md flex-col items-center md:max-w-xl'>
-                <div className='mt-6 mb-10 text-center text-lg leading-normal text-neutral-0'>
+                <div className='mt-6 mb-10 text-center text-lg leading-normal text-neutral-0 md:text-xl'>
                   ClickHouse is the fastest and most resource efficient
                   open-source database for real-time apps and analytics.
                 </div>
