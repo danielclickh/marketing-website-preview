22a6550ca885879a48b39fb982d3a02987e87f40
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-11-01 11:51:56 +0000

    prod update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,14 @@ import UTMPersist from '../components/UTMPersist'
 import Head from 'next/head'
 
 const gtmId = process.env.NEXT_PUBLIC_GTM ?? 'GTM-TL8H72K'
+const websiteUrl = process.env.NEXT_PUBLIC_WEBSITE_URL
+let is_prod = false
+
+if (websiteUrl === 'https://clickhouse.com') {
+  is_prod = true
+} else {
+  is_prod = false
+}
 
 const inter = Inter({
   subsets: [],
@@ -47,15 +55,30 @@ function MyApp({ Component, pageProps }: AppProps) {
       <UTMPersist />
 
       {/* GTM - Prod/Env environments */}
-      <Script id='google-tag-manager' strategy='lazyOnload'>
-        {`
+      {is_prod ? (
+        // THIS IS PRODUCTION
+        <Script id='google-tag-manager' strategy='lazyOnload'>
+          {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer', '${gtmId}');
           `}
-      </Script>
+        </Script>
+      ) : (
+        // THIS IS DEV/LOCAL
+        <Script id='google-tag-manager' strategy='lazyOnload'>
+          {`
+            <!-- Google Tag Manager -->
+           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=BzKh0v8t1wje2QxxRxIGzA&gtm_preview=env-74&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${gtmId}');
+          `}
+        </Script>
+      )}
 
       <Script
         id='stripmkttok-script'
@@ -63,22 +86,45 @@ function MyApp({ Component, pageProps }: AppProps) {
         type='text/javascript'
         async
       />
-      <Script
-        defer
-        data-strict-csp
-        data-skip-css='false'
-        src='https://cdn-prod.securiti.ai/consent/cookie-consent-sdk-loader-strict-csp.js'
-        data-tenant-uuid='8555e54b-cd0b-45d7-9c1c-e9e088bf774a'
-        data-domain-uuid='e058d040-977c-4594-aa2c-84b844ce5cf0'
-        data-backend-url='https://app.securiti.ai'
-        onReady={() => {
-          const cookieSettingsButton = document.querySelector(
-            '#cookie-settings-button'
-          )
-          cookieSettingsButton?.classList.remove('hidden')
-          cookieSettingsButton?.classList.add('cmp-revoke-consent')
-        }}
-      />
+      {/* Securiti.ai Cookie Banner - first is produciton mode, second is dev */}
+      {is_prod ? (
+        // THIS IS PRODUCTION
+        <Script
+          defer
+          data-strict-csp
+          data-skip-css='false'
+          src='https://cdn-prod.securiti.ai/consent/cookie-consent-sdk-loader-strict-csp.js'
+          data-tenant-uuid='8555e54b-cd0b-45d7-9c1c-e9e088bf774a'
+          data-domain-uuid='e058d040-977c-4594-aa2c-84b844ce5cf0'
+          data-backend-url='https://app.securiti.ai'
+          onReady={() => {
+            const cookieSettingsButton = document.querySelector(
+              '#cookie-settings-button'
+            )
+            cookieSettingsButton?.classList.remove('hidden')
+            cookieSettingsButton?.classList.add('cmp-revoke-consent')
+          }}
+        />
+      ) : (
+        // THIS IS DEV/LOCAL
+        <Script
+          defer
+          data-skip-css='false'
+          data-strict-csp
+          src='https://cdn-prod.securiti.ai/consent/cookie-consent-sdk-loader-strict-csp.js'
+          data-tenant-uuid='8555e54b-cd0b-45d7-9c1c-e9e088bf774a'
+          data-domain-uuid='e058d040-977c-4594-aa2c-84b844ce5cf0'
+          data-backend-url='https://app.securiti.ai'
+          onReady={() => {
+            console.log('Cookie banner in dev mode')
+            const cookieSettingsButton = document.querySelector(
+              '#cookie-settings-button'
+            )
+            cookieSettingsButton?.classList.remove('hidden')
+            cookieSettingsButton?.classList.add('cmp-revoke-consent')
+          }}
+        />
+      )}
     </>
   )
 }
