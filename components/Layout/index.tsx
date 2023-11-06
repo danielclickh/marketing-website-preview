6b43396146fc888425ac7233cfccd639f21ba75e
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-11-06 16:44:33 +0000

    change class name

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -16,7 +16,7 @@ function Layout({ children, footerData, headerData, seo }: Props) {
     <div>
       {seo && <SeoContainer {...seo} />}
       <Header {...headerData} />
-      <div className='class-for-dale'>{children}</div>
+      <div className='readable-content'>{children}</div>
       <Footer {...footerData} />
     </div>
   )
