dbb73ea826624fd82096286dc699cdefa99cac30
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-11-16 15:16:02 +0000

    added all content. accordion to work on next

--- pages/use-cases/business-intelligence/index.tsx
+++ pages/use-cases/business-intelligence/index.tsx
@@ -2,22 +2,19 @@ import { GetStaticProps } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 import React, { useEffect } from 'react'
-import Tilt from 'react-parallax-tilt'
 import { CUIButton } from '../../../components/ClickUI'
 import GetStartedFree from '../../../components/GetStartedFree'
 import Layout from '../../../components/Layout'
 import LogoCarousel from '../../../components/LogoCarousel'
 import Markdown from '../../../components/Markdown'
 import AccordionComponent from '../../../components/RealTimeDiagram/Accordion'
-import Feature from '../../../components/RealTimeDiagram/feature-check'
 import { SuiText, SuiTitle } from '../../../components/sui'
 import { findOne } from '../../../lib/api/strapi'
 import { getCommonProps } from '../../../lib/utils/getCommonProps'
 import { CommonProps } from '../../../types/homepage'
+import bigNumbers from './big-numbers.json'
 import callouts from './callouts.json'
-import faqs from './faqs.json'
-import features from './features.json'
-import quotes from './quotes.json'
+import references from './supporting-references.json'
 
 interface RealTimeAnalyticsPageProps extends CommonProps {
   customerStories: any
@@ -133,77 +130,46 @@ export default function RealTimeAnalyticsPage({
           </div>
         </div>
 
-        <div className=' bg-neutral-725 '>
-          <div className='bg-shadow-element-left red-shadow section-container max-w-7xl'>
-            <div className='flex flex-col justify-between py-16 xl:flex-row xl:px-12'></div>
-          </div>
-
-          <div className='clip-inverted-triangle before:-top-40'>
-            <div className='mx-auto max-w-7xl'>
-              <div className='relative z-20 flex flex-col rounded-lg border-t-2 border-primary-300 bg-neutral-900 text-left text-neutral-0 shadow-lg'>
-                <div className='p-10'>
-                  <div className='flex flex-col gap-x-6 gap-y-6 lg:flex-row'>
-                    {quotes.map((quote) => (
-                      <Tilt
-                        tiltEnable={false}
-                        glareEnable={true}
-                        glareMaxOpacity={0.4}
-                        glareColor='rgba(251, 255, 70, 0.08)'
-                        glarePosition='all'
-                        className='flex-1'
-                        key={quote.id}>
-                        {quote.href ? (
-                          <Link href={quote.href} target={quote.target}>
-                            <div className='animate-fade-in relative flex h-full w-full flex-col rounded-lg border border-neutral-725 bg-neutral-900/50 p-6 px-4 text-center shadow-card hover:bg-neutral-725/90 hover:shadow-lg'>
-                              <Image
-                                src='/images/Quote.svg'
-                                width={37}
-                                height={28}
-                                alt='Quote'
-                                className='mb-4 block'
-                              />
-                              <Markdown
-                                className='min-h-auto text-left xl:min-h-[250px]'
-                                children={quote.content}
-                              />
-                              <Image
-                                src={quote.logo}
-                                width={quote.imgWidth}
-                                height={quote.imgHeight}
-                                alt={quote.title}
-                                className='mt-12 xl:mt-auto'
-                              />
-                            </div>
-                          </Link>
-                        ) : (
-                          <div className='animate-fade-in relative flex h-full w-full flex-col rounded-lg border border-neutral-725 bg-neutral-900/50 p-6 px-4 text-center shadow-card hover:bg-neutral-800/90 hover:shadow-lg'>
-                            <Image
-                              src='/images/Quote.svg'
-                              width={37}
-                              height={28}
-                              alt='Quote'
-                              className='mb-4 block'
-                            />
-                            <Markdown
-                              className='min-h-auto text-left xl:min-h-[250px]'
-                              children={quote.content}
-                            />
-                            <Image
-                              src={quote.logo}
-                              width={quote.imgWidth}
-                              height={quote.imgHeight}
-                              alt={quote.title}
-                              className='mt-12 xl:mt-auto'
-                            />
-                          </div>
+        <div className='bg-neutral-725 '>
+          <div className='bg-neutral-725 text-neutral-0'>
+            <div className='container mx-auto max-w-5xl px-4 pb-16 pt-12 sm:px-8 md:px-8 2xl:px-0'>
+              <h2 className='text-center font-basier text-2xl font-semibold lg:text-4xl lg:leading-relaxed'>
+                Discover why developers are choosing ClickHouse to power their
+                real-time data warehouse.
+              </h2>
+              <div className='py-12'>
+                <div className='mx-auto grid max-w-3xl grid-cols-2 justify-between gap-y-10 gap-x-16 text-center md:grid-cols-4'>
+                  {bigNumbers.map((number, index) => {
+                    return (
+                      <div key={index}>
+                        <div className='flex min-h-[144px] min-w-[144px] items-center rounded-[36px] border-4 border-primary-300 bg-[#1E1E1B] text-center'>
+                          <p className='w-full text-4xl font-semibold text-primary-300'>
+                            {number.stat}
+                          </p>
+                        </div>
+                        <p className='mt-6 text-lg font-semibold'>
+                          {number.content}
+                        </p>
+                        {number.readmore && (
+                          <Markdown
+                            className='text-center text-sm'
+                            children={number.readmore}
+                          />
                         )}
-                      </Tilt>
-                    ))}
-                  </div>
+                        {number.subcontent && (
+                          <p className='mt-4 text-center text-xs'>
+                            {number.subcontent}
+                          </p>
+                        )}
+                      </div>
+                    )
+                  })}
                 </div>
               </div>
             </div>
           </div>
+
+          <div className='clip-inverted-triangle before:-top-40'></div>
         </div>
 
         <div className='relative z-10 mx-auto bg-primary-300'>
@@ -231,64 +197,72 @@ export default function RealTimeAnalyticsPage({
         </div>
 
         <div className='bg-neutral-725 pb-24'>
-          <div className='relative mx-auto pt-12  md:px-0 md:pt-24'>
+          <div className='relative mx-auto pt-12 md:px-0 md:pt-24'>
             <div className='mx-auto max-w-7xl'>
-              <div className='flex w-full flex-col items-center pt-6 pb-12'>
-                <Image
-                  src='/images/use-cases/logging/icon-how.svg'
-                  alt='System overview'
-                  width={72}
-                  height={73}
-                />
-                <SuiTitle type='h2' className='mt-8 text-center'>
-                  Real-time Applications and Dashboards
-                </SuiTitle>
-                <p className='mx-auto mb-12 max-w-3xl px-9 pt-6 text-center text-sm'>
-                  ClickHouse is used across industries to power systems and
-                  applications where real-time analysis, evaluation, and
-                  querying are critical.
-                </p>
-                <div className='mb-12 flex w-full flex-col gap-6 px-8 lg:flex-row lg:px-6 xl:px-0'>
-                  {[10, 20, 30].map((section) => (
-                    <div
-                      key={section}
-                      className='items-center rounded-md border border-neutral-0/30 bg-[rgba(62,62,62,1)] py-4 px-6 text-left lg:w-1/3'>
-                      {features
-                        .filter((feature) => feature.section === section)
-                        .map((feature) => (
-                          <Feature key={feature.id} feature={feature} />
-                        ))}
-                    </div>
-                  ))}
+              <div className='mx-auto max-w-6xl'>
+                <div className='flex w-full flex-col items-center pt-6 pb-12'>
+                  <div className='bg-shadow-element-right yellow-shadow grid justify-between gap-10 lg:grid-cols-2'>
+                    {callouts.map((callout) => (
+                      <div
+                        key={callout.id}
+                        className='rounded-lg border border-t-4 border-[#414141] border-t-primary-300 bg-neutral-900 p-7 shadow-lg'>
+                        <Image
+                          src={callout.icon}
+                          alt={callout.title}
+                          width={32}
+                          height={32}
+                          className='mb-4'
+                        />
+                        <h3 className='mb-4 text-lg font-bold'>
+                          {callout.title}
+                        </h3>
+                        <div className='rich_content text-base text-neutral-200'>
+                          <Markdown children={callout.content} />
+                        </div>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
               <AccordionComponent />
-              <div className='mx-auto max-w-5xl px-4 xl:px-0'>
-                <div className='grid justify-between gap-20 pt-20 lg:grid-cols-2'>
-                  {callouts.map((feature) => (
-                    <div key={feature.id} className='px-3'>
-                      <Image
-                        src={feature.icon}
-                        alt={feature.title}
-                        width={32}
-                        height={32}
-                        className='mb-4'
-                      />
-                      <h3 className='mb-4 text-lg font-bold'>
-                        {feature.title}
-                      </h3>
-                      <div className='rich_content text-base text-neutral-200'>
-                        <Markdown children={feature.content} />
-                      </div>
-                    </div>
-                  ))}
-                </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='bg-primary-300 py-14'>
+          <div className='mx-auto max-w-5xl px-8 md:px-0'>
+            <div className='flex flex-col items-start justify-between gap-x-24 gap-y-10 text-center text-black md:flex-row'>
+              <div>
+                <h3 className='mb-8 font-basier text-3xl font-bold leading-10'>
+                  Best-in-class
+                  <br />
+                  performance
+                </h3>
+                <p className='text-sm leading-6'>
+                  Unlike other JVM-based solutions which are limited in their
+                  ability to scale vertically due to expensive GC cycles on
+                  larger heaps, ClickHouse leverages the full resources of a
+                  machine and scales both horizontally and vertically to systems
+                  with hundreds of cores and petabytes of storage.
+                </p>
+              </div>
+              <div>
+                <h3 className='mb-8 font-basier text-3xl font-bold leading-10'>
+                  Flexible and scalable concurrency
+                </h3>
+                <p className='text-sm leading-6'>
+                  With ClickHouse, you can build the powerful Business
+                  Intelligence applications your users will love without
+                  worrying about responsiveness at scale. Ingest millions of
+                  rows per second. Handle the most heavily concurrent workloads.
+                  All without compromising query speed.
+                </p>
               </div>
             </div>
           </div>
         </div>
 
-        <div className='bg-shadow-element-right yellow-shadow '>
+        <div className='bg-shadow-element-right yellow-shadow'>
           <div className='section-container mb-24 flex w-full pt-24 text-neutral-0 md:px-8 2xl:px-0 '>
             <div className='mx-auto flex w-full flex-col justify-center rounded-xl border border-neutral-700/80 bg-neutral-900/50 bg-right bg-no-repeat py-16 px-4 xl:px-24'>
               <div className='flex flex-col text-center'>
@@ -304,69 +278,18 @@ export default function RealTimeAnalyticsPage({
                 </div>
                 <div className='bg-neutral-725 p-8'>
                   <ol className='list-decimal space-y-2 text-left	text-primary-300'>
-                    <li>
-                      <Link
-                        href='/blog/asynchronous-data-inserts-in-clickhouse'
-                        className='text-primary-300'>
-                        Asynchronous Data Inserts in ClickHouse
-                      </Link>
-                    </li>
-
-                    <li>
-                      <a
-                        href='/blog/real-time-event-streaming-with-confluent-cloud-clickhouse-and-clickpipes'
-                        className='text-primary-300'>
-                        Real-time event streaming with ClickHouse, Confluent
-                        Cloud and ClickPipes
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href='/blog/building-real-time-applications-with-clickhouse-and-hex-notebook-keeper-engine'
-                        className='text-primary-300'>
-                        Adding Real-Time Analytics to a Supabase Application
-                        With ClickHouse
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href='https://python.langchain.com/docs/integrations/vectorstores/clickhouse'
-                        className='text-primary-300'>
-                        Building Real-time Analytics Apps with ClickHouse and
-                        Hex
-                      </a>
-                    </li>
-
-                    <li>
-                      <a
-                        href='/blog/using-materialized-views-in-clickhouse'
-                        className='text-primary-300'>
-                        Using Materialized Views in ClickHouse
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href='/blog/working-with-time-series-data-and-functions-ClickHouse'
-                        className='text-primary-300'>
-                        Working with Time Series Data in ClickHouse
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href='/blog/clickhouse-postgresql-change-data-capture-cdc-part-1'
-                        className='text-primary-300'>
-                        Change Data Capture (CDC) with PostgreSQL and ClickHouse
-                        - Part 1
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href='/blog/clickhouse-postgresql-change-data-capture-cdc-part-2'
-                        className='text-primary-300'>
-                        Change Data Capture (CDC) with PostgreSQL and ClickHouse
-                        - Part 2
-                      </a>
-                    </li>
+                    {references.map((reference, index) => {
+                      return (
+                        <li key={index}>
+                          <Link
+                            href={reference.href}
+                            target={reference.target}
+                            className='text-primary-300'>
+                            {reference.text}
+                          </Link>
+                        </li>
+                      )
+                    })}
                   </ol>
                 </div>
               </div>
