14db4e3103c715e9f8dd6b6baf71ee6111dd1e84
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-12-11 10:47:51 +0000

    stars

--- lib/utils/getCommonProps.ts
+++ lib/utils/getCommonProps.ts
@@ -20,7 +20,7 @@ export async function getCommonProps(): Promise<Props> {
   // )
   // const data = await githubData.json()
 
-  const stars = 31963
+  const stars = 32030
   return {
     footerData,
     platforms: getStartedData.platforms,
