b7625a7f65352a6018d2be57fc6993d5839ceed3
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-12-13 16:36:33 +0000

    api route in test mode

--- pages/api/pricing-api.ts
+++ pages/api/pricing-api.ts
@@ -33,6 +33,12 @@ const handler = async (
 ) => {
   const { provider, region, tier } = req.query
 
+  //only for testing
+
+  return res
+    .status(200)
+    .json({ computeUnitPrice: 0.00182, storageUnitPrice: 6.85e-7 })
+
   // Validate the query params were provided.
   // Using `typeof` ensures the value is not undefined and is not an array of values.
   if (
