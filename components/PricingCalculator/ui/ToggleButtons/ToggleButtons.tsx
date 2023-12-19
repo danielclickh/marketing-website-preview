3eac82586c0e23aa2eac7cb0206cd04fdd19e68d
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-12-19 10:25:06 +0000

    updated provider and tier styles

--- components/PricingCalculator/ui/ToggleButtons/ToggleButtons.tsx
+++ components/PricingCalculator/ui/ToggleButtons/ToggleButtons.tsx
@@ -26,7 +26,7 @@ export function ToggleButtons<T extends string = string>({
           className={`flex gap-2 rounded-[4px] bg-neutral-750 py-2 px-10 text-sm font-medium transition-all delay-75 hover:cursor-pointer hover:border hover:border-primary-300 hover:bg-neutral-725 ${
             value === option.value
               ? 'border border-primary-300'
-              : 'border border-primary-600'
+              : 'border border-neutral-700'
           }`}
           onClick={() => {
             router.push(
