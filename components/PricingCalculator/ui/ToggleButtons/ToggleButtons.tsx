2d30b3dcaaa1211185649e8e7af98d3c06ec387b
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2023-12-19 10:39:54 +0000

    styles for hovers

--- components/PricingCalculator/ui/ToggleButtons/ToggleButtons.tsx
+++ components/PricingCalculator/ui/ToggleButtons/ToggleButtons.tsx
@@ -23,7 +23,7 @@ export function ToggleButtons<T extends string = string>({
       {options.map((option, index) => (
         <div
           key={index}
-          className={`flex gap-2 rounded-[4px] bg-neutral-750 py-2 px-10 text-sm font-medium transition-all delay-75 hover:cursor-pointer hover:border hover:border-primary-300 hover:bg-neutral-725 ${
+          className={`flex gap-2 rounded-[4px] bg-neutral-750 py-2 px-10 text-sm font-medium transition-all delay-75 hover:cursor-pointer hover:border-primary-500 hover:bg-neutral-725 hover:bg-opacity-80 hover:shadow-xl ${
             value === option.value
               ? 'border border-primary-300'
               : 'border border-neutral-700'
