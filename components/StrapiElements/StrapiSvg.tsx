dfe9c6867afe0e56cc41dad0c3d7fe82a4b740da
Levi Cole <levi@skape.co>
2023-12-19 15:02:55 +0000

    Added prefixes to SVG id="" props

--- components/StrapiElements/StrapiSvg.tsx
+++ components/StrapiElements/StrapiSvg.tsx
@@ -5,6 +5,7 @@ const strapiApiUrl =
   process.env.STRAPI_API_URL ?? 'https://cms.clickhouse.com:1337'
 
 export default function StrapiSvg({
+  id,
   url,
   className = '',
   width,
@@ -12,8 +13,28 @@ export default function StrapiSvg({
   svgText = ''
 }: StrapiImageProps) {
   try {
+
+    // Create a mutable svg string
+    let svgString = svgText;
+
+    // Get all ID values from the svg
+    const matches = svgString.matchAll(/\s+id="([^"]+)"/gi);
+
+    // Replace all id references with a new preifxed value
+    // @ts-ignore
+    for (const match of matches) {
+      const fullMatch = match[0]; // id="circle"
+      const idMatch = match[1]; // circle
+      const newId = `svg-id-${id}-${idMatch}`; // svg-id-123-circle
+
+      const idRegex = new RegExp(`#${idMatch}([^-_a-zA-Z0-9])`, 'g');
+      svgString = svgString.replaceAll(fullMatch, ` id="${newId}"`);
+      svgString = svgString.replaceAll(idRegex, `#${newId}$1`);
+    }
+
     return (
       <Markdown
+        children={svgString}
         encloseByDiv={false}
         components={{
           svg: ({ node, ...params }) => {
@@ -26,9 +47,7 @@ export default function StrapiSvg({
               />
             )
           }
-        }}>
-        {svgText}
-      </Markdown>
+        }} />
     )
   } catch (e) {
     console.log('Error fetching svg', `${strapiApiUrl}${url}`)
