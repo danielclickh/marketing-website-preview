8dfc82ce59342ad28998dc6cbfd99798e1730f44
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2024-01-04 11:49:14 +0000

    updated eu-west-2 acceptable

--- components/PricingCalculator/CalculatorTypesOptions.ts
+++ components/PricingCalculator/CalculatorTypesOptions.ts
@@ -58,6 +58,7 @@ export const acceptableRegions = [
   { provider: 'aws', region: 'us-west-2' },
   { provider: 'aws', region: 'us-east-1' },
   { provider: 'aws', region: 'eu-west-1' },
+  { provider: 'aws', region: 'eu-west-2' },
   { provider: 'aws', region: 'eu-central-1' },
   { provider: 'aws', region: 'ap-southeast-1' },
   { provider: 'aws', region: 'ap-south-1' },
