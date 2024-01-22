23cd110eee1b1ffa8b48c79fa35e4c0aaea8f514
Levi Cole <levi@skape.co>
2024-01-22 10:19:05 +0000

    Fixed issue with the `<SeoContainer />` component which would ignore the `imageUrl` property which now takes priority if it has a value.

--- components/SeoContainer/index.tsx
+++ components/SeoContainer/index.tsx
@@ -6,13 +6,14 @@ const siteUrl = process.env.NEXT_PUBLIC_WEBSITE_URL ?? 'https://clickhouse.com'
 
 function SeoContainer({
   image,
+  imageUrl,
   title = '',
   description = '',
   type,
   siteName,
   path
 }: SeoMetadata) {
-  const imageUrl = image?.[0]?.url || '/images/social_share.png'
+  const socialImageUrl = imageUrl || siteUrl + image?.[0]?.url || `${siteUrl}/images/social_share.png`
 
   const canonicalUrl =
     path === '/blog/forecasting-using-clickhouse'
@@ -38,7 +39,7 @@ function SeoContainer({
       )}
       {type && <meta property='og:type' content={type} />}
       {siteName && <meta name='og:site_name' content={siteName} />}
-      <meta property='og:image' content={siteUrl + imageUrl} />
+      <meta property='og:image' content={socialImageUrl} />
 
       {/* Twitter */}
       <meta name='twitter:card' content='summary_large_image' />
@@ -46,7 +47,7 @@ function SeoContainer({
       {description.length > 0 && (
         <meta name='twitter:description' content={description} />
       )}
-      <meta name='twitter:image' content={siteUrl + imageUrl} />
+      <meta name='twitter:image' content={socialImageUrl} />
     </Head>
   )
 }
