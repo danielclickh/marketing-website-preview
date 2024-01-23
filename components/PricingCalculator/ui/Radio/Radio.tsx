ddf1a5b9b47e90afd917258b9b804860b387731b
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2024-01-23 11:59:30 +0100

    radio style tweaks

--- components/PricingCalculator/ui/Radio/Radio.tsx
+++ components/PricingCalculator/ui/Radio/Radio.tsx
@@ -24,32 +24,32 @@ const RadioGroupComponent: React.FC<RadioGroupComponentProps> = ({ value }) => {
 
   return (
     <RadioGroup.Root
-      className='mt-4 flex flex-row items-center gap-2.5 gap-x-6'
+      className='mt-5 flex flex-row items-center gap-2.5 gap-x-6'
       value={value}
       onValueChange={onValueChange}
-      aria-label='Is your data compress?'>
+      aria-label='Is your data compressed?'>
       <div className='flex items-center'>
         <RadioGroup.Item
-          className='h-[25px] w-[25px] cursor-pointer rounded-full border border-neutral-700 bg-neutral-750 outline-none hover:border-primary-500 hover:bg-neutral-725 hover:bg-opacity-80 hover:shadow-xl'
+          className='h-[16px] w-[16px] cursor-pointer rounded-full border border-neutral-700 bg-neutral-750 outline-none hover:border-primary-500 hover:bg-neutral-725 hover:bg-opacity-80 hover:shadow-xl'
           value='no'
           id='r2'>
-          <RadioGroup.Indicator className="relative flex h-full w-full items-center justify-center after:block after:h-[7px] after:w-[7px] after:rounded-[50%] after:bg-primary-300 after:content-['']" />
+          <RadioGroup.Indicator className="relative flex h-full w-full items-center justify-center after:block after:h-[8px] after:w-[8px] after:rounded-[50%] after:bg-primary-300 after:content-['']" />
         </RadioGroup.Item>
         <label
-          className='cursor-pointer pl-[15px] text-[15px] leading-none text-white'
+          className='cursor-pointer pl-[8px] text-[15px] leading-none text-white'
           htmlFor='r2'>
           No
         </label>
       </div>{' '}
       <div className='flex items-center'>
         <RadioGroup.Item
-          className='h-[25px] w-[25px] cursor-pointer rounded-full border border-neutral-700 bg-neutral-750 outline-none hover:border-primary-500 hover:bg-neutral-725 hover:bg-opacity-80 hover:shadow-xl'
+          className='h-[16px] w-[16px] cursor-pointer rounded-full border border-neutral-700 bg-neutral-750 outline-none hover:border-primary-500 hover:bg-neutral-725 hover:bg-opacity-80 hover:shadow-xl'
           value='yes'
           id='r1'>
-          <RadioGroup.Indicator className="relative flex h-full w-full items-center justify-center after:block after:h-[7px] after:w-[7px] after:rounded-[50%] after:bg-primary-300 after:content-['']" />
+          <RadioGroup.Indicator className="relative flex h-full w-full items-center justify-center after:block after:h-[8px] after:w-[8px] after:rounded-[50%] after:bg-primary-300 after:content-['']" />
         </RadioGroup.Item>
         <label
-          className='cursor-pointer pl-[15px] text-[15px] leading-none text-white'
+          className='cursor-pointer pl-[8px] text-[15px] leading-none text-white'
           htmlFor='r1'>
           Yes
         </label>
