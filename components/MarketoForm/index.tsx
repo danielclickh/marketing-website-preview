26d6c40646d95e7f200e8dd3bf41339c187df5ca
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2024-02-13 13:39:47 +0000

    add full url to iframe

--- components/MarketoForm/index.tsx
+++ components/MarketoForm/index.tsx
@@ -46,6 +46,7 @@ interface MarketoObjectController extends SpoofedMarketoObject {
 type MarketoFormProps = {
   formId: string
   disclaimer?: string | false
+  siteUrl?: string
 
   // Callbacks
   onLoad?: (formObject: SpoofedMarketoObject) => any
@@ -59,7 +60,10 @@ export default function MarketoForm({
   formId,
   disclaimer = 'By registering, you acknowledge that ClickHouse will process your personal information in accordance with our [Privacy Policy](/legal/privacy-policy).',
   onLoad,
-  onSuccess
+  onSuccess,
+  siteUrl = process.env.NEXT_PUBLIC_WEBSITE_URL
+    ? process.env.NEXT_PUBLIC_WEBSITE_URL
+    : ''
 }: MarketoFormProps) {
   const router = useRouter()
   const instanceId = useId()
@@ -251,15 +255,20 @@ export default function MarketoForm({
   return (
     <>
       {mountIframe && (
-        <iframe
-          ref={iframeRef}
-          src={resolveHref(router, `/marketo-forms/${formId}?${queryString}`)}
-          height={iframeHeight < 24 ? 24 : iframeHeight}
-          scrolling='no' // Deprecated but still hides scrollbars
-          className={`w-full !bg-transparent transition-opacity ${
-            formLoaded ? 'opacity-100' : 'opacity-0'
-          }`}
-        />
+        <>
+          <iframe
+            ref={iframeRef}
+            src={resolveHref(
+              router,
+              `${siteUrl}/marketo-forms/${formId}?${queryString}`
+            )}
+            height={iframeHeight < 24 ? 24 : iframeHeight}
+            scrolling='no' // Deprecated but still hides scrollbars
+            className={`w-full !bg-transparent transition-opacity ${
+              formLoaded ? 'opacity-100' : 'opacity-0'
+            }`}
+          />
+        </>
       )}
 
       {formLoaded && !formSuccess && disclaimer && disclaimer.length && (
