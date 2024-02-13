96e9d62a442b49cbd553468b67c669c2ffbac0c5
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2024-02-13 11:51:34 +0000

    removed dupe privacy notice on byoc form

--- pages/cloud/bring-your-own-cloud/index.tsx
+++ pages/cloud/bring-your-own-cloud/index.tsx
@@ -89,16 +89,6 @@ export default function Page({ footerData, headerData, seo }: HomePageProps) {
                   <div className='text-center'>Loading form...</div>
                 )}
 
-                {formLoaded && !formSuccess && (
-                  <div className='disclaimer-text mt-8 text-center text-sm font-medium text-neutral-200'>
-                    <Markdown>
-                      By registering, you acknowledge that ClickHouse will
-                      process your personal information in accordance with our
-                      [Privacy Policy](/legal/privacy-policy).
-                    </Markdown>
-                  </div>
-                )}
-
                 {formSuccess && (
                   <div ref={formSuccessRef} className='text-center'>
                     <h3 className='text-2xl font-bold'>Thank you!</h3>
