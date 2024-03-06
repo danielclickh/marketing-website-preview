79b5468439b90a6e1b8c4bf278db6f92c8c5286a
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2024-03-06 17:00:01 +0000

    add blog link

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -66,7 +66,7 @@ export default function Header({ header, github: { stars } }: HeaderProps) {
   }, [])
 
   header.banner =
-    '<a href="/blog">chDB joins the ClickHouse family. Read more</a>'
+    '<a href="/blog/chdb-joins-clickhouse-family">chDB joins the ClickHouse family. Read more</a>'
 
   return (
     <>
