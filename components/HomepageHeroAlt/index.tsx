a24664e57c182a2abb6d652bac68d93701064a76
Levi Cole <levi@skape.co>
2024-03-11 11:54:55 +0000

    Initial progress on new hero

--- components/HomepageHeroAlt/index.tsx
+++ components/HomepageHeroAlt/index.tsx
@@ -1,29 +1,88 @@
+import Image from 'next/image'
+import { galaxyOnClick } from '../../lib/galaxy/galaxy'
+import { CUIButton } from '../ClickUI'
 import RollerText from '../RollerText'
 import { SuiText, SuiTitle } from '../sui'
 
 export default function HomepageHeroAlt() {
   return (
-    <div className='bg-primary-300 py-48 text-black'>
-      <div className='section-container'>
-        <SuiTitle type='h1'>
-          The{' '}
-          <span className='tilted tilted-black'>
-            <span className='tilted-content text-white'>real-time</span>
-          </span>{' '}
-          <br />
-          data warehouse for <br />
-          <RollerText
-            phraseList={[
-              'analytics',
-              'observability',
-              'ML & GenAI',
-              'business intelligence',
-              'financial services',
-              'fraud & cybersecurity',
-              'gaming'
-            ]}
-          />
-        </SuiTitle>
+    <div className='relative overflow-x-hidden border-primary-300 lg:border-t-[38px]'>
+      <div className='absolute top-0 left-0 bottom-0 z-10 hidden w-1/2 bg-primary-300 lg:block'></div>
+      <div className='section-container relative z-20 grid lg:grid-cols-2'>
+        {/* Text column */}
+        <div className='relative z-30 order-last bg-primary-300 py-32 text-black lg:order-first'>
+          <SuiTitle type='h1'>
+            The{' '}
+            <span className='tilted tilted-black'>
+              <span className='tilted-content text-white'>real-time</span>
+            </span>{' '}
+            <br />
+            data warehouse for <br />
+            <RollerText
+              phraseList={[
+                'analytics',
+                'observability',
+                'ML & GenAI',
+                'business intelligence',
+                'financial services',
+                'fraud & cybersecurity',
+                'gaming'
+              ]}
+            />
+          </SuiTitle>
+          <div className='inline-block w-auto'>
+            <SuiText size='lg' className='mt-6 mb-16 w-auto lg:!text-[20px]'>
+              Unlock faster queries without skyrocketing costs.
+            </SuiText>
+            <div className='flex w-auto gap-6'>
+              <CUIButton
+                type='primary-dark'
+                size='lg'
+                weight='semibold'
+                href='#'
+                linkClass='w-full'
+                className='w-full'>
+                Start free cloud trial
+              </CUIButton>
+              <CUIButton
+                type='secondary-dark'
+                weight='semibold'
+                size='lg'
+                href='https://clickhouse.com/docs/en/intro'
+                linkClass='w-full'
+                className='w-full !border-black !text-black !transition-all hover:!text-white'>
+                Download open-source
+              </CUIButton>
+            </div>
+          </div>
+        </div>
+
+        {/* Graphic column */}
+        <div className='relative order-first lg:order-last'>
+          {/* Angle separator */}
+          <div className='absolute top-0 left-0 bottom-0 right-0 z-20 hidden text-primary-300 lg:block'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='auto'
+              height='100%'
+              fill='none'
+              viewBox='0 0 272 608'>
+              <path
+                fill='currentColor'
+                d='M272 .00966757 0 0v607.989h1.7469c28.0458.006 52.8287-18.248 61.1401-45.034L227.52 32.3775C233.493 13.129 251.3.0097294 271.453.00966921L272 .00966757Z'
+              />
+            </svg>
+          </div>
+          <div className='lg:absolute lg:top-0 lg:left-8 lg:bottom-0 lg:right-0 lg:z-10'>
+            <Image
+              src='/images/homepage/home-hero-product-ui.png'
+              width={3000}
+              height={1931}
+              alt='ClickHouse Product Screenshot'
+              className='aspect-[3000/1931] h-auto w-full lg:h-full lg:w-auto lg:max-w-none'
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
