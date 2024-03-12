6b25885fbdd26b971ce3ce282d608d794242f727
Levi Cole <levi@skape.co>
2024-03-12 17:52:13 +0000

    Added hacky logo resizing to new trusted by layout

--- components/HomepageSectionTrustedBy/index.tsx
+++ components/HomepageSectionTrustedBy/index.tsx
@@ -7,14 +7,26 @@ export default function HomepageSectionTrustedBy({
 }: {
   customerStories: HomepageCustomerStories
 }) {
+  const logoSizeModifier = 0.875
+
+  // Hacky way of resizing the logos
+  // First we clone the array so not to modify else where
+  // Then we alter the strapi data which gets used by the LogoCarousel component
+  const logos = structuredClone(customerStories.logos).map((logo) => {
+    if (logo.darkLogoPng?.width && logo.darkLogoPng?.height) {
+      logo.darkLogoPng.width = logo.darkLogoPng.width * logoSizeModifier
+      logo.darkLogoPng.height = logo.darkLogoPng.height * logoSizeModifier
+    }
+    if (logo.lightLogoPng?.width && logo.lightLogoPng?.height) {
+      logo.lightLogoPng.width = logo.lightLogoPng.width * logoSizeModifier
+      logo.lightLogoPng.height = logo.lightLogoPng.height * logoSizeModifier
+    }
+    return logo
+  })
+
   // Split the customerStories.logos array into two separate arrays
-  const logos1 = customerStories.logos.slice(
-    0,
-    Math.ceil(customerStories.logos.length / 2)
-  )
-  const logos2 = customerStories.logos.slice(
-    Math.ceil(customerStories.logos.length / 2)
-  )
+  const logos1 = logos.slice(0, Math.ceil(logos.length / 2))
+  const logos2 = logos.slice(Math.ceil(logos.length / 2))
 
   return (
     <div className='my-16 md:my-20'>
@@ -24,7 +36,7 @@ export default function HomepageSectionTrustedBy({
         className='mb-4 text-center uppercase tracking-wide text-primary-300'>
         Trusted by
       </SuiText>
-      <div className='relative z-10 flex flex-wrap place-items-center items-center justify-center gap-6 self-center grayscale invert md:gap-x-14'>
+      <div className='relative z-10 flex flex-wrap place-items-center items-center justify-center gap-6 self-center opacity-90 grayscale invert md:gap-x-14'>
         <LogoCarousel
           logoColor='white'
           logos={logos1}
