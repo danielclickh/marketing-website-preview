8ea88cccc97367c6df861cd0c754a87d4a876e3b
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2024-03-18 13:18:03 +0000

    fix experiements .cloud links

--- components/UTMPersist/index.tsx
+++ components/UTMPersist/index.tsx
@@ -1,19 +1,21 @@
+import { Experiment, Result } from '@growthbook/growthbook'
 import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 import { Galaxy } from '../../lib/galaxy/web/browser'
-import { Experiment, Result } from '@growthbook/growthbook'
 
 type UTMs = {
   [key: string]: string
 }
 
-const updateLinks = () => {
+const updateLinks = (experimentId?: string, variationId?: string) => {
   const links = Array.from(document.querySelectorAll('a'))
   for (const link of links) {
     if (link.hostname.includes('.cloud')) {
-      const updatedURL = appendUTMsToLink(link.href)
-
-      link.href = appendGalaxySessionIDToLink(updatedURL)
+      link.href = appendUTMsToLink(link.href)
+      link.href = appendGalaxySessionIDToLink(link.href)
+      if (experimentId && variationId) {
+        link.href = appendExperimentToLink(link.href, experimentId, variationId)
+      }
     }
   }
 }
@@ -24,21 +26,7 @@ export const onExperimentViewed = (
 ) => {
   const experimentId = experiment.key
   const variationId = result.key
-
-  console.log('Experiment viewed:', { experimentId, variationId })
-
-  const links = Array.from(document.querySelectorAll('a'))
-  for (const link of links) {
-    if (link.hostname.includes('.cloud')) {
-      const updatedURL = appendExperimentParamsToLink(
-        link.href,
-        experimentId,
-        variationId
-      )
-      link.href = updatedURL
-      updateLinks()
-    }
-  }
+  updateLinks(experimentId, variationId)
 }
 
 const UTMPersist = () => {
@@ -94,7 +82,7 @@ export function appendUTMsToLink(url: string): string {
 }
 
 export function appendGalaxySessionIDToLink(url: string): string {
-  const galaxy_id = Galaxy.getAnonymousId()
+  const galaxy_id = Galaxy.getGalaxySessionId()
   const urlObject = new URL(url)
 
   // Append galaxy session id to links that contain ".cloud"
@@ -137,18 +125,37 @@ function storeUTMsInStorage(utms: UTMs) {
   localStorage.setItem('ch-utms', JSON.stringify(data))
 }
 
-export function appendExperimentParamsToLink(
+// Function to append experiment and variation to link
+function appendExperimentToLink(
   url: string,
   experimentId: string,
   variationId: string
 ): string {
   const urlObject = new URL(url)
 
-  // Append experiment parameters to links that contain ".cloud"
-  if (experimentId && variationId) {
-    urlObject.searchParams.set('experimentId', experimentId)
-    urlObject.searchParams.set('variationId', variationId)
-  }
+  // Retrieve existing experiments and parse them into an object
+  const experimentsQuery = urlObject.searchParams.get('experiments')
+
+  let experiments: Record<string, string> = experimentsQuery
+    ? experimentsQuery
+        .split(',')
+        .reduce((acc: Record<string, string>, curr: string) => {
+          const [id, variation] = curr.split(':')
+          acc[id] = variation
+          return acc
+        }, {})
+    : {}
+
+  // Update the experiments object with the new experiment and variation
+  experiments[experimentId] = variationId
+
+  // Convert the experiments object back into a query string
+  const newExperimentsQuery = Object.entries(experiments)
+    .map(([id, variation]) => `${id}:${variation}`)
+    .join(',')
+
+  // Set the updated experiments query back on the URL
+  urlObject.searchParams.set('experiments', newExperimentsQuery)
 
   return urlObject.toString()
 }
