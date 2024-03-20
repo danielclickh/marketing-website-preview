1a2ab67144fe1418f23d9de8b50a495966021b0e
Levi Cole <levi@skape.co>
2024-03-20 17:05:12 +0000

    Fixed timeline label alignment

--- pages/real-time-data-warehouse/index.tsx
+++ pages/real-time-data-warehouse/index.tsx
@@ -818,7 +818,7 @@ const TimelineCard = forwardRef(function TimelineCard(
         {active && (
           <>
             <span className='md:hidden'>{label}</span>
-            <span className='tilted tilted-yellow hidden py-2 px-3 md:inline-block'>
+            <span className='tilted tilted-yellow -my-2 hidden py-2 px-3 md:inline-block'>
               <span className='tilted-content'>{label}</span>
             </span>
           </>
