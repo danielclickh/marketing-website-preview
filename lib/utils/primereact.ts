04a4ed5d63c563dee4a77a09d1a9a3b692ecc8bd
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2024-05-09 14:47:14 +0100

    add prime react mulitselect

---
+++ lib/utils/primereact.ts
@@ -0,0 +1,163 @@
+import { classNames } from '../../lib/utils/classNames'
+
+export const TRANSITIONS = {
+  overlay: {
+    timeout: 150
+  }
+}
+
+export const Tailwind = {
+  multiselect: {
+    root: ({ props }: { props: any }) => ({
+      className: classNames(
+        'inline-flex cursor-pointer select-none text-white',
+        'border border-primary-600 rounded-full py-3 px-4 pr-0 text-sm font-semibold',
+        {
+          'opacity-60 select-none pointer-events-none cursor-default':
+            props.disabled
+        }
+      )
+    }),
+    labelContainer: ({ props }: { props: any }) => ({
+      className: classNames('overflow-hidden flex flex-auto cursor-pointer')
+    }),
+    label: ({ props }: { props: any }) => ({
+      className: classNames(
+        'block overflow-hidden whitespace-nowrap cursor-pointer overflow-ellipsis',
+        'text-white ',
+        'transition duration-200'
+      )
+    }),
+    token: {
+      className: classNames(
+        'bg-gray-300  text-gray-700 rounded-full',
+        'cursor-default inline-flex items-center'
+      )
+    },
+    removeTokenIcon: ({ props }: { props: any }) => ({
+      className: 'ml-2' // Adjust as needed
+    }),
+    trigger: {
+      className: classNames(
+        'flex items-center justify-center shrink-0',
+        'bg-transparent text-white w-12 rounded-tr-lg rounded-br-lg'
+      )
+    },
+    panel: {
+      className: classNames('bg-neutral-700 text-white border-0 shadow-lg')
+    },
+    header: {
+      className: classNames(
+        'border-b border-gray-300 text-gray-700 bg-gray-100 rounded-t-lg',
+        'flex items-center justify-between'
+      )
+    },
+    headerCheckboxContainer: {
+      className: classNames(
+        'inline-flex cursor-pointer select-none align-bottom relative',
+        'mr-2',
+        'w-6 h-6'
+      )
+    },
+    headerCheckbox: {
+      root: ({ props }: { props: any }) => ({
+        className: classNames(
+          'flex items-center justify-center',
+          'border-2 w-6 h-6 text-gray-600 rounded-lg transition-colors duration-200',
+          'hover:border-blue-500 focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)]',
+          {
+            'border-gray-300 bg-white': !props?.checked,
+            'border-blue-500 bg-blue-500': props?.checked
+          }
+        )
+      })
+    },
+    headerCheckboxIcon: ({ props }: { props: any }) => ({
+      className: 'w-4 h-4 transition-all duration-200 text-white text-base'
+    }),
+    closeButton: {
+      className: classNames(
+        'flex items-center justify-center overflow-hidden relative',
+        'w-8 h-8 text-gray-500 border-0 bg-transparent rounded-full transition duration-200 ease-in-out mr-2 last:mr-0',
+        'hover:text-gray-700 hover:border-transparent hover:bg-gray-200',
+        'focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)]'
+      )
+    },
+    closeIcon: ({ props }: { props: any }) => ({
+      className: 'w-4 h-4 inline-block'
+    }),
+    wrapper: {
+      className: classNames(
+        'overflow-auto',
+        'bg-white text-gray-700 border-0 rounded-md shadow-lg'
+      )
+    },
+    list: ({ props }: { props: any }) => ({
+      className: 'py-3 list-none m-0'
+    }),
+    item: ({ context }: { context: any }) => ({
+      className: classNames(
+        'cursor-pointer font-normal overflow-hidden relative whitespace-nowrap',
+        'm-0 p-3 border-0  transition-shadow duration-200 rounded-none',
+        {
+          'text-gray-700 hover:text-gray-700 hover:bg-gray-200':
+            !context || !context.selected,
+          'bg-gray-300 text-gray-700 hover:text-gray-700 hover:bg-gray-200':
+            context && context.focused && !context.selected,
+          'bg-blue-100 text-blue-700':
+            context && context.focused && context.selected,
+          'bg-blue-50 text-blue-700':
+            context && !context.focused && context.selected
+        }
+      )
+    }),
+
+    checkboxContainer: {
+      className: classNames(
+        'inline-flex cursor-pointer select-none align-bottom relative',
+        'mr-2',
+        'w-6 h-6'
+      )
+    },
+    checkbox: ({ context }: { context: any }) => ({
+      className: classNames(
+        'flex items-center justify-center',
+        'border-2 w-6 h-6 text-gray-600 rounded-lg transition-colors duration-200',
+        'hover:border-blue-500 focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)]',
+        {
+          'border-gray-300  bg-white': !context || !context.selected,
+          'border-blue-500 bg-blue-500': context && context.selected
+        }
+      )
+    }),
+    checkboxIcon: ({ props }: { props: any }) => ({
+      className: 'w-4 h-4 transition-all duration-200 text-white text-base'
+    }),
+    itemGroup: {
+      className: classNames(
+        'm-0 p-3 text-gray-800 bg-white font-bold',
+        'cursor-auto'
+      )
+    },
+    filterContainer: ({ props }: { props: any }) => ({
+      className: 'relative' // Adjust as needed
+    }),
+    filterInput: {
+      root: {
+        className: classNames(
+          'pr-7 -mr-7',
+          'w-full',
+          'font-sans text-base text-gray-700 bg-white py-3 px-3 border border-gray-300 transition duration-200 rounded-lg appearance-none',
+          'hover:border-blue-500 focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)]'
+        )
+      }
+    },
+    filterIcon: ({ props }: { props: any }) => ({
+      className: '-mt-2 absolute top-1/2'
+    }),
+    clearIcon: ({ props }: { props: any }) => ({
+      className: 'text-gray-500 right-12 -mt-2 absolute top-1/2'
+    }),
+    transition: TRANSITIONS.overlay
+  }
+}
--- pages/user-stories/index.tsx
+++ pages/user-stories/index.tsx
@@ -2,183 +2,18 @@ import { CirclePlay } from 'lucide-react'
 import { GetStaticProps } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/router'
+import { PrimeReactProvider } from 'primereact/api'
+import { MultiSelect } from 'primereact/multiselect'
 import { useEffect, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
 import Layout from '../../components/Layout'
 import { findOne } from '../../lib/api/strapi'
 import { galaxyOnPage } from '../../lib/galaxy/galaxy'
 import { getCommonProps } from '../../lib/utils/getCommonProps'
 import { UseCaseCategory, UserStoriesPage } from '../../types/userStories'
-import { useRouter } from 'next/router'
-import { useSearchParams } from 'next/navigation'
-import { PrimeReactProvider } from 'primereact/api'
-import { MultiSelect } from 'primereact/multiselect'
-import { classNames } from '../../lib/utils/classNames'
-import Tailwind from 'primereact/passthrough/tailwind'
-
-const TRANSITIONS = {
-  overlay: {
-    timeout: 150,
-    classNames: {
-      enter: 'opacity-0 scale-75',
-      enterActive:
-        'opacity-100 !scale-100 transition-transform transition-opacity duration-150 ease-in',
-      exit: 'opacity-100',
-      exitActive: '!opacity-0 transition-opacity duration-150 ease-linear'
-    }
-  }
-}
-
-const Tailwinda = {
-  multiselect: {
-    root: ({ props }: { props: any }) => ({
-      className: classNames(
-        'inline-flex cursor-pointer select-none',
-        'bg-white dark:bg-gray-900 border border-gray-400 dark:border-blue-900/40  transition-colors duration-200 ease-in-out rounded-md',
-        'w-full md:w-80',
-        {
-          'opacity-60 select-none pointer-events-none cursor-default':
-            props.disabled
-        }
-      )
-    }),
-    labelContainer: 'overflow-hidden flex flex-auto cursor-pointer',
-    label: ({ props }: { props: any }) => ({
-      className: classNames(
-        'block overflow-hidden whitespace-nowrap cursor-pointer overflow-ellipsis',
-        'text-gray-800 dark:text-white/80',
-        'p-3 transition duration-200',
-        {
-          '!p-3':
-            props.display !== 'chip' &&
-            (props.value == null || props.value == undefined),
-          '!py-1.5 px-3': props.display === 'chip' && props.value !== null
-        }
-      )
-    }),
-    token: {
-      className: classNames(
-        'py-1 px-2 mr-2 bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-white/80 rounded-full',
-        'cursor-default inline-flex items-center'
-      )
-    },
-    removeTokenIcon: 'ml-2',
-    trigger: {
-      className: classNames(
-        'flex items-center justify-center shrink-0',
-        'bg-transparent text-gray-600 dark:text-white/70 w-12 rounded-tr-lg rounded-br-lg'
-      )
-    },
-    panel: {
-      className: classNames(
-        'bg-white dark:bg-gray-900 text-gray-700 dark:text-white/80 border-0 rounded-md shadow-lg'
-      )
-    },
-    header: {
-      className: classNames(
-        'p-3 border-b border-gray-300 dark:border-blue-900/40 text-gray-700 dark:text-white/80 bg-gray-100 dark:bg-gray-800 rounded-t-lg',
-        'flex items-center justify-between'
-      )
-    },
-    headerCheckboxContainer: {
-      className: classNames(
-        'inline-flex cursor-pointer select-none align-bottom relative',
-        'mr-2',
-        'w-6 h-6'
-      )
-    },
-    headerCheckbox: {
-      root: ({ props }: { props: any }) => ({
-        className: classNames(
-          'flex items-center justify-center',
-          'border-2 w-6 h-6 text-gray-600 dark:text-white/70 rounded-lg transition-colors duration-200',
-          'hover:border-blue-500 focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)] dark:focus:shadow-[0_0_0_0.2rem_rgba(147,197,253,0.5)]',
-          {
-            'border-gray-300 dark:border-blue-900/40 bg-white dark:bg-gray-900':
-              !props?.checked,
-            'border-blue-500 bg-blue-500': props?.checked
-          }
-        )
-      })
-    },
-    headerCheckboxIcon:
-      'w-4 h-4 transition-all duration-200 text-white text-base',
-    closeButton: {
-      className: classNames(
-        'flex items-center justify-center overflow-hidden relative',
-        'w-8 h-8 text-gray-500 dark:text-white/70 border-0 bg-transparent rounded-full transition duration-200 ease-in-out mr-2 last:mr-0',
-        'hover:text-gray-700 dark:hover:text-white/80 hover:border-transparent hover:bg-gray-200 dark:hover:bg-gray-800/80 ',
-        'focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)] dark:focus:shadow-[0_0_0_0.2rem_rgba(147,197,253,0.5)]'
-      )
-    },
-    closeIcon: 'w-4 h-4 inline-block',
-    wrapper: {
-      className: classNames(
-        'max-h-[200px] overflow-auto',
-        'bg-white text-gray-700 border-0 rounded-md shadow-lg',
-        'dark:bg-gray-900 dark:text-white/80'
-      )
-    },
-    list: 'py-3 list-none m-0',
-    item: ({ context }: { context: any }) => ({
-      className: classNames(
-        'cursor-pointer font-normal overflow-hidden relative whitespace-nowrap',
-        'm-0 p-3 border-0  transition-shadow duration-200 rounded-none',
-        {
-          'text-gray-700 hover:text-gray-700 hover:bg-gray-200 dark:text-white/80 dark:hover:bg-gray-800':
-            !context.focused && !context.selected,
-          'bg-gray-300 text-gray-700 dark:text-white/80 dark:bg-gray-800/90 hover:text-gray-700 hover:bg-gray-200 dark:text-white/80 dark:hover:bg-gray-800':
-            context.focused && !context.selected,
-          'bg-blue-100 text-blue-700 dark:bg-blue-400 dark:text-white/80':
-            context.focused && context.selected,
-          'bg-blue-50 text-blue-700 dark:bg-blue-300 dark:text-white/80':
-            !context.focused && context.selected
-        }
-      )
-    }),
-    checkboxContainer: {
-      className: classNames(
-        'inline-flex cursor-pointer select-none align-bottom relative',
-        'mr-2',
-        'w-6 h-6'
-      )
-    },
-    checkbox: ({ context }: { context: any }) => ({
-      className: classNames(
-        'flex items-center justify-center',
-        'border-2 w-6 h-6 text-gray-600 dark:text-white/80 rounded-lg transition-colors duration-200',
-        'hover:border-blue-500 focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)] dark:focus:shadow-[0_0_0_0.2rem_rgba(147,197,253,0.5)]',
-        {
-          'border-gray-300 dark:border-blue-900/40  bg-white dark:bg-gray-900':
-            !context.selected,
-          'border-blue-500 bg-blue-500': context.selected
-        }
-      )
-    }),
-    checkboxIcon: 'w-4 h-4 transition-all duration-200 text-white text-base',
-    itemGroup: {
-      className: classNames(
-        'm-0 p-3 text-gray-800 bg-white font-bold',
-        'dark:bg-gray-900 dark:text-white/80',
-        'cursor-auto'
-      )
-    },
-    filterContainer: 'relative',
-    filterInput: {
-      root: {
-        className: classNames(
-          'pr-7 -mr-7',
-          'w-full',
-          'font-sans text-base text-gray-700 bg-white py-3 px-3 border border-gray-300 transition duration-200 rounded-lg appearance-none',
-          'dark:bg-gray-900 dark:border-blue-900/40 dark:hover:border-blue-300 dark:text-white/80',
-          'hover:border-blue-500 focus:outline-none focus:outline-offset-0 focus:shadow-[0_0_0_0.2rem_rgba(191,219,254,1)] dark:focus:shadow-[0_0_0_0.2rem_rgba(147,197,253,0.5)]'
-        )
-      }
-    },
-    filterIcon: '-mt-2 absolute top-1/2',
-    clearIcon: 'text-gray-500 right-12 -mt-2 absolute top-1/2',
-    transition: TRANSITIONS.overlay
-  }
-}
+import { Tailwind } from '../../lib/utils/primereact'
 
 export const getStaticProps: GetStaticProps<UserStoriesPage> =
   async function getStaticProps() {
@@ -279,7 +114,7 @@ function CustomerStoriesPage({
     }
   })
 
-  const [selectedCities, setSelectedCities] = useState(null)
+  const [selectedUseCases, setselectedUseCases] = useState(null)
   const [useCases, setUseCases] = useState<UseCaseCategory[]>()
 
   useEffect(() => {
@@ -291,7 +126,12 @@ function CustomerStoriesPage({
       <PrimeReactProvider
         value={{
           unstyled: true,
-          pt: Tailwind
+          pt: Tailwind,
+          ptOptions: {
+            mergeSections: true,
+            mergeProps: true,
+            classNameMergeFunction: twMerge
+          }
         }}>
         <div className='pt-10'>
           <div className='container mx-auto flex max-w-7xl flex-col px-4 md:px-8 2xl:px-0'>
@@ -314,19 +154,20 @@ function CustomerStoriesPage({
                   orderByDate
                     ? 'bg-primary-300 text-black'
                     : 'border-opacity-[0.3] text-white'
-                } rounded-full border border-primary-500 py-2 px-3 text-sm font-semibold text-black`}
+                } rounded-full border border-primary-500 py-3 px-4 text-sm font-semibold text-black`}
                 onClick={toggleOrderByDate}>
                 Latest
               </button>
               <div className=''>
                 <MultiSelect
-                  value={selectedCities}
-                  onChange={(e) => setSelectedCities(e.value)}
+                  value={selectedUseCases}
+                  onChange={(e) => setselectedUseCases(e.value)}
                   options={useCases}
                   optionLabel='name'
                   placeholder='Use Case'
                   maxSelectedLabels={0}
                   panelHeaderTemplate={<></>}
+                  selectedItemsLabel='Use Case ({0})'
                   unstyled
                 />
               </div>
