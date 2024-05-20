15cc54a029fbf025adacc9daaa84e345362152a6
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2024-05-20 21:44:41 +0100

    revert to staging m3ter api

--- createPricingFile.ts
+++ createPricingFile.ts
@@ -29,7 +29,7 @@ function generatePricingFile(pricings: any) {
 async function triggerPricingFile() {
   log('Starting to build pricing file')
 
-  const allPricings = await getPricingsByPlan(config.planId)
+  const allPricings = await getPricingsByPlan(configStaging.planId)
 
   // Generate modified 'acceptableRegions' array for 'gcp' provider
   const modifiedAcceptableRegions = acceptableRegions.map((regionObj) => {
