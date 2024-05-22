dd2a2f16f14c9477d3b3a81d20f6a12ca2797f4c
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2024-05-22 13:49:19 +0100

    styled checkboxes

--- lib/utils/primereact.ts
+++ lib/utils/primereact.ts
@@ -99,7 +99,7 @@ export const Tailwind = {
     item: ({ context }: { context: any }) => ({
       className: classNames(
         'cursor-pointer font-normal overflow-hidden relative whitespace-nowrap text-white flex items-center',
-        'm-0 px-4 py-1.5 border-0 transition-shadow duration-200 rounded-none text-base',
+        'm-0 px-4 py-1.5 border-0 transition-shadow duration-200 rounded-none text-[14px]',
         {
           'text-white hover:bg-neutral-725 hover:text-primary-900':
             !context || context.selected
