26a33c4b6554fc80ba49eb11181266748c18130c
Levi Cole <levi@skape.co>
2024-06-24 09:37:18 +0100

    Fixed word break issue with LinkWithArrow component

--- components/LinkWithArrow/index.tsx
+++ components/LinkWithArrow/index.tsx
@@ -1,5 +1,6 @@
 import Link, { LinkProps } from 'next/link'
 import React from 'react'
+import Nbsp from '../Nbsp'
 interface Props extends LinkProps {
   children: React.ReactNode
   className?: string
@@ -13,8 +14,12 @@ export default function LinkWithArrow({
   return (
     <Link {...props} className={`group/linkWithArrow ${className}`}>
       {children}
-      <span className='inline whitespace-nowrap pl-0.5 transition group-hover/linkWithArrow:translate-x-1/2'>
-        -&gt;
+      <Nbsp />
+      <span className='relative whitespace-nowrap'>
+        <span className='opacity-0'>-&gt;</span>
+        <span className='absolute left-0 top-1/2 block -translate-y-1/2 transition-all group-hover/linkWithArrow:indent-1'>
+          -&gt;
+        </span>
       </span>
     </Link>
   )
