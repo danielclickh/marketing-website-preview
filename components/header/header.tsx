4328d8a03903c7ddb4eed9ca499c24844a915757
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2024-06-24 14:34:32 +0100

    MS typo 😑

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -59,7 +59,7 @@ export default function Header({ header, github: { stars } }: HeaderProps) {
   }
 
   const [headerBanner, setHeaderBanner] = useState(
-    '<a href="/blog/clickhouse-cloud-is-now-generally-available-on-microsoft-azure?loc=eyebrow">ClickHouse Cloud now available on Microsfot Azure</a>'
+    '<a href="/blog/clickhouse-cloud-is-now-generally-available-on-microsoft-azure?loc=eyebrow">ClickHouse Cloud now available on Microsoft Azure</a>'
   )
 
   useEffect(() => {
