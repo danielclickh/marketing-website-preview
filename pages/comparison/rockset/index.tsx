f93a4b9169d6beeaae83d4f066e07043d6518310
Gareth Jones <gareth@clickhouse.com>
2024-06-24 16:49:36 -0400

    Adding images and content

--- pages/comparison/rockset/index.tsx
+++ pages/comparison/rockset/index.tsx
@@ -1,13 +1,22 @@
 import { GetStaticProps } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
 import { useRef, useState } from 'react'
+import GetStartedFree from '../../../components/GetStartedFree'
 import GrowingCommunity from '../../../components/GrowingCommunity'
 import HRSeparator from '../../../components/HRSeparator'
 import Layout from '../../../components/Layout'
+import Markdown from '../../../components/Markdown'
 import MarketoForm from '../../../components/MarketoForm'
+import AccordionComponent from '../../../components/RealTimeDiagram/Accordion'
+import Feature from '../../../components/RealTimeDiagram/feature-check'
+import features from '../../use-cases/real-time-analytics/features.json'
+import callouts from '../../use-cases/real-time-analytics/callouts.json'
+import { SuiButton, SuiTitle } from '../../../components/sui'
 import { findOne } from '../../../lib/api/strapi'
 import { getCommonProps } from '../../../lib/utils/getCommonProps'
 import { ContactProps } from '../../../types/contact'
+import { Button } from '@clickhouse/click-ui'
 
 interface DriftAPI {
   startInteraction: (options: { interactionId: number }) => void
@@ -63,6 +72,7 @@ export default function Page({
   const formSuccessRef = useRef<HTMLDivElement | null>(null)
   const [formSuccess, setFormSuccess] = useState(false)
   const [formLoaded, setFormLoaded] = useState(false)
+  const [showForm, setShowForm] = useState(false)
 
   return (
     <>
@@ -77,19 +87,43 @@ export default function Page({
                     <span className='text-[#E5C3FF]'>[Rockset]</span> to
                     ClickHouse
                   </h1>
-                  <h2 className='mb-6 font-basier text-2xl font-semibold leading-tight text-neutral-200'>
-                    At ClickHouse, we're not going anywhere. Looking for help
-                    with your migration? Get in touch!
+                  <h2 className='mb-6 font-basier text-xl font-medium leading-tight text-neutral-200'>
+                    [Rockset] is being deprecated. Are you facing the daunting
+                    task of needing to migrate your production workloads before
+                    a rapidly approaching cut-off date? We’re here to help.
                   </h2>
                   <div className='mx-auto mb-6 max-w-2xl text-neutral-200'>
                     <div className='prose prose-neutral'>
-                      <p>Text</p>
+                      <p>
+                        ClickHouse and Rocksest are both used to power real-time
+                        analytics workloads and customer-facing applications.
+                        But ClickHouse outshines Rockset in scalability,
+                        ingestion throughput, query performance,
+                        cost-efficiency, and much more.
+                      </p>
+
+                      <p>
+                        Contact us today to learn more about how ClickHouse
+                        compares to Rockset, and how our dedicated experts can
+                        help you transition to ClickHouse.
+                      </p>
                     </div>
+                  </div>
+                  <div className='flex pt-8'>
+                    <SuiButton
+                      type='primary'
+                      onClick={() => setShowForm(!showForm)}>
+                      Get personalized support
+                    </SuiButton>
+
+                    <SuiButton type='empty' color='primary'>
+                      Start a 30-day free trial
+                    </SuiButton>
                   </div>
                 </div>
               </div>
-              <div className='ml-auto w-full lg:max-w-lg'>
-                <div className='w-full rounded-lg border border-neutral-800 bg-neutral-900 p-8 shadow-card duration-300 ease-in-out'>
+              {showForm ? (
+                <div className='rounded-lg border border-neutral-800 bg-neutral-900 p-8 shadow-card duration-300 ease-in-out lg:max-w-lg'>
                   {!formLoaded && (
                     <div className='text-center'>Loading form...</div>
                   )}
@@ -127,12 +161,178 @@ export default function Page({
                     </div>
                   )}
                 </div>
-              </div>
+              ) : (
+                <div className='ml-auto w-full lg:max-w-lg'>
+                  <Image
+                    src='/images/comparisons/rockset-migration-2.svg'
+                    width='485'
+                    height='448'
+                    alt='clickhouse to rockset migration'
+                  />
+                </div>
+              )}
             </div>
           </div>
         </div>
         <HRSeparator className='my-24' />
-        <GrowingCommunity />
+        <div className='bg-neutral-725 pb-24'>
+          <div className='relative mx-auto pt-12  md:px-0 md:pt-24'>
+            <div className='mx-auto max-w-7xl'>
+              <div className='flex w-full flex-col items-center pt-6 pb-12'>
+                <Image
+                  src='/images/use-cases/logging/icon-how.svg'
+                  alt='System overview'
+                  width={72}
+                  height={73}
+                />
+                <SuiTitle type='h2' className='mt-8 text-center'>
+                  Real-time Applications and Dashboards
+                </SuiTitle>
+                <p className='mx-auto mb-12 max-w-3xl px-9 pt-6 text-center text-sm'>
+                  ClickHouse is used across industries to power systems and
+                  applications where real-time analysis, evaluation, and
+                  querying are critical.
+                </p>
+                <div className='mb-12 flex w-full flex-col gap-6 px-8 lg:flex-row lg:px-6 xl:px-0'>
+                  {[10, 20, 30].map((section) => (
+                    <div
+                      key={section}
+                      className='items-center rounded-md border border-neutral-0/30 bg-[rgba(62,62,62,1)] py-4 px-6 text-left lg:w-1/3'>
+                      {features
+                        .filter((feature) => feature.section === section)
+                        .map((feature) => (
+                          <Feature key={feature.id} feature={feature} />
+                        ))}
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <AccordionComponent />
+              <div className='mx-auto max-w-5xl px-4 xl:px-0'>
+                <div className='grid justify-between gap-20 pt-20 lg:grid-cols-2'>
+                  {callouts.map((feature) => (
+                    <div key={feature.id} className='px-3'>
+                      <Image
+                        src={feature.icon}
+                        alt={feature.title}
+                        width={32}
+                        height={32}
+                        className='mb-4'
+                      />
+                      <h3 className='mb-4 text-lg font-bold'>
+                        {feature.title}
+                      </h3>
+                      <div className='rich_content text-base text-neutral-200'>
+                        <Markdown children={feature.content} />
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='bg-shadow-element-right yellow-shadow '>
+          <div className='section-container mb-24 flex w-full pt-24 text-neutral-0 md:px-8 2xl:px-0 '>
+            <div className='mx-auto flex w-full flex-col justify-center rounded-xl border border-neutral-700/80 bg-neutral-900/50 bg-right bg-no-repeat py-16 px-4 xl:px-24'>
+              <div className='flex flex-col text-center'>
+                <SuiTitle type='h2' color='white'>
+                  Supporting{' '}
+                  <span className='tilted tilted-yellow'>
+                    <span className='tilted-content'>references</span>
+                  </span>{' '}
+                </SuiTitle>
+                <div className='mx-auto mb-8 mt-6 max-w-2xl text-center text-neutral-300'>
+                  For detailed guides about how to get started with ClickHouse
+                  for real-time analytics workloads, follow along in our blog:
+                </div>
+                <div className='bg-neutral-725 p-8'>
+                  <ol className='list-decimal space-y-2 text-left	text-primary-300'>
+                    <li>
+                      <Link
+                        href='/blog/asynchronous-data-inserts-in-clickhouse'
+                        className='text-primary-300'>
+                        Asynchronous Data Inserts in ClickHouse
+                      </Link>
+                    </li>
+                    <li>
+                      <a
+                        href='/blog/real-time-event-streaming-with-confluent-cloud-clickhouse-and-clickpipes'
+                        className='text-primary-300'>
+                        Real-time event streaming with ClickHouse, Confluent
+                        Cloud and ClickPipes
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href='/blog/building-real-time-applications-with-clickhouse-and-hex-notebook-keeper-engine'
+                        className='text-primary-300'>
+                        Adding Real-Time Analytics to a Supabase Application
+                        With ClickHouse
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href='https://python.langchain.com/docs/integrations/vectorstores/clickhouse'
+                        className='text-primary-300'>
+                        Building Real-time Analytics Apps with ClickHouse and
+                        Hex
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href='/blog/using-materialized-views-in-clickhouse'
+                        className='text-primary-300'>
+                        Using Materialized Views in ClickHouse
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href='/blog/working-with-time-series-data-and-functions-ClickHouse'
+                        className='text-primary-300'>
+                        Working with Time Series Data in ClickHouse
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href='/blog/clickhouse-postgresql-change-data-capture-cdc-part-1'
+                        className='text-primary-300'>
+                        Change Data Capture (CDC) with PostgreSQL and ClickHouse
+                        - Part 1
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href='/blog/clickhouse-postgresql-change-data-capture-cdc-part-2'
+                        className='text-primary-300'>
+                        Change Data Capture (CDC) with PostgreSQL and ClickHouse
+                        - Part 2
+                      </a>
+                    </li>{' '}
+                    <li>
+                      <a
+                        href='/blog/how-cloudflare-processes-hundreds-of-millions-of-rows-per-second-with-clickhouse'
+                        className='text-primary-300'>
+                        How Cloudflare Processes Hundreds of Millions of Rows
+                        per Second with ClickHouse
+                      </a>
+                    </li>
+                  </ol>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='section-container my-20 text-neutral-0 md:px-8 xl:my-44 2xl:px-0'>
+            <GetStartedFree
+              href='https://clickhouse.cloud/signUp?loc=real-time-use-case-getstarted-footer'
+              textBefore='Get started with ClickHouse'
+              textSlanted='Cloud'
+              textAfter='for free'
+            />
+          </div>
+        </div>
       </Layout>
     </>
   )
