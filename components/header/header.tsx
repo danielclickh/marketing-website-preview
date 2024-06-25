ba8df48e9db85accdea3531e8ba840b58a835577
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2024-06-25 11:01:06 +0100

    updated eyebrow link

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -59,7 +59,7 @@ export default function Header({ header, github: { stars } }: HeaderProps) {
   }
 
   const [headerBanner, setHeaderBanner] = useState(
-    '<a href="/blog/clickhouse-cloud-is-now-generally-available-on-microsoft-azure?loc=eyebrow">ClickHouse Cloud now available on Microsoft Azure</a>'
+    '<a href="/blog/clickhouse-cloud-launched-microsoft-launched?loc=eyebrow">ClickHouse Cloud now available on Microsoft Azure</a>'
   )
 
   useEffect(() => {
