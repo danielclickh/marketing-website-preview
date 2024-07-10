5c5792c13e6af215a9171c0dee2e49fe20fc6686
Levi Cole <levi@skape.co>
2024-07-10 10:23:41 +0100

    Improved hover for new integrations button style

--- components/Navigation/sub-navs/NavigationSubNavProducts.tsx
+++ components/Navigation/sub-navs/NavigationSubNavProducts.tsx
@@ -98,7 +98,7 @@ export default function NavigationSubNavProducts() {
         onClick={() =>
           galaxyOnClick('topNav.productMenu.integrationsHighlightSelect')
         }
-        className='hidden w-full rounded-b-lg border-t border-white/5 px-4 py-3 text-center text-sm font-medium md-mid:block'>
+        className='hidden w-full rounded-b-lg border-t border-white/5 px-4 py-3 text-center text-sm font-medium transition-colors hover:bg-neutral-700/25 hover:text-primary-300 md-mid:block'>
         View our integrations
       </LinkWithArrow>
     </>
