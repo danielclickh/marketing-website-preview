23d45da63c5accd1f4a5b4c7a88b586b101ae537
Levi Cole <levi@skape.co>
2024-07-23 10:43:09 +0100

    Initial progress on hero, added table content and trusted by logos

--- pages/comparison/bigquery/index.tsx
+++ pages/comparison/bigquery/index.tsx
@@ -1,11 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { CUIButton } from '../../../components/ClickUI'
 import ComparisonTable from '../../../components/ComparisonTable'
-import { findAll } from '../../../lib/api/strapi'
+import HomepageSectionTrustedByAlt from '../../../components/HomepageSectionTrustedByAlt'
+import { SuiText, SuiTitle } from '../../../components/sui'
+import { findAll, findOne } from '../../../lib/api/strapi'
 import { galaxyOnPage } from '../../../lib/galaxy/galaxy'
 import { getCommonProps } from '../../../lib/utils/getCommonProps'
 import { REVALIDATE_SECONDS } from '../../../lib/utils/revalidationConfig'
 import { ComparisonPage, ComparisonProps } from '../../../types/comparisons'
 import Layout from '../../../components/Layout'
+import { HomepageCustomerStories } from '../../../types/homepage'
+import logos from './logos.png'
+
+export interface BigQueryPageProps extends ComparisonProps {
+  customerStories: HomepageCustomerStories
+}
 
 export async function getStaticProps() {
   const { data }: { data: ComparisonPage[] } = await findAll('comparisons', {
@@ -24,13 +35,23 @@ export async function getStaticProps() {
     }
   }
 
+  const { customerStories } = await findOne('homepage', {
+    populate: [
+      'customerStories',
+      'customerStories.*',
+      'customerStories.logos.*',
+      'customerStories.logos.darkLogoPng'
+    ]
+  })
+
   const comparison = data[0]
 
   const seo = comparison.seo
   if (seo) seo.path = `/comparison/${comparison.slug}`
 
-  const props: ComparisonProps = {
+  const props: BigQueryPageProps = {
     comparison,
+    customerStories,
     seo,
     ...(await getCommonProps())
   }
@@ -85,29 +106,130 @@ export default function BigQueryPage({
   footerData,
   headerData,
   seo,
-  comparison
-}: ComparisonProps) {
+  comparison,
+  customerStories
+}: BigQueryPageProps) {
   galaxyOnPage(`${comparison.slug}ComparisonPage`)
   return (
     <Layout footerData={footerData} seo={seo} headerData={headerData}>
-      <div className='mx-auto max-w-[1010px]'>
+      <div className='container mx-auto my-16 flex max-w-7xl items-center px-8 2xl:px-0'>
+        <div className='grid max-w-[800px] grid-cols-1 gap-6'>
+          <div>
+            <span className='inline-block rounded-full border border-primary-500 bg-primary-700 px-4 py-1 text-xs text-primary-300'>
+              Comparisons
+            </span>
+          </div>
+          <SuiTitle type='h1' weight='bold'>
+            ClickHouse <span className='text-primary-300'>vs</span> BigQuery
+          </SuiTitle>
+          <SuiText className='text-xl'>
+            BigQuery handles ad-hoc queries and smaller data volumes
+            effectively, but scaling turns cost and performance management into
+            a significant challenge. Read more below to learn about how
+            ClickHouse and BigQuery compare in cost, performance, and with
+            supported features.
+          </SuiText>
+          <div className='mt-6 flex max-w-[523px] gap-4'>
+            <CUIButton
+              type='primary'
+              size='lg'
+              weight='semibold'
+              href='#'
+              linkClass='flex-1 w-full'
+              className='w-full'>
+              Get personalized support
+            </CUIButton>
+            <CUIButton
+              type='secondary'
+              size='lg'
+              weight='semibold'
+              href='#'
+              linkClass='flex-1 w-full'
+              className='w-full'>
+              Start a free 30-day trial
+            </CUIButton>
+          </div>
+          <SuiText className='text-sm'>
+            Read our comprehensive guide about{' '}
+            <Link href='#' className='text-primary-300'>
+              migrating from ClickHouse to BigQuery
+            </Link>
+          </SuiText>
+        </div>
+        <Image
+          src={logos}
+          alt='ClickHouse vs BigQuery'
+          width={240}
+          height={245}
+          className='mx-auto'
+        />
+      </div>
+      <div className='container mx-auto my-16 max-w-7xl px-8 2xl:px-0'>
         <ComparisonTable
           columns={[
-            { heading: 'ClickHouse', rowIcon: <YesIcon />, highlight: true },
-            { heading: 'BigQuery', rowIcon: <NoIcon /> }
+            {
+              heading: 'ClickHouse',
+              width: '45%',
+              rowIcon: <YesIcon />,
+              highlight: true
+            },
+            {
+              heading: 'BigQuery',
+              width: '35%',
+              rowIcon: <NoIcon />
+            }
           ]}
           rows={[
             {
-              heading: 'Line 1',
-              values: ['Yes', 'No']
+              heading: 'Fast and efficient',
+              values: [
+                'Up to **95% faster** querying speeds and 60% less storage space required.',
+                'Slower querying speeds and requires more storage.'
+              ]
             },
             {
-              heading: 'Line 2',
-              values: ['Yes', 'Maybe']
+              heading: 'Cost-effective',
+              values: [
+                'Up to **100x** more cost-effective.',
+                'More costly for BigQuery for analytics workloads.'
+              ]
+            },
+            {
+              heading: 'Modern SQL',
+              values: [
+                'Standard SQL enhanced with numerous **extensions and improvements** (e.g. lambda functions and higher-order functions), that make analytical tasks very user-friendly.',
+                'Support for only standard SQL can make analytics more complex.'
+              ]
+            },
+            {
+              heading: 'Easy data analytics',
+              values: [
+                '**150+ pre-built aggregation functions** plus powerful aggregation combinators, fully vectorized and parallelized.\n\n**1300+ data processing functions** for domains like mathematics, geo, machine learning, time series, and more.',
+                'Requires writing more complex SQL due to its limited set of aggregate and regular data processing functions.'
+              ]
+            },
+            {
+              heading: 'Rich data type support',
+              values: [
+                'Advanced data types like JSON, maps, and arrays plus over **80 array functions** for modeling and solving a wide range of problems simply and intuitively.',
+                'Support for limited number of data types including only 8 array functions.'
+              ]
+            },
+            {
+              heading: 'World class\ninteroperability',
+              values: [
+                'Native support fo reading data in over **90 file formats** from most data sources which makes it easy to analyze data regardless of its shape and location. ',
+                'Limited interoperability. Supports only 5 file formats and 19 data sources.'
+              ]
             }
           ]}
         />
       </div>
+      <HomepageSectionTrustedByAlt
+        className='!my-24'
+        heading='Trusted by'
+        customerStories={customerStories}
+      />
     </Layout>
   )
 }
