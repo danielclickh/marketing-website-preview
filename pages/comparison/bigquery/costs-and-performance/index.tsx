c956cf9340bb878908b110a459f038a5dec9fe11
Levi Cole <levi@skape.co>
2024-07-24 22:01:57 +0100

    Missed file from last commit

--- pages/comparison/bigquery/costs-and-performance/index.tsx
+++ pages/comparison/bigquery/costs-and-performance/index.tsx
@@ -1,10 +1,12 @@
 import Image, { ImageProps } from 'next/image'
-import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
 import React, { useRef, useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import type { Swiper as SwiperClass } from 'swiper/types'
 import { CUIButton, CUICard } from '../../../../components/ClickUI'
 import Footer from '../../../../components/Footer'
 import HomepageSectionTrustedByAlt from '../../../../components/HomepageSectionTrustedByAlt'
+import Markdown from '../../../../components/Markdown'
 import MarketoForm from '../../../../components/MarketoForm'
 import SeoContainer from '../../../../components/SeoContainer'
 import { SuiText, SuiTitle } from '../../../../components/sui'
@@ -16,19 +18,23 @@ import { REVALIDATE_SECONDS } from '../../../../lib/utils/revalidationConfig'
 import logoFull from '../../../../public/logo-full.svg'
 import { ComparisonPage, ComparisonProps } from '../../../../types/comparisons'
 import { HomepageCustomerStories } from '../../../../types/homepage'
+import iconQuote from '../icon-quote.svg'
 import styles from './styles.module.scss'
 import bgArrows from './bg-arrows.png'
 import chartCostsQuering from './chart-costs-querying.svg'
 import chartCostsStoring from './chart-costs-storing.svg'
 import chartCostsLoading from './chart-costs-loading.svg'
 import chartCostsQueryingSpeed from './chart-costs-querying-speed.svg'
-
 import chartPerformanceTableScan from './chart-performance-table-scan.svg'
 import chartPerformanceIndexSupport from './chart-performance-index-support.svg'
-
+import chartPerformanceQuerying from './chart-performance-querying.svg'
+import chartPerformanceStoring from './chart-performance-storing.svg'
 import logoGcs from './logo-gcs.svg'
 import logoAws from './logo-aws.svg'
 import logoAzure from './logo-azure.svg'
+import logoAdevinta from './logo-adevinta.svg'
+import logoBlock from './logo-block.png'
+import logoPerfect from './logo-perfect.svg'
 
 export interface BigQueryCostsAndPerformancePageProps extends ComparisonProps {
   customerStories: HomepageCustomerStories
@@ -138,12 +144,13 @@ export default function BigQueryCostsAndPerformancePage({
           <>
             <HeroCosts onSupportClick={() => setIsModalOpen(true)} />
             <IntroGraphsCosts />
+            <QuotesCosts />
             <div className='bg-neutral-750 py-16 lg:py-24'>
               <div className='section-container flex flex-col gap-16 lg:flex-row lg:items-center lg:justify-between lg:gap-10'>
                 <div className='space-y-4 border-l-4 border-primary-300 pl-6 lg:max-w-[615px]'>
                   <SuiTitle type='h3' className='text-2xl'>
                     Juspay{' '}
-                    <span className='tilted tilted-yellow'>
+                    <span className='tilted tilted-sm tilted-yellow'>
                       <span className='tilted-content'>cut costs by 90%</span>
                     </span>{' '}
                     after <br /> migrating to ClickHouse.
@@ -200,6 +207,62 @@ export default function BigQueryCostsAndPerformancePage({
           <>
             <HeroPerformance onSupportClick={() => setIsModalOpen(true)} />
             <IntroGraphsPerformance />
+            <QuotesPerformance />
+            <div className='bg-neutral-750 py-16 lg:py-24'>
+              <div className='section-container flex flex-col gap-16 lg:flex-row lg:items-center lg:justify-between lg:gap-10'>
+                <div className='space-y-4 border-l-4 border-primary-300 pl-6 lg:max-w-[615px]'>
+                  <SuiTitle type='h3' className='text-2xl'>
+                    ClickHouse is purpose-built to power your most
+                    data-intensive applications
+                  </SuiTitle>
+                  <SuiText className='opacity-80'>
+                    BigQuery is a traditional data warehouse that’s optimized
+                    for ad-hoc and infrequent queries. Their pricing model -
+                    which charges for BigQuery “slots” - is calculated based on
+                    the amount of data scanned to perform a query. This can
+                    become exorbitantly expensive for analytics workloads,
+                    particularly where applications invoke queries and
+                    concurrency is high.
+                  </SuiText>
+                </div>
+                <Image
+                  src={chartPerformanceStoring}
+                  alt='Storing 1 billion rows'
+                  width={501}
+                  height={346}
+                  className='order-first mx-auto lg:order-last lg:mx-0'
+                />
+              </div>
+            </div>
+            <div className='my-16 lg:my-24'>
+              <div className='section-container flex flex-col gap-16 lg:flex-row lg:items-center lg:justify-between lg:gap-10'>
+                <div className='space-y-4 border-l-4 border-primary-300 pl-6 lg:max-w-[615px]'>
+                  <SuiTitle type='h3' className='text-2xl'>
+                    Juspay{' '}
+                    <span className='tilted tilted-sm tilted-yellow'>
+                      <span className='tilted-content'>cut costs by 90%</span>
+                    </span>{' '}
+                    after <br /> migrating to ClickHouse.
+                  </SuiTitle>
+                  <SuiText className='opacity-80'>
+                    BigQuery is a traditional data warehouse that’s optimized
+                    for ad-hoc and infrequent queries. Their pricing model -
+                    which charges for BigQuery “slots” - is calculated based on
+                    the amount of data scanned to perform a query. This can
+                    become exorbitantly expensive for analytics workloads,
+                    particularly where applications invoke queries and
+                    concurrency is high.
+                  </SuiText>
+                </div>
+                <Image
+                  src={chartPerformanceQuerying}
+                  alt='Querying 1 billion rows'
+                  width={501}
+                  height={326}
+                  className='order-first mx-auto lg:mx-0'
+                />
+              </div>
+            </div>
           </>
         )}
 
@@ -220,7 +283,7 @@ export default function BigQueryCostsAndPerformancePage({
                 our community of thousands more!
               </SuiText>
             </div>
-            <div className='order-first flex gap-6 lg:order-last'>
+            <div className='order-first flex gap-6 lg:order-last  '>
               <CUICard className='w-full max-w-40 p-5'>
                 <Image
                   src={logoGcs}
@@ -576,5 +639,189 @@ function IntroGraphsPerformance() {
         particularly where applications invoke queries and concurrency is high.
       </SuiText>
     </IntroCharts>
+  )
+}
+
+type QuotesProps = {
+  quotes: Array<{
+    quote: string | React.ReactNode
+    logo?: Omit<ImageProps, 'className'>
+  }>
+}
+
+function Quotes({ quotes }: QuotesProps) {
+  const [swiperInstance, setSwiperInstance] = useState<null | SwiperClass>(null)
+
+  return (
+    <div className='group/quotes relative mb-12 mt-24 xl:my-24'>
+      <div className='section-container relative z-0'>
+        <Swiper
+          onInit={(instance: SwiperClass) => setSwiperInstance(instance)}
+          spaceBetween={32}
+          breakpoints={{
+            1024: {
+              slidesPerView: 2
+            }
+          }}
+          loop={true}
+          className={`sm:!-mx-8 sm:!px-8 ${styles.quotesMask}`}>
+          {quotes.map(({ quote, logo }, index) => {
+            const quoteIsString = typeof quote === 'string'
+            return (
+              <SwiperSlide key={index} className='group !h-auto !self-stretch'>
+                <CUICard className='p-6 lg:p-12'>
+                  <div className='flex h-full flex-col'>
+                    <Image
+                      src={iconQuote}
+                      alt='Quote'
+                      width={36}
+                      height={28}
+                      className='mb-6'
+                    />
+                    <div className='grid grid-cols-1 gap-6 text-lg lg:mb-10'>
+                      {quoteIsString && (
+                        <Markdown encloseByDiv={false}>{quote}</Markdown>
+                      )}
+                      {!quoteIsString && quote}
+                    </div>
+                    {logo && <Image {...logo} className='mt-auto' />}
+                  </div>
+                </CUICard>
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      </div>
+      <div className='pointer-events-none z-10 mt-10 flex justify-center gap-10 xl:absolute xl:left-0 xl:right-0 xl:top-1/2 xl:mt-0 xl:-translate-y-1/2 xl:justify-between xl:px-4 2xl:px-12'>
+        <button
+          className='pointer-events-auto opacity-40 transition-opacity hover:!opacity-90 group-hover/quotes:opacity-70'
+          onClick={() => swiperInstance?.slidePrev()}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='14'
+            height='24'
+            fill='none'
+            viewBox='0 0 14 24'>
+            <path
+              fill='#fff'
+              d='M13.06 20.94a1.5 1.5 0 1 1-2.12 2.12l-10-10a1.5 1.5 0 0 1 0-2.12l10-10a1.5 1.5 0 0 1 2.12 2.12L4.13 12l8.93 8.94Z'
+            />
+          </svg>
+        </button>
+        <button
+          className='pointer-events-auto opacity-40 transition-opacity hover:!opacity-90 group-hover/quotes:opacity-70'
+          onClick={() => swiperInstance?.slideNext()}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='14'
+            height='24'
+            fill='none'
+            viewBox='0 0 14 24'>
+            <path
+              fill='#fff'
+              d='m13.06 13.06-10 10a1.5 1.5 0 0 1-2.12-2.12L9.87 12 .94 3.06A1.5 1.5 0 0 1 3.06.94l10 10a1.5 1.5 0 0 1 0 2.12Z'
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+function QuotesCosts() {
+  return (
+    <Quotes
+      quotes={[
+        {
+          quote:
+            'We needed a solution that could scale, but also provide end-user facing analytics capabilities with low latency and high throughput.”',
+          logo: {
+            src: logoAdevinta,
+            alt: 'Adevinta',
+            width: 143,
+            height: 33
+          }
+        },
+        {
+          quote:
+            'We needed a solution that could scale, but also provide end-user facing analytics capabilities with low latency and high throughput.',
+          logo: {
+            src: logoBlock,
+            alt: 'Block',
+            width: 200,
+            height: 40
+          }
+        },
+        {
+          quote:
+            'We needed a solution that could scale, but also provide end-user facing analytics capabilities with low latency and high throughput.”',
+          logo: {
+            src: logoAdevinta,
+            alt: 'Adevinta',
+            width: 143,
+            height: 33
+          }
+        },
+        {
+          quote:
+            'We needed a solution that could scale, but also provide end-user facing analytics capabilities with low latency and high throughput.',
+          logo: {
+            src: logoBlock,
+            alt: 'Block',
+            width: 200,
+            height: 40
+          }
+        }
+      ]}
+    />
+  )
+}
+
+function QuotesPerformance() {
+  return (
+    <Quotes
+      quotes={[
+        {
+          quote:
+            'In comparison, BigQuery was 2x more expensive due to its pricing model that charges based on bytes scanned”',
+          logo: {
+            src: logoAdevinta,
+            alt: 'Adevinta',
+            width: 143,
+            height: 33
+          }
+        },
+        {
+          quote:
+            'We have saved costs, savings not to be sniffed at, but that was not the driving factor. This was a qualitative step. We just could not do the things we wanted until we had ClickHouse and that is why we’re so excited about it.”',
+          logo: {
+            src: logoPerfect,
+            alt: 'Perfect',
+            width: 212,
+            height: 59
+          }
+        },
+        {
+          quote:
+            'In comparison, BigQuery was 2x more expensive due to its pricing model that charges based on bytes scanned”',
+          logo: {
+            src: logoAdevinta,
+            alt: 'Adevinta',
+            width: 143,
+            height: 33
+          }
+        },
+        {
+          quote:
+            'We have saved costs, savings not to be sniffed at, but that was not the driving factor. This was a qualitative step. We just could not do the things we wanted until we had ClickHouse and that is why we’re so excited about it.”',
+          logo: {
+            src: logoPerfect,
+            alt: 'Perfect',
+            width: 212,
+            height: 59
+          }
+        }
+      ]}
+    />
   )
 }
