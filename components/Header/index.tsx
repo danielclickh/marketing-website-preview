d8ec187065cbc9ce70d134a9e20c32262b82a056
Levi Cole <levi@skape.co>
2024-07-29 14:47:06 +0100

    Updated eyebrow text

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -57,8 +57,7 @@ export default function Header({ github: { stars } }: HeaderProps) {
           <LinkWithArrow
             href='/blog/clickhouse-welcomes-peerdb-adding-the-fastest-postgres-cdc-to-the-fastest-olap-database?loc=eyebrow'
             className='relative z-50 block w-full bg-primary-300 px-4 py-1 text-center text-sm font-medium text-primary-900'>
-            The fastest Postgres CDC with the fastest OLAP - ClickHouse joins
-            force with Peer DB
+            Ingest your Postgres data in minutes - ClickHouse joins forces with PeerDB
           </LinkWithArrow>
         )}
 
