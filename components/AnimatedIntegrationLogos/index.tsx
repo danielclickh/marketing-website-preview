95adafac6fb4faa946ab8db4a0b7d01cb906e4bc
Levi Cole <levi@skape.co>
2024-07-29 13:01:36 +0100

    Added tile linke

--- components/AnimatedIntegrationLogos/index.tsx
+++ components/AnimatedIntegrationLogos/index.tsx
@@ -26,7 +26,9 @@ export default function AnimatedIntegrationLogos({ play }: { play: boolean }) {
           />
           <SuiText size='xs'>
             Fast, simple and cost effective Postgres replication using PeerDB.{' '}
-            <Link href='#' className='text-primary-300 hover:underline'>
+            <Link
+              href='https://clickhouse.com/blog/clickhouse-welcomes-peerdb-adding-the-fastest-postgres-cdc-to-the-fastest-olap-database'
+              className='text-primary-300 hover:underline'>
               <div className='absolute inset-0 transition-colors hover:bg-white/5' />
               Learn more
             </Link>
