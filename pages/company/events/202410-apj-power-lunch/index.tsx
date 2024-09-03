837bf375c508f43b5f3e0845f553144c37e91fd9
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2024-09-03 11:36:35 +0100

    added separate page and form

---
+++ pages/company/events/202410-apj-power-lunch/index.tsx
@@ -0,0 +1,270 @@
+import { GetStaticProps } from 'next'
+import Link from 'next/link'
+import React, { useRef, useState } from 'react'
+import EventPost from '../../../../components/EventPostList/EventPost'
+import EventsContainerMarketo from '../../../../components/EventsContainer-Marketo'
+import Layout from '../../../../components/Layout'
+import Markdown from '../../../../components/Markdown'
+import { StrapiImage } from '../../../../components/StrapiElements'
+import { SuiText, SuiTitle } from '../../../../components/sui'
+import { findAll } from '../../../../lib/api/strapi'
+import { getCommonProps } from '../../../../lib/utils/getCommonProps'
+import { REVALIDATE_SECONDS } from '../../../../lib/utils/revalidationConfig'
+import { EventProps, EventType } from '../../../../types/events'
+import MarketoForm from '../../../../components/MarketoForm'
+import Image from 'next/image'
+
+export const getStaticProps: GetStaticProps<EventProps> =
+  async function getStaticProps() {
+    const { data } = await findAll('events', {
+      filters: {
+        slug: {
+          $eq: '202410-apj-power-lunch'
+        }
+      },
+      sort: ['localDatetime:DESC'],
+      populate: [
+        'thumbnailPng',
+        'hostedBy',
+        'hostedBy.hosts',
+        'hostedBy.hosts.avatarPng',
+        'agenda',
+        'agenda.items',
+        'location',
+        'darkFeatureImagePng',
+        'lightFeatureImagePng',
+        'form'
+      ]
+    })
+
+    const { data: recentEvents }: { data: Array<EventType> } = await findAll(
+      'events',
+      {
+        filters: {
+          localDatetime: {
+            $gte: new Date().toISOString()
+          },
+          slug: {
+            $notContains: 'clickhouse-fundamentals'
+          }
+        },
+        sort: ['localDatetime:ASC'],
+        populate: [
+          'thumbnailPng',
+          'hostedBy',
+          'hostedBy.hosts',
+          'hostedBy.hosts.avatarPng',
+          'agenda',
+          'agenda.items',
+          'location',
+          'darkFeatureImagePng',
+          'lightFeatureImagePng',
+          'form'
+        ],
+        pagination: { limit: 3 }
+      }
+    )
+
+    const commonProps = await getCommonProps()
+    const page = data[0]
+
+    if (!page) {
+      return {
+        notFound: true,
+        revalidate: REVALIDATE_SECONDS
+      }
+    }
+
+    if (page.eventVideoUrl) {
+      return {
+        props: {},
+        redirect: {
+          destination: page.eventVideoUrl,
+          permanent: false
+        }
+      }
+    }
+    return {
+      props: {
+        ...page,
+        seo: {
+          title: page.title,
+          description: page.shortDescription,
+          image: [data[0].thumbnailPng],
+          type: 'website',
+          siteName: 'ClickHouse',
+          path: `/company/events/clickhouse-fundamentals`
+        },
+        recentEvents,
+        ...commonProps
+      },
+      revalidate: REVALIDATE_SECONDS
+    }
+  }
+
+function EventPage({
+  agenda,
+  hostedBy,
+  category,
+  title,
+  richDescription,
+  form,
+  localDatetime,
+  recordedVimeoUrl,
+  footerData,
+  headerData,
+  recentEvents,
+  lightFeatureImagePng,
+  seo
+}: EventProps) {
+  const formSuccessRef = useRef<HTMLDivElement | null>(null)
+  const [formSuccess, setFormSuccess] = useState(false)
+  const [formLoaded, setFormLoaded] = useState(false)
+  return (
+    <Layout footerData={footerData} seo={seo} headerData={headerData}>
+      <div className='flex flex-col'>
+        <div className='mx-auto flex w-full max-w-7xl flex-col px-4 pb-16 pt-24 sm:px-8 2xl:px-0'>
+          <div className='event-container mx-auto block w-full lg:flex lg:items-start lg:justify-between'>
+            <div className='mb-16 mr-0 flex-auto lg:mb-0 lg:mr-16 lg:max-w-2xl'>
+              <div className='section_metadata mb-20'>
+                <h4 className='mb-2 text-base font-semibold text-primary-300'>
+                  <Link
+                    className='hover:text-primary-400'
+                    href='/company/news-events'>
+                    News &amp; Events
+                  </Link>{' '}
+                  / {category}
+                </h4>
+                <h1
+                  className='mb-8 font-basier text-4xl font-semibold leading-tight md:text-5.5xl
+            '>
+                  {title}
+                </h1>
+                {richDescription && (
+                  <div className='prose prose-neutral'>
+                    <Markdown encloseByDiv={false}>{richDescription}</Markdown>
+                  </div>
+                )}
+              </div>
+              {hostedBy && (
+                <div className='hosted_by mb-16'>
+                  <h3 className='mb-7 text-xl font-bold'>{hostedBy.title}</h3>
+                  <div className='grid grid-cols-1 flex-wrap gap-3 sm:grid-cols-2'>
+                    {hostedBy.hosts.map((host) => (
+                      <div
+                        className='flex gap-5'
+                        key={`${host.name}-${host.role}`}>
+                        {host.avatarPng && (
+                          <StrapiImage
+                            {...host.avatarPng}
+                            alt={host.avatarPng.caption}
+                            width={64}
+                            height={64}
+                            className='h-11 w-11 rounded-full'
+                          />
+                        )}
+                        <div className='flex flex-col'>
+                          <p className='mb-1 text-base font-medium'>
+                            {host.name}
+                          </p>
+                          <p className='flex-auto text-sm font-medium text-neutral-300'>
+                            {host.role}
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+              {agenda && (
+                <div className='agenda'>
+                  <SuiTitle type='h2' className='mb-7'>
+                    {agenda.title}
+                  </SuiTitle>
+                  <div className='agenda_items gap-1'>
+                    {agenda.items.map((agendaItem) => (
+                      <div
+                        className='agenda_item flex items-start'
+                        key={agendaItem.time}>
+                        <SuiText
+                          size='base'
+                          color='secondary'
+                          weight='medium'
+                          className='mb-1 w-16'>
+                          {agendaItem.time}
+                        </SuiText>
+                        <SuiText
+                          size='base'
+                          weight='medium'
+                          className='flex-auto'>
+                          {agendaItem.topic}
+                        </SuiText>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className='ml-auto w-full lg:max-w-lg'>
+              {lightFeatureImagePng && (
+                <Image
+                  src={lightFeatureImagePng.url}
+                  width={512}
+                  height={293}
+                  alt='Featured image'
+                  className='mb-20 hidden h-auto w-full rounded-lg object-cover lg:block'
+                />
+              )}
+              <div className='w-full rounded-lg border border-neutral-800 bg-neutral-900 p-8 shadow-card duration-300 ease-in-out'>
+                {!formLoaded && (
+                  <div className='text-center'>Loading form...</div>
+                )}
+
+                {!formSuccess && (
+                  <MarketoForm
+                    formId='1274'
+                    disclaimer={`By registering, you acknowledge that ClickHouse will process your personal information in accordance with our [Privacy Policy](/legal/privacy-policy).`}
+                    onLoad={() => setFormLoaded(true)}
+                    onValidate={() => {
+                      console.log('hello')
+                    }}
+                    onSuccess={() => {
+                      setFormSuccess(true)
+
+                      // Delay needed to allow the ref to update before scrolling
+                      setTimeout(() => {
+                        formSuccessRef.current?.scrollIntoView({
+                          behavior: 'smooth'
+                        })
+                      }, 10)
+
+                      return false // Stops page from reloading
+                    }}
+                  />
+                )}
+                {formSuccess && (
+                  <div ref={formSuccessRef} className='text-center'>
+                    <h3 className='text-2xl font-bold'>Thank you!</h3>
+                    <p className='mt-2 text-neutral-200'>We'll be in touch.</p>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='bg-shadow-element yellow-shadow align-shadow-right mx-auto mb-40 max-w-7xl px-4 pb-10 sm:px-8 2xl:px-0'>
+        <div className='relative z-20'>
+          <h3 className='mb-10 font-basier text-4xl'>Upcoming events</h3>
+          <div className='grid grid-cols-1 justify-center gap-8 md:grid-cols-2 lg:grid-cols-3'>
+            {recentEvents.map((event: EventType) => (
+              <EventPost key={event.id} {...event} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default EventPage
