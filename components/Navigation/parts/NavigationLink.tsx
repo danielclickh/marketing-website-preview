4651c31622b6fb83e6676a9b9dea96760ab84b57
clickhouse-adrianfraguela <adrian.fraguela@clickhouse.com>
2024-09-13 13:38:57 +0100

    remove prefetch on nav

--- components/Navigation/parts/NavigationLink.tsx
+++ components/Navigation/parts/NavigationLink.tsx
@@ -18,6 +18,7 @@ const NavigationLink = forwardRef<HTMLAnchorElement, NavigationLinkProps>(
       <Link
         ref={ref}
         href={href}
+        prefetch={false}
         {...props}
         className={`flex flex-wrap rounded-lg px-2 py-2.5 text-sm font-medium transition-colors hover:bg-neutral-700/25 hover:text-primary-300 md-mid:inline-flex ${className} ${
           isHovered ? 'bg-neutral-700/25 text-primary-300' : ''
