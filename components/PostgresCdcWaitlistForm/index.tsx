c69158e679913569139453d306f034e10c80d995
Levi Cole <levi@skape.co>
2024-11-20 11:11:01 +0000

    Fixed form success message

--- components/PostgresCdcWaitlistForm/index.tsx
+++ components/PostgresCdcWaitlistForm/index.tsx
@@ -6,33 +6,38 @@ export default function PostgresCdcWaitlistForm() {
   const [formSuccess, setFormSuccess] = useState(false)
   const formSuccessRef = useRef<HTMLDivElement>(null)
   return (
-    <>
-      <MarketoForm
-        formId='1293'
-        onLoad={() => setFormLoaded(true)}
-        onSuccess={() => {
-          setFormSuccess(true)
-          // Delay needed to allow the ref to update before scrolling
-          setTimeout(() => {
-            formSuccessRef.current?.scrollIntoView({
-              behavior: 'smooth'
-            })
-          }, 10)
+    <div className='relative'>
+      <div
+        className={`transition ${
+          formSuccess ? 'pointer-events-none opacity-10 blur-sm' : ''
+        }`}>
+        <MarketoForm
+          formId='1293'
+          onLoad={() => setFormLoaded(true)}
+          onSuccess={() => {
+            setFormSuccess(true)
+            // Delay needed to allow the ref to update before scrolling
+            setTimeout(() => {
+              formSuccessRef.current?.scrollIntoView({
+                behavior: 'smooth'
+              })
+            }, 10)
 
-          return false // Stops page from reloading
-        }}
-      />
+            return false // Stops page from reloading
+          }}
+        />
+      </div>
       {!formLoaded && <div className='text-center'>Loading form...</div>}
       {formSuccess && (
-        <div ref={formSuccessRef}>
-          <h3 className='text-center text-2xl font-bold'>
-            Thank you for your submission!
-          </h3>
-          <p className='mt-2 text-center text-neutral-200'>
-            We will be in touch soon.
-          </p>
+        <div className='absolute inset-0 flex ' ref={formSuccessRef}>
+          <div className='my-auto w-full text-center'>
+            <h3 className='text-2xl font-bold'>
+              Thank you for your submission!
+            </h3>
+            <p className='mt-2 text-neutral-200'>We will be in touch soon.</p>
+          </div>
         </div>
       )}
-    </>
+    </div>
   )
 }
