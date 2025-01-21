f11c80244d14be1fc4d160426cfdcdb4335bdd20
Levi Cole <levi@skape.co>
2025-01-21 17:53:40 +0000

    Fixed plan button alignment in safari

--- components/PricingV2/parts/PlanSelector/index.tsx
+++ components/PricingV2/parts/PlanSelector/index.tsx
@@ -32,7 +32,7 @@ export default function PlanSelector() {
           return (
             <button
               key={index}
-              className={`flex flex-1 flex-col justify-center rounded-[4px] border border-neutral-700 bg-neutral-750 px-3 py-2 text-center text-center text-sm shadow-input transition-colors focus:outline-none ${
+              className={`block flex-1 rounded-[4px] border border-neutral-700 bg-neutral-750 px-3 py-2 text-center text-sm shadow-input transition-colors focus:outline-none ${
                 isActive
                   ? 'border-primary'
                   : 'hover:border-primary-500 hover:bg-neutral-725 hover:bg-opacity-80 hover:shadow-xl'
