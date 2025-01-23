080fe3264a395cb09966637e8ceea47772502da3
Levi Cole <levi@skape.co>
2025-01-23 13:07:34 +0000

    Fixed mobile spacing between form and results

--- components/PricingV2/parts/Estimator/index.tsx
+++ components/PricingV2/parts/Estimator/index.tsx
@@ -15,7 +15,7 @@ export default function Estimator() {
     <div
       id='pricing-calculator' // Used for scrolling into view and sharing
       className='mx-auto max-w-5xl px-4 sm:px-8 xl:px-0'>
-      <div className='flex flex-col lg:-mx-6 lg:flex-row lg:items-start lg:justify-center'>
+      <div className='flex flex-col gap-y-8 lg:-mx-6 lg:flex-row lg:items-start lg:justify-center'>
         {/* Form */}
         <div className='w-full space-y-8 lg:w-1/2 lg:pr-6'>
           <PlanSelector />
