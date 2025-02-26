e651ed4eabc42afa78c6885b51a6e76c46183fb4
Levi Cole <levi@skape.co>
2025-02-26 18:39:01 +0000

    Added stripe button to events form success

---
+++ components/StripeBuyButton/index.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react'
+
+export interface StripeBuyButtonProps {
+  id: `buy_btn_${string}`
+  customerEmail?: string
+}
+
+export default function StripeBuyButton({
+  id,
+  customerEmail
+}: StripeBuyButtonProps) {
+  // Manage external script
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://js.stripe.com/v3/buy-button.js'
+    document.head.appendChild(script)
+    return () => {
+      document.head.removeChild(script)
+    }
+  }, [])
+
+  return (
+    <div>
+      <stripe-buy-button
+        buy-button-id={id}
+        customer-email={customerEmail}
+        publishable-key={
+          process.env.NEXT_PUBLIC_STRIPE_BUTTON_PUBLISHABLE_KEY
+        }></stripe-buy-button>
+    </div>
+  )
+}
