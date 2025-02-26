d531e821038ebefa2a873f62670b1df60acdf05f
Gareth Jones <github@garethdjones.com>
2025-02-25 19:37:18 -0500

    mobile fix for hero banner

--- components/LogoAnnouncementLink/index.tsx
+++ components/LogoAnnouncementLink/index.tsx
@@ -35,7 +35,7 @@ export default function LogoAnnouncementLink({
   return (
     <Link
       {...link}
-      className={`group flex min-h-20 flex-col rounded-[4px] border shadow-sm md:flex-row ${theme.link} ${className}`}>
+      className={`group flex min-h-20 rounded-[4px] border shadow-sm flex-row ${theme.link} ${className}`}>
       <div
         className={`flex flex-shrink-0 flex-grow-0 rounded-l-sm items-center justify-center p-4 md:py-0 ${theme.logo}`}>
         <Image {...logo} alt='logo' />
