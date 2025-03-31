7417fba10b58988bf8db5af3ac1fff002a45db7a
Levi Cole <levi@skape.co>
2025-03-31 16:26:42 +0100

    Typo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -110,7 +110,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             {/* GTM */}
             <GoogleTagManager gtmId={gtmId} />
 
-            {/* Scarf tracking pixel - Explicityly instructed not to be managed through GTM */}
+            {/* Scarf tracking pixel - Explicitly instructed not to be managed through GTM */}
             <img
               referrerPolicy='no-referrer-when-downgrade'
               src='https://static.scarf.sh/a.png?x-pxid=e6377503-591b-4886-9398-e69c7fee0b91'
