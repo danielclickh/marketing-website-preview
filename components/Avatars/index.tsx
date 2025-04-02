92ed2c5995471ecf3c01675ca94201d59e90696c
Levi Cole <levi@skape.co>
2025-04-02 12:36:50 +0100

    Fixed layer order

--- components/Avatars/index.tsx
+++ components/Avatars/index.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react'
 import { StrapiImageType } from '../../lib/api/strapi/types'
 import { StrapiImage } from '../StrapiElements'
 
@@ -8,17 +7,20 @@ export interface AvatarsProps {
 
 export default function Avatars({ avatars }: AvatarsProps) {
   return (
-    <div className='flex -space-x-4'>
+    <div className='flex -space-x-3'>
       {avatars.map((avatar, avatarIndex) => {
         return (
-          <Fragment key={avatarIndex}>
+          <span
+            key={avatarIndex}
+            className='relative'
+            style={{ zIndex: avatars.length - avatarIndex }}>
             <StrapiImage
               {...avatar}
               width={44}
               height={44}
               className='aspect-square !w-14 !h-auto rounded-full border-4 border-neutral-900'
             />
-          </Fragment>
+          </span>
         )
       })}
     </div>
