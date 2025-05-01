04bf9996f8d425764a89d57c5bbcd4f7ef0a84e3
Levi Cole <levi.cole@clickhouse.com>
2025-05-01 09:54:52 +0100

    Removed unwanted filter

--- src/pages/company/events/[slug].tsx
+++ src/pages/company/events/[slug].tsx
@@ -30,9 +30,6 @@ export const getServerSideProps: GetServerSideProps<EventProps> =
           },
           {
             $or: getStagingOnlyFilters()
-          },
-          {
-            $or: getUnlistedFilters()
           }
         ]
       },
