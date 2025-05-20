e6006be2446468791853eec5940558b4120d461d
Levi Cole <levi.cole@clickhouse.com>
2025-05-20 15:24:19 +0100

    Slight page restructure

--- src/pages/government/index.tsx
+++ src/pages/government/index.tsx
@@ -4,8 +4,9 @@ import comparisonRedshift from './assets/comparison-redshift.png'
 import comparisonSnowflake from './assets/comparison-snowflake.png'
 import diagram from './assets/diagram.png'
 import heroGraphic from './assets/hero-graphic.png'
-import { CUICard } from '@/components/ClickUI'
+import { CUIButton, CUICard } from '@/components/ClickUI'
 import FitText from '@/components/FitText'
+import HRSeparator from '@/components/HRSeparator'
 import Layout from '@/components/Layout'
 import MarketoForm from '@/components/MarketoForm'
 import { SuiText, SuiTitle } from '@/components/sui'
@@ -193,113 +194,150 @@ export default function Page({ seo, headerData, footerData }: CommonProps) {
       </section>
 
       {/* Government on AWS */}
-      <section className='my-16 lg:my-24'>
-        <div className='section-container mx-auto'>
-          {/* Intro */}
-          <div className='mx-auto max-w-4xl space-y-6 text-center'>
+      <section className='my-16 overflow-hidden lg:my-24'>
+        <div className='section-container mx-auto flex flex-col gap-16 lg:flex-row lg:items-center lg:gap-10'>
+          {/* Content */}
+          <div className='mx-auto max-w-xl space-y-6 text-center lg:ml-0 lg:pr-12 lg:text-left'>
             <Image
               width={72}
               height={72}
               src='/images/cloud/bring-your-own-cloud/upload-icon.svg'
               alt='Cloud icon'
-              className='mx-auto'
+              className='mx-auto lg:ml-0'
             />
-            <SuiTitle type='h2'>ClickHouse Government on AWS</SuiTitle>
-            <SuiText size='lg' className='text-neutral-200'>
-              Deploy the full power of ClickHouse Cloud on any government
-              network, including FedRAMP Moderate, High, and IL levels 2-6.
-              Benefit from included NIST 800-53 compliance documentation for
-              simplified ATO. Initially available for AWS government users, this
-              offering delivers all ClickHouse Cloud features with enhanced
-              security, including FIPS 140-3 support.
+            <SuiTitle type='h2'>
+              ClickHouse Government <br />
+              self-managed on AWS
+            </SuiTitle>
+            <SuiText className='text-neutral-200'>
+              Deploy the full power of ClickHouse Cloud as a self-managed
+              solution within any government network, including FedRAMP
+              Moderate, High, and IL levels 2-6. Benefit from included NIST
+              800-53 compliance documentation for simplified ATO. Initially
+              available for AWS government users, this offering delivers all
+              ClickHouse Cloud features with enhanced security, including FIPS
+              140-3 support.
             </SuiText>
-            <SuiText size='lg' className='text-neutral-200'>
-              <Link
-                href='#waitlist'
-                className='font-bold text-primary-300 hover:underline'>
-                Join the waitlist
-              </Link>
-            </SuiText>
+            <CUIButton type='primary' className='inline-block'>
+              Join the waitlist
+            </CUIButton>
           </div>
 
-          {/* Comparisons */}
-          <div className='my-12 grid grid-cols-1 gap-4 md:grid-cols-2 lg:my-20 lg:grid-cols-4 2xl:gap-8'>
-            <ComparisonCard
-              logo={
-                <Image
-                  src={comparisonSnowflake}
-                  alt='SnowFlake'
-                  width={171 / 2}
-                  height={164 / 2}
-                />
-              }
-              name='SnowFlake'
-              savings='3-5x'
-              performance='2x'
-              link='/comparison/snowflake?loc=government'
-            />
-            <ComparisonCard
-              logo={
-                <Image
-                  src={comparisonBigQuery}
-                  alt='BigQuery'
-                  width={171 / 2}
-                  height={164 / 2}
-                />
-              }
-              name='BigQuery'
-              savings='10x'
-              performance='10x'
-              link='/comparison/bigquery?loc=government'
-            />
-            <ComparisonCard
-              logo={
-                <Image
-                  src={comparisonPostgres}
-                  alt='Postgres'
-                  width={171 / 2}
-                  height={164 / 2}
-                />
-              }
-              name='Postgres'
-              savings='5x'
-              performance='1000x'
-              link='/comparison/postgresql?loc=government'
-            />
-            <ComparisonCard
-              logo={
-                <Image
-                  src={comparisonRedshift}
-                  alt='Redshift'
-                  width={171 / 2}
-                  height={164 / 2}
-                />
-              }
-              name='Redshift'
-              savings='4x'
-              performance='5x'
-              link='/comparison/redshift?loc=government'
+          {/* Image */}
+          <div className='relative hidden flex-1 md:block lg:min-h-[568px]'>
+            <Image
+              src='/images/cloud/cloud_hero_image.png'
+              alt='ClickHouse Cloud'
+              width={960}
+              height={516}
+              className='mx-auto h-auto w-full max-w-3xl lg:absolute lg:left-0 lg:top-0 lg:mx-0 lg:h-full lg:!w-auto lg:max-w-none'
             />
           </div>
+        </div>
+      </section>
 
-          {/* Footnote */}
-          <div className='mx-auto max-w-4xl space-y-6 text-center'>
-            <SuiText size='lg' className='text-neutral-200'>
-              At a time when the government mission is only growing and budgets
-              are shrinking, ClickHouse Government utilizes object storage and
-              data compression to achieve 3-5x cost reduction while delivering
-              2-10x improved performance over other solutions.
-            </SuiText>
-            <SuiText size='lg' className='text-neutral-200'>
-              For more information, check out our{' '}
-              <Link
-                href='/use-cases?loc=government'
-                className='font-bold text-primary-300 hover:underline'>
-                use cases
-              </Link>{' '}
-              page.
-            </SuiText>
-          </div>
+      <HRSeparator className='!max-w-none' />
+
+      {/* Comparisons */}
+      <section className='section-container my-16 flex flex-col gap-16 lg:my-24 lg:flex-row-reverse lg:items-center lg:justify-between lg:gap-10'>
+        {/* Content */}
+        <div className='mx-auto max-w-xl space-y-6 text-center lg:mr-0 lg:text-left'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='72'
+            height='73'
+            fill='none'
+            className='mx-auto lg:ml-0'>
+            <rect
+              width='70'
+              height='70'
+              x='1'
+              y='1.67'
+              stroke='#FAFF69'
+              stroke-width='2'
+              rx='15'
+            />
+            <g clip-path='url(#a)'>
+              <path
+                fill='#FAFF69'
+                d='M42.79 23.4c1.85-.7 3.84-1 5.82-.88l.19.02a2 2 0 0 1 1.66 1.66l.02.18.02.38a14.05 14.05 0 0 1-4.1 10.4l-.9.9v9.32a2 2 0 0 1-.45 1.27l-.14.15-5.37 5.37a2 2 0 0 1-3.36-1.01l-.02-.14-.87-6.17-7.14-7.14-6.19-.87a2 2 0 0 1-1.12-3.39l5.36-5.36.15-.14a2 2 0 0 1 1.27-.45h9.32l.9-.9.26-.26a14.03 14.03 0 0 1 4.69-2.94ZM27.07 41.27a.75.75 0 0 1 .36 1.46h-.02l-.12.04-.49.21c-.4.2-.96.55-1.52 1.11-1.02 1.02-2.1 2.8-2.26 5.89 3.09-.16 4.87-1.24 5.89-2.26a5.91 5.91 0 0 0 1.36-2.13v-.02a.75.75 0 0 1 1.46.36l-.73-.18.73.18v.01l-.01.02-.02.06a4.06 4.06 0 0 1-.34.85c-.27.52-.7 1.22-1.4 1.91-1.35 1.36-3.63 2.64-7.35 2.72h-.36a.75.75 0 0 1-.75-.75c0-3.94 1.32-6.32 2.72-7.72a7.41 7.41 0 0 1 2.76-1.73l.06-.02h.02l.16.61-.15-.62Zm9.73 3.5.84 6.04.02.07a.5.5 0 0 0 .32.34.5.5 0 0 0 .51-.12l5.36-5.36.07-.08a.5.5 0 0 0 .08-.28v-7.82l-7.2 7.2ZM48.52 24a12.53 12.53 0 0 0-9.39 3.42l-.23.23L29.56 37 36 43.44l9.34-9.34.23-.23a12.55 12.55 0 0 0 3.41-9.4.5.5 0 0 0-.46-.45Zm-20.9 5a.5.5 0 0 0-.28.08l-.08.07-5.36 5.36a.5.5 0 0 0 .28.85l6.05.84 7.21-7.2h-7.82Z'
+              />
+            </g>
+            <defs>
+              <clipPath id='a'>
+                <path fill='#fff' d='M16 16.67h40v40H16z' />
+              </clipPath>
+            </defs>
+          </svg>
+          <SuiTitle type='h2'>
+            Savings and performance gains with ClickHouse Government
+          </SuiTitle>
+          <SuiText className='text-neutral-200'>
+            At a time when the government mission is only growing and budgets
+            are shrinking, ClickHouse Government utilizes object storage and
+            data compression to achieve 3-5x cost reduction while delivering
+            2-10x improved performance over other solutions.
+          </SuiText>
+        </div>
+
+        {/* Grid */}
+        <div className='grid w-full grid-cols-1 gap-4 md:grid-cols-2 md:gap-5 lg:pr-10'>
+          <ComparisonCard
+            logo={
+              <Image
+                src={comparisonSnowflake}
+                alt='SnowFlake'
+                width={171 / 2}
+                height={164 / 2}
+              />
+            }
+            name='SnowFlake'
+            savings='3-5x'
+            performance='2x'
+            link='/comparison/snowflake?loc=government'
+          />
+          <ComparisonCard
+            logo={
+              <Image
+                src={comparisonBigQuery}
+                alt='BigQuery'
+                width={171 / 2}
+                height={164 / 2}
+              />
+            }
+            name='BigQuery'
+            savings='10x'
+            performance='10x'
+            link='/comparison/bigquery?loc=government'
+          />
+          <ComparisonCard
+            logo={
+              <Image
+                src={comparisonPostgres}
+                alt='Postgres'
+                width={171 / 2}
+                height={164 / 2}
+              />
+            }
+            name='Postgres'
+            savings='5x'
+            performance='1000x'
+            link='/comparison/postgresql?loc=government'
+          />
+          <ComparisonCard
+            logo={
+              <Image
+                src={comparisonRedshift}
+                alt='Redshift'
+                width={171 / 2}
+                height={164 / 2}
+              />
+            }
+            name='Redshift'
+            savings='4x'
+            performance='5x'
+            link='/comparison/redshift?loc=government'
+          />
         </div>
       </section>
 
@@ -544,7 +582,7 @@ function ComparisonCard({
           </FitText>
           <FitText
             minFontSize={9}
-            maxFontSize={14}
+            maxFontSize={12}
             className='w-full font-bold uppercase text-[#B3B6BD]'>
             Performance
           </FitText>
