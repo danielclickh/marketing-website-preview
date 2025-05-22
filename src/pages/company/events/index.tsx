3e37fed46b4bf2663a21e2d900472e1ba34538c5
Levi Cole <levi.cole@clickhouse.com>
2025-05-22 16:39:49 +0100

    Added events seo title override

--- src/pages/company/events/index.tsx
+++ src/pages/company/events/index.tsx
@@ -61,6 +61,7 @@ export const getStaticProps: GetStaticProps<EventsPageProps> =
       events
     ])
 
+    seo.title = 'Events - ClickHouse'
     seo.path = '/company/events'
 
     let featuredEvent: EventType | undefined
