24e27d01363bbb755b8124111974cc21e81cce7e
Levi Cole <levi.cole@clickhouse.com>
2025-05-23 17:35:17 +0100

    Lets try that again!

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -77,7 +77,10 @@ export async function getStaticPaths() {
   // We'll pre-render only these paths at build time.
   // { fallback: 'blocking' } will server-render pages
   // on-demand if the path doesn't exist.
-  return { paths: paths.filter((path) => path !== '/ai'), fallback: 'blocking' }
+  return {
+    paths: paths.filter((item) => item.params.slug !== '/ai'),
+    fallback: 'blocking'
+  }
 }
 
 export default function RichContentPage({
