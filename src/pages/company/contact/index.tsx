e6569c8bbd31ea6acf4ab6d8925d0fa4ba76ad0b
Levi Cole <levi.cole@clickhouse.com>
2025-05-27 16:36:12 +0100

    Minor alignment tweak

--- src/pages/company/contact/index.tsx
+++ src/pages/company/contact/index.tsx
@@ -43,7 +43,9 @@ export default function ContactPage({
             <SuiTitle type='h1' className='mb-4'>
               {title}
             </SuiTitle>
-            <div className='max-w-3xl text-neutral-200'>{description}</div>
+            <div className='mx-auto max-w-3xl text-neutral-200'>
+              {description}
+            </div>
           </div>
           <div className='container mx-auto flex max-w-7xl flex-col bg-opacity-10 px-8 pb-8 pt-14 text-center md:bg-no-repeat 2xl:px-0'>
             <div className='w-full space-y-5 self-center text-left md:max-w-screen-sm'>
