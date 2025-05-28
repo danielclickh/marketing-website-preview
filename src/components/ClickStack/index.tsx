c59d2bf7825002e0897bfa3b8afe8ee5faa97088
Levi Cole <levi.cole@clickhouse.com>
2025-05-28 09:54:20 +0100

    Fixed clickstack interactivity areas

--- src/components/ClickStack/index.tsx
+++ src/components/ClickStack/index.tsx
@@ -73,7 +73,7 @@ function Layer({
 }) {
   return (
     <div
-      className={`absolute inset-0 ${styles.layerPerspective} ${className}`}
+      className={`pointer-events-none absolute inset-0 ${styles.layerPerspective} ${className}`}
       style={
         {
           '--color': color
@@ -81,7 +81,7 @@ function Layer({
       }>
       <div
         onClick={onClick}
-        className={`flex items-center justify-center overflow-hidden rounded-[8%] border-2 bg-gradient-to-br from-[rgba(38,38,35,0.9)] to-[rgba(16,16,16,0.9)] backdrop-blur-sm transition duration-300 ${active ? '' : `opacity-15 ${onClick ? 'cursor-pointer hover:opacity-40' : ''}`} ${styles.layer}`}>
+        className={`flex items-center justify-center overflow-hidden rounded-[8%] border-2 bg-gradient-to-br from-[rgba(38,38,35,0.9)] to-[rgba(16,16,16,0.9)] backdrop-blur-sm transition duration-300 ${active ? '' : `opacity-15 ${onClick ? 'pointer-events-auto cursor-pointer hover:opacity-40' : ''}`} ${styles.layer}`}>
         <Image
           src={logo}
           width={180}
