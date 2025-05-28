516382c0ffae1d62e0f3c72904a8888eac14c4de
Levi Cole <levi.cole@clickhouse.com>
2025-05-28 15:35:47 +0100

    Replaced video card image with reusable component

---
+++ src/components/MarketingVideoThumbnail/index.tsx
@@ -0,0 +1,35 @@
+import fallbackTumbnail from './fallback.png'
+import Image from 'next/image'
+import { useState } from 'react'
+
+export interface MarketingVideoThumbnailProps {
+  videoId: string
+}
+
+export default function MarketingVideoThumbnail({
+  videoId
+}: MarketingVideoThumbnailProps) {
+  const [displayFallback, setDisplayFallback] = useState<boolean>(false)
+  let thumbnail = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`
+  return (
+    <div className='relative aspect-[774/420] bg-primary-300'>
+      <Image
+        src={thumbnail}
+        alt={`YouTube Video: ${videoId}`}
+        width={774}
+        height={420}
+        onError={() => setDisplayFallback(true)}
+        className={'absolute z-0 h-full origin-top-left object-cover'}
+      />
+      <Image
+        src={fallbackTumbnail}
+        alt={`YouTube Video: ${videoId}`}
+        width={774}
+        height={420}
+        className={
+          displayFallback ? 'absolute z-10 h-full object-cover' : 'hidden'
+        }
+      />
+    </div>
+  )
+}
