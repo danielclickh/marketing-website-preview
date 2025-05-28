0615994527a0dab2874a23999d8da3f5bea8367c
Levi Cole <levi.cole@clickhouse.com>
2025-05-28 16:21:29 +0100

    A different approach to custom carousel navigation

--- src/pages/openhouse/index.tsx
+++ src/pages/openhouse/index.tsx
@@ -48,7 +48,7 @@ import OpenHouseAccordionItem from '@/components/OpenHouseAccordionItem'
 import OpenHouseButton from '@/components/OpenHouseButton'
 import OpenHouseHeader from '@/components/OpenHouseHeader'
 import SeoContainer from '@/components/SeoContainer'
-import { fetchAll, findAll, getStagingOnlyFilters } from '@/lib/api/strapi'
+import { fetchAll, getStagingOnlyFilters } from '@/lib/api/strapi'
 import { convertDateToString } from '@/lib/utils/dateUtils'
 import { getCommonProps } from '@/lib/utils/getCommonProps'
 import { limitStringByWord, stripHtmlTags } from '@/lib/utils/strings'
@@ -59,11 +59,12 @@ import { AnimatePresence, motion } from 'framer-motion'
 import { GetStaticProps } from 'next'
 import Image, { ImageProps } from 'next/image'
 import Link from 'next/link'
-import { useCallback, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { isValidElement, Children } from 'react'
 import 'swiper/css'
 import { Navigation } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import type { Swiper as SwiperClass } from 'swiper/types'
 
 interface OpenHousePageProps extends CommonProps {
   blogs: Array<
@@ -1316,6 +1317,7 @@ function ContentCarousel({
 }) {
   const prevRef = useRef<null | HTMLButtonElement>(null)
   const nextRef = useRef<null | HTMLButtonElement>(null)
+  const swiperRef = useRef<null | SwiperClass>(null)
 
   const modeButtonClasses: Record<'dark' | 'light', string> = {
     dark: 'bg-ch-yellow ring-neutral-900 ring-offset-neutral-900',
@@ -1327,9 +1329,76 @@ function ContentCarousel({
     light: ''
   }
 
+  useEffect(() => {
+    const swiperClass = swiperRef.current
+    const prev = prevRef.current
+    const next = nextRef.current
+    if (
+      swiperClass &&
+      swiperClass.params &&
+      swiperClass.params.navigation &&
+      swiperClass.navigation &&
+      prev &&
+      next
+    ) {
+      // Assign the navigation elements
+      if (swiperClass.params.navigation === true) {
+        swiperClass.params.navigation = {}
+      }
+      swiperClass.params.navigation.prevEl = prevRef.current
+      swiperClass.params.navigation.nextEl = nextRef.current
+
+      // Initialize navigation
+      swiperClass.navigation.destroy()
+      swiperClass.navigation.init()
+      swiperClass.navigation.update()
+    }
+  }, [swiperRef, prevRef, nextRef])
+
   return (
-    <div className='relative flex flex-col'>
-      <div className='pointer-events-none z-10 order-last mt-4 flex w-full items-center justify-center gap-4 lg:absolute lg:left-0 lg:right-0 lg:top-1/2 lg:mt-0 lg:-translate-y-1/2 lg:justify-between'>
+    <div className='relative'>
+      <Swiper
+        modules={[Navigation]}
+        slidesPerView={1}
+        spaceBetween={6}
+        speed={600}
+        watchSlidesProgress={true}
+        allowTouchMove={true}
+        breakpoints={{
+          480: {
+            slidesPerView: 1.25,
+            spaceBetween: 10
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 32
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 32
+          }
+        }}
+        navigation={true}
+        className='!overflow-visible'
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper
+        }}>
+        {Children.map(children, (child, index) => {
+          if (isValidElement(child)) {
+            return (
+              <SwiperSlide key={index} className='!h-auto'>
+                {({ isVisible }) => (
+                  <div
+                    className={`h-full transition-opacity ${isVisible ? '' : 'pointer-events-none opacity-50'}`}>
+                    {child}
+                  </div>
+                )}
+              </SwiperSlide>
+            )
+          }
+        })}
+      </Swiper>
+      <div className='pointer-events-none z-10 mt-4 flex items-center justify-center gap-4 lg:absolute lg:left-0 lg:right-0 lg:top-1/2 lg:mt-0 lg:-translate-y-1/2 lg:justify-between'>
         <button
           ref={prevRef}
           type='button'
@@ -1357,47 +1426,6 @@ function ContentCarousel({
           />
         </button>
       </div>
-      <Swiper
-        modules={[Navigation]}
-        slidesPerView={1}
-        spaceBetween={6}
-        speed={600}
-        watchSlidesProgress={true}
-        allowTouchMove={true}
-        breakpoints={{
-          480: {
-            slidesPerView: 1.25,
-            spaceBetween: 10
-          },
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 32
-          },
-          1024: {
-            slidesPerView: 3,
-            spaceBetween: 32
-          }
-        }}
-        navigation={{
-          prevEl: prevRef.current,
-          nextEl: nextRef.current
-        }}
-        className='order-first w-full !overflow-visible'>
-        {Children.map(children, (child, index) => {
-          if (isValidElement(child)) {
-            return (
-              <SwiperSlide key={index} className='!h-auto'>
-                {({ isVisible }) => (
-                  <div
-                    className={`h-full transition-opacity ${isVisible ? '' : 'pointer-events-none opacity-50'}`}>
-                    {child}
-                  </div>
-                )}
-              </SwiperSlide>
-            )
-          }
-        })}
-      </Swiper>
     </div>
   )
 }
