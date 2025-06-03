e51b011fe2ad5383f844b684a899dee40652ddd1
Levi Cole <levi.cole@clickhouse.com>
2025-06-03 13:04:27 +0100

    Simplified newsletter component markup

--- src/components/NewsLetter/index.tsx
+++ src/components/NewsLetter/index.tsx
@@ -4,19 +4,17 @@ import { NewsLetterData } from '@/types/homepage'
 
 function NewsLetter({ title, description }: NewsLetterData) {
   return (
-    <div className='rounded bg-white/5 p-6'>
-      <div className='flex flex-col justify-between gap-6 md:flex-row md:items-center'>
-        <div className='flex w-full flex-col md:w-1/2'>
-          <SuiTitle type='h3' className='mb-2.5'>
-            {title}
-          </SuiTitle>
-          <SuiText size='sm' weight='medium' color='secondary'>
-            {description}
-          </SuiText>
-        </div>
-        <div className='flex-1'>
-          <NewsLetterForm />
-        </div>
+    <div className='flex flex-col justify-between gap-6 rounded bg-white/5 p-4 md:flex-row md:items-center md:p-6'>
+      <div className='w-full md:w-1/2'>
+        <SuiTitle type='h3' className='mb-2.5'>
+          {title}
+        </SuiTitle>
+        <SuiText size='sm' weight='medium' color='secondary'>
+          {description}
+        </SuiText>
+      </div>
+      <div className='flex-1'>
+        <NewsLetterForm />
       </div>
     </div>
   )
