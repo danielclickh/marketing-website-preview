09e3f0268a0018939619e2f1634c26e63e3575d1
Levi Cole <levi.cole@clickhouse.com>
2025-07-14 11:58:02 +0100

    Reenabled redirect and updated cookie key

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,7 @@ export const config = {
     // - _next/static (excludes static files)
     // - _next/image (excludes image optimization files)
     // - favicon.ico (excludes favicon file)
-    //'/((?!api|_next/static|_next/image|favicon.ico).*)'
+    '/((?!api|_next/static|_next/image|favicon.ico).*)'
   ]
 }
 
@@ -43,22 +43,10 @@ const i18nRedirectionMap: Record<string, Record<string, string>> = {
       '/jp/use-cases/machine-learning-and-data-science',
     '/use-cases/real-time-analytics': '/jp/use-cases/real-time-analytics'
   }
-  /*EN: {
-    '/jp': '/',
-    '/jp/clickhouse': '/clickhouse',
-    '/jp/cloud': '/cloud',
-    '/jp/company/contact': '/company/contact',
-    '/jp/use-cases': '/use-cases',
-    '/jp/use-cases/business-intelligence': '/use-cases/data-warehousing',
-    '/jp/use-cases/logging-and-metrics': '/use-cases/observability',
-    '/jp/use-cases/machine-learning-and-data-science':
-      '/use-cases/machine-learning-and-data-science',
-    '/jp/use-cases/real-time-analytics': '/use-cases/real-time-analytics'
-  }*/
 }
 
 export function middleware(request: NextRequest) {
-  const cookieKey = 'user-country-code-v2'
+  const cookieKey = 'user-country-code-v3'
 
   // Get the country code from the request's geo data (ISO 3166-1 alpha-2 format)
   // Note: geo data is only available on Vercel deployment; defaults to 'unknown' otherwise
