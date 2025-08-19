e882ad0f1eba69218cb879c3f46cab1e72c936d4
Levi Cole <levi.cole@clickhouse.com>
2025-08-19 12:52:07 +0100

    New layout and cms endpoint

--- src/pages/company/news/index.tsx
+++ src/pages/company/news/index.tsx
@@ -1,35 +1,74 @@
+import { CUICard } from '@/components/ClickUI'
 import FollowUs from '@/components/FollowUs'
 import Layout from '@/components/Layout'
 import NewsItem from '@/components/NewsItem'
-import Pagination from '@/components/Pagination'
-import { SuiTitle } from '@/components/sui'
-import { findOne } from '@/lib/api/strapi'
+import { StrapiImageUrl } from '@/components/StrapiElements'
+import { SuiText, SuiTitle } from '@/components/sui'
+import { fetchAll, findOne } from '@/lib/api/strapi'
+import { StrapiImageType } from '@/lib/api/strapi/types'
 import { useGalaxyOnPage } from '@/lib/galaxy/galaxy'
+import { convertDateToString } from '@/lib/utils/dateUtils'
 import { getCommonProps } from '@/lib/utils/getCommonProps'
-import { NewsAndEventsData, NewsPageProps } from '@/types/newsEvents'
+import { CommonProps } from '@/types/homepage'
 import { GetStaticProps } from 'next'
-import { useMemo, useState } from 'react'
+import Link from 'next/link'
+import React from 'react'
 
-export const getStaticProps: GetStaticProps<NewsPageProps> =
+interface StrapiNewsItem {
+  title: string
+  slug: string
+  redirectUrl: string
+  publisherName: string
+  publisherAvatar: StrapiImageType
+  type: 'News' | 'Press release'
+  publishedDate: string
+  excerpt: null | string
+}
+
+interface PageProps extends CommonProps {
+  newsItems: Array<StrapiNewsItem>
+  pressItems: Array<StrapiNewsItem>
+}
+
+export const getStaticProps: GetStaticProps<PageProps> =
   async function getStaticProps() {
-    const newsEvents: Promise<NewsAndEventsData> = findOne('news-and-event', {
-      populate: [
-        'newsItems',
-        'newsItems.ctaButton',
-        'pressReleases',
-        'pressReleases.ctaButton'
-      ]
+    const newsPromise: Promise<Array<StrapiNewsItem>> = fetchAll('news-items', {
+      filters: {
+        type: {
+          $eq: 'News'
+        }
+      },
+      populate: ['publisherAvatar'],
+      sort: ['publishedDate:desc']
     })
 
-    const { newsItems, pressReleases } = await newsEvents
+    const pressPromise: Promise<Array<StrapiNewsItem>> = fetchAll(
+      'news-items',
+      {
+        filters: {
+          type: {
+            $eq: 'Press release'
+          }
+        },
+        populate: ['publisherAvatar'],
+        sort: ['publishedDate:desc']
+      }
+    )
 
-    const commonProps = await getCommonProps()
+    const commonPromise = getCommonProps()
+
+    const [newsItems, pressItems, commonProps] = await Promise.all([
+      newsPromise,
+      pressPromise,
+      commonPromise
+    ])
+
     return {
       props: {
         newsItems,
-        pressReleases,
+        pressItems,
         seo: {
-          title: 'News - ClickHouse',
+          title: 'News & Press releases - ClickHouse',
           path: '/company/news'
         },
         ...commonProps
@@ -39,50 +78,88 @@ export const getStaticProps: GetStaticProps<NewsPageProps> =
 
 export default function News({
   newsItems,
-  pressReleases,
+  pressItems,
   footerData,
   headerData,
   seo
-}: NewsPageProps) {
+}: PageProps) {
   useGalaxyOnPage('newsPage')
 
   return (
     <Layout footerData={footerData} seo={seo} headerData={headerData}>
-      <section className='section-container my-10 lg:my-16'>
-        <SuiTitle type='h1' className='mb-10 text-center lg:mb-16'>
-          News
-        </SuiTitle>
-        <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
-          {newsItems.map((newsItem, newsIndex) => (
-            <NewsItem
-              key={newsIndex}
-              source={newsItem.publication}
-              date={newsItem.date}
-              title={newsItem.headline}
-              abstract={newsItem.shortIntro}
-              ctaButton={newsItem.ctaButton}
-            />
-          ))}
+      <div className='section-container my-16 flex flex-col items-start gap-x-16 gap-y-16 lg:my-24 lg:flex-row'>
+        {/* Main */}
+        <div>
+          <SuiTitle type='h1'>Press releases</SuiTitle>
+          <ul className='space-y-6 divide-y divide-white/5'>
+            {pressItems.map((item, itemIndex) => (
+              <li key={itemIndex} className='pt-6'>
+                <div className='group/newsItem relative'>
+                  <p className='mb-2 font-inconsolata text-primary-300'>
+                    {item.publisherName} •{' '}
+                    {convertDateToString(item.publishedDate)}
+                  </p>
+                  <SuiTitle type='h3'>
+                    <Link href={item.redirectUrl}>
+                      <span className='absolute inset-0' />
+                      {item.title}
+                    </Link>
+                  </SuiTitle>
+                  <div className='flex flex-col'>
+                    <SuiText
+                      className='mb-6 leading-relaxed text-neutral-200'
+                      size='sm'>
+                      {item.excerpt}
+                    </SuiText>
+                    <div className='mt-auto text-primary-300 group-hover/newsItem:underline'>
+                      Read more -&gt;
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
-      </section>
 
-      <section className='section-container my-10 lg:my-16'>
-        <SuiTitle type='h2' className='mb-10 text-center lg:mb-16'>
-          Press releases
-        </SuiTitle>
-        <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
-          {pressReleases.map((pressRelease, pressReleaseIndex) => (
-            <NewsItem
-              key={pressReleaseIndex}
-              source={pressRelease.publication}
-              date={pressRelease.date}
-              title={pressRelease.headline}
-              abstract={pressRelease.shortIntro}
-              ctaButton={pressRelease.ctaButton}
-            />
-          ))}
+        {/* Sidebar */}
+        <div className='w-full lg:max-w-md'>
+          <CUICard>
+            <CUICard.Body className='p-4 lg:p-6'>
+              <SuiTitle type='h2'>In the news</SuiTitle>
+              <ul className='space-y-6 divide-y divide-white/5'>
+                {newsItems.map((item, itemIndex) => (
+                  <li key={itemIndex} className='pt-6'>
+                    <div className='group/newsItem relative'>
+                      <SuiTitle type='h3'>
+                        <Link
+                          href={item.redirectUrl}
+                          className='text-primary-300 hover:underline'>
+                          <span className='absolute inset-0' />
+                          {item.title}
+                        </Link>
+                      </SuiTitle>
+                      <div className='mt-4 flex items-center gap-4'>
+                        <StrapiImageUrl
+                          {...item.publisherAvatar}
+                          width={100}
+                          height={100}
+                          unoptimized={false}
+                          className='size-11 rounded-full object-cover object-center'
+                        />
+                        <p className='font-inconsolata leading-tight text-neutral-200'>
+                          {item.publisherName}
+                          <br />
+                          {convertDateToString(item.publishedDate)}
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </CUICard.Body>
+          </CUICard>
         </div>
-      </section>
+      </div>
 
       <FollowUs />
     </Layout>
