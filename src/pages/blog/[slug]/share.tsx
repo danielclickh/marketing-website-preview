a476fc6ecd0d5b0b0116c88336a96be475670119
Levi Cole <levi.cole@clickhouse.com>
2025-08-21 18:22:28 +0100

    Removed debug text

--- src/pages/blog/[slug]/share.tsx
+++ src/pages/blog/[slug]/share.tsx
@@ -57,7 +57,7 @@ export const getServerSideProps = (async ({ req, params }) => {
   return {
     props: {
       seo: {
-        title: `Sharing: ${blog.title}`,
+        title: blog.title,
         description: blog.shortDescription,
         type: 'article',
         siteName: 'ClickHouse',
