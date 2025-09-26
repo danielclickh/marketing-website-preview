adc8e05615fe296426660d5358db6484745d8d6f
Levi Cole <levi.cole@clickhouse.com>
2025-09-26 11:22:27 +0100

    Created new LogoStack component for reusable clickstack effect

---
+++ src/components-cleaned/LogoStack/index.tsx
@@ -0,0 +1,94 @@
+import styles from './styles.module.scss'
+import ScaleToContainer from '@/components/ScaleToContainer'
+import Image, { ImageProps } from 'next/image'
+
+const spacingFixPercentage = 13
+
+type Logo = { src: ImageProps['src'] } & Partial<Omit<ImageProps, 'src'>>
+
+type Layer = {
+  logo: Logo
+  color?: React.CSSProperties['backgroundColor']
+  active?: boolean
+  onClick?: () => void
+}
+
+export interface LogoStackProps {
+  width?: number
+  gap?: number
+  layers: Array<Layer>
+}
+
+export default function LogoStack({
+  width = 261,
+  gap = 64,
+  layers
+}: LogoStackProps) {
+  const layerWidth = ((100 - spacingFixPercentage * 2) / 100) * width
+  return (
+    <ScaleToContainer scaleUp={false}>
+      <div
+        className='relative select-none'
+        style={{
+          width: width,
+          aspectRatio: `${width}/${layerWidth + gap * (layers.length - 1) + 4}`
+        }}>
+        {layers.map((layer, layerIndex) => {
+          return (
+            <Layer
+              key={layerIndex}
+              logo={layer.logo}
+              color={layer.color}
+              active={layer.active}
+              onClick={layer.onClick}
+              style={{
+                zIndex: layers.length - layerIndex,
+                top: `${(gap * layerIndex).toFixed(2)}px`
+              }}
+            />
+          )
+        })}
+      </div>
+    </ScaleToContainer>
+  )
+}
+
+function Layer({
+  logo: { src, width, height, alt = '', className = '', ...logo },
+  color = '#fff',
+  style = {},
+  active = true,
+  onClick
+}: {
+  logo: Logo
+  color?: string
+  style?: Omit<React.CSSProperties, '--color' | 'left' | 'right'>
+  active?: boolean
+  onClick?: () => void
+}) {
+  return (
+    <div
+      className={`pointer-events-none absolute top-0 aspect-square ${styles.layerPerspective}`}
+      style={
+        {
+          ...style,
+          '--color': color,
+          left: `${spacingFixPercentage}%`,
+          right: `${spacingFixPercentage}%`
+        } as React.CSSProperties
+      }>
+      <div
+        onClick={onClick}
+        className={`flex items-center justify-center overflow-hidden rounded-[8%] border-2 bg-gradient-to-br from-[rgba(38,38,35,0.9)] to-[rgba(16,16,16,0.9)] backdrop-blur-sm transition duration-300 ${onClick ? 'pointer-events-auto cursor-pointer' : ''} ${active ? '' : `opacity-15 ${onClick ? 'hover:opacity-40' : ''}`} ${styles.layer}`}>
+        <Image
+          src={src}
+          width={width ?? 180}
+          height={height ?? 180}
+          alt={alt}
+          className={`h-full w-full max-w-none ${className}`}
+          {...logo}
+        />
+      </div>
+    </div>
+  )
+}
--- src/components/ClickStack/index.tsx
+++ src/components/ClickStack/index.tsx
@@ -1,10 +1,7 @@
 import logoClickhouse from './assets/clickhouse.svg'
 import logoHyperdx from './assets/hyperdx.svg'
 import logoOpentelemetry from './assets/opentelemetry.svg'
-import styles from './styles.module.scss'
-import { StaticImport } from 'next/dist/shared/lib/get-img-props'
-import Image from 'next/image'
-import React, { useCallback } from 'react'
+import LogoStack from '@/components-cleaned/LogoStack'
 
 type CallbackStack = 'hyperdx' | 'clickhouse' | 'opentelemetry'
 
@@ -21,75 +18,28 @@ export default function ClickStack({
   opentelemetry = true,
   onClick
 }: ClickStackProps) {
-  const clickHandler = useCallback(
-    (stack: CallbackStack) => {
-      if (onClick) {
-        onClick(stack)
-      }
-    },
-    [onClick]
-  )
   return (
-    <div className='aspect-[14/18] w-[245px] px-6'>
-      <div className='relative aspect-square'>
-        <Layer
-          logo={logoHyperdx}
-          color='#4FFA7A'
-          active={hyperdx}
-          className='z-30'
-          onClick={() => clickHandler('hyperdx')}
-        />
-        <Layer
-          logo={logoClickhouse}
-          color='#FAFF69'
-          active={clickhouse}
-          className='z-20 translate-y-1/3'
-          onClick={() => clickHandler('clickhouse')}
-        />
-        <Layer
-          logo={logoOpentelemetry}
-          color='#F5A800'
-          active={opentelemetry}
-          className='z-10 translate-y-2/3'
-          onClick={() => clickHandler('opentelemetry')}
-        />
-      </div>
-    </div>
-  )
-}
-
-function Layer({
-  logo,
-  color,
-  className,
-  active = true,
-  onClick
-}: {
-  logo: string | StaticImport
-  color: string
-  className?: string
-  active?: boolean
-  onClick?: () => void
-}) {
-  return (
-    <div
-      className={`pointer-events-none absolute inset-0 ${styles.layerPerspective} ${className}`}
-      style={
+    <LogoStack
+      layers={[
         {
-          '--color': color
-        } as React.CSSProperties
-      }>
-      <div
-        onClick={onClick}
-        className={`flex items-center justify-center overflow-hidden rounded-[8%] border-2 bg-gradient-to-br from-[rgba(38,38,35,0.9)] to-[rgba(16,16,16,0.9)] backdrop-blur-sm transition duration-300 ${onClick ? 'pointer-events-auto cursor-pointer' : ''} ${active ? '' : `opacity-15 ${onClick ? 'hover:opacity-40' : ''}`} ${styles.layer}`}>
-        <Image
-          src={logo}
-          width={180}
-          height={180}
-          alt=''
-          className='w-full max-w-none'
-        />
-      </div>
-    </div>
+          logo: { src: logoHyperdx },
+          active: hyperdx,
+          color: '#4FFA7A',
+          onClick: onClick ? () => onClick('hyperdx') : undefined
+        },
+        {
+          logo: { src: logoClickhouse },
+          active: clickhouse,
+          color: '#FAFF69',
+          onClick: onClick ? () => onClick('clickhouse') : undefined
+        },
+        {
+          logo: { src: logoOpentelemetry },
+          active: opentelemetry,
+          color: '#F5A800',
+          onClick: onClick ? () => onClick('opentelemetry') : undefined
+        }
+      ]}
+    />
   )
 }
