b22dd6bfb32b43b295e57ec94166c3c1b4f29d40
Levi Cole <levi.cole@clickhouse.com>
2025-10-24 22:29:38 +0100

    Fixed blog content overflow width

--- src/pages/blog/[slug]/index.tsx
+++ src/pages/blog/[slug]/index.tsx
@@ -296,7 +296,7 @@ export default function BlogPage({
               )}
 
               {content && (
-                <div className='flex flex-col lg:flex-row' ref={contentRef}>
+                <div className='w-full' ref={contentRef}>
                   <Markdown
                     allowDirectives={true}
                     className='rich-text-content leading-6'
