67796e96b1cd9648a7c3905b9e4a581436f2d269
Levi Cole <levi.cole@clickhouse.com>
2025-10-24 22:35:04 +0100

    Mobile overflow fix

--- src/pages/blog/[slug]/index.tsx
+++ src/pages/blog/[slug]/index.tsx
@@ -253,7 +253,7 @@ export default function BlogPage({
             <ArrowLeftIcon className='mr-2 w-4 transition-transform group-hover/backButton:-translate-x-1' />
             Back
           </SmartBackButton>
-          <div className='flex flex-col gap-y-8 lg:grid lg:grid-cols-12 lg:gap-x-6'>
+          <div className='flex w-full flex-col gap-y-8 lg:grid lg:grid-cols-12 lg:gap-x-6'>
             {/* Blog meta */}
             <div className='order-1 lg:order-none lg:col-span-11 lg:mb-12 xl:col-span-9'>
               <Breadcrumbs>
