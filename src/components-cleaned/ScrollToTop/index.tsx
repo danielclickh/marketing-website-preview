21988fa46b85b54777b1a9d3cd32862ce4a25a50
Levi Cole <levi.cole@clickhouse.com>
2025-10-28 10:45:10 +0000

    Fixed issue with content behind scroll top not being clickable

--- src/components-cleaned/ScrollToTop/index.tsx
+++ src/components-cleaned/ScrollToTop/index.tsx
@@ -33,11 +33,11 @@ export default function ScrollToTop({ showFrom, hideAt }: ScrollToTopProps) {
 
   return (
     <div
-      className={`fixed bottom-0 left-1/2 z-50 -translate-x-1/2 pb-4 transition duration-500 ${show ? '' : 'pointer-events-none translate-y-full opacity-0'}`}>
+      className={`pointer-events-none fixed bottom-0 left-1/2 z-50 -translate-x-1/2 pb-4 transition duration-500 ${show ? '' : 'translate-y-full opacity-0'}`}>
       <span className='absolute -inset-x-20 -inset-y-10 rounded-full bg-black/40 blur-3xl' />
       <button
         onClick={clickHandler}
-        className='flex gap-2 rounded-full border border-neutral-700/90 bg-neutral-950/90 px-3 py-1 text-sm text-neutral-200 shadow-lg backdrop-blur transition-colors hover:bg-neutral-900/90'>
+        className={`flex gap-2 rounded-full border border-neutral-700/90 bg-neutral-950/90 px-3 py-1 text-sm text-neutral-200 shadow-lg backdrop-blur transition-colors hover:bg-neutral-900/90 ${show ? 'pointer-events-auto' : 'pointer-events-none'}`}>
         <span className='-rotate-90'>-&gt;</span>
         Scroll to top
       </button>
