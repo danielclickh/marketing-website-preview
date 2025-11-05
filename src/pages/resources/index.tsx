2146b762cb2d085e5a6b8b48a28e1c6819b31abd
Levi Cole <levi.cole@clickhouse.com>
2025-11-05 17:02:58 +0000

    Initial progress on client side resource searching

--- src/pages/resources/index.tsx
+++ src/pages/resources/index.tsx
@@ -1,5 +1,6 @@
 import Breadcrumbs from '@/components-cleaned/Breadcrumbs'
 import PillFilters from '@/components-cleaned/PillFilters'
+import ResourcesArchive from '@/components-cleaned/ResourcesArchive'
 import Layout from '@/components/Layout'
 import { SuiSearchField, SuiTitle } from '@/components/sui'
 import {
@@ -47,66 +48,7 @@ export default function RsourcesPage({
             Resource Hub
           </SuiTitle>
 
-          <div className='flex items-center gap-6'>
-            <SuiSearchField
-              placeholder='Search by title or keyword...'
-              htmlFor='search'
-              className='max-w-[300px]'
-              value={''}
-              onChange={console.log}
-            />
-            {categories.length > 1 && (
-              <nav>
-                <PillFilters
-                  options={[
-                    {
-                      kind: 'link',
-                      label: 'View all',
-                      href: '/resources',
-                      active: true
-                    },
-                    ...categories.map((category) => ({
-                      kind: 'link' as const,
-                      label: category.name,
-                      href: `/resources/${category.slug}`
-                    }))
-                  ]}
-                />
-              </nav>
-            )}
-          </div>
-
-          <hr className='my-6 h-px border-0 bg-white/20' />
-
-          <ul className='space-y-10'>
-            {resources.map((resource, resourceIndex) => {
-              return (
-                <li className='relative space-y-4' key={resourceIndex}>
-                  <SuiTitle type='h2' className='!text-xl'>
-                    <Link
-                      href={`/resources/${resource.category.slug}/${resource.slug}`}
-                      className='text-primary-300 hover:underline'>
-                      <span className='absolute inset-0' />
-                      {resource.title}
-                    </Link>
-                  </SuiTitle>
-                  <p className='mt-2 text-neutral-200'>{resource.excerpt}</p>
-                  {(resource.author || resource.date) && (
-                    <p className='text-sm text-neutral-200'>
-                      {[
-                        resource.author?.name,
-                        resource?.date
-                          ? convertDateToString(resource.date)
-                          : null
-                      ]
-                        .filter(Boolean)
-                        .join(' • ')}
-                    </p>
-                  )}
-                </li>
-              )
-            })}
-          </ul>
+          <ResourcesArchive resources={resources} categories={categories} />
         </div>
       </div>
     </Layout>
