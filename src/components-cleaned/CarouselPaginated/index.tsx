d46ba4a0ca0941a6c4ca53a81aef5b7f704067a2
Levi Cole <levi.cole@clickhouse.com>
2025-11-17 10:31:44 +0000

    Added ref forwarding to paginated carousel component

--- src/components-cleaned/CarouselPaginated/index.tsx
+++ src/components-cleaned/CarouselPaginated/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import styles from './styles.module.scss'
-import { Children, useRef } from 'react'
+import { Children, useRef, forwardRef } from 'react'
 import 'swiper/css'
 import { Navigation, Pagination } from 'swiper/modules'
 import { Swiper, SwiperProps, SwiperSlide } from 'swiper/react'
@@ -13,79 +13,86 @@ export interface CarouselPaginatedProps
   theme?: 'light' | 'dark'
 }
 
-export default function CarouselPaginated({
-  children,
-  className = '',
-  carouselClass,
-  theme = 'light',
-  ...swiperOptions
-}: CarouselPaginatedProps) {
-  const prevRef = useRef<null | HTMLButtonElement>(null)
-  const nextRef = useRef<null | HTMLButtonElement>(null)
-  const paginationRef = useRef<null | HTMLDivElement>(null)
-
-  const mergedOptions: SwiperProps = {
-    // User options
-    ...swiperOptions,
-
-    // Defaults
-    modules: [Pagination, Navigation, ...(swiperOptions.modules || [])],
-    pagination: {
-      enabled: true,
-      clickable: true,
-      dynamicBullets: true,
-      el: paginationRef.current
+const CarouselPaginated = forwardRef<HTMLDivElement, CarouselPaginatedProps>(
+  (
+    {
+      children,
+      className = '',
+      carouselClass,
+      theme = 'light',
+      ...swiperOptions
     },
-    navigation: {
-      enabled: true,
-      prevEl: prevRef.current,
-      nextEl: nextRef.current
-    },
-    className: carouselClass
-  }
+    ref
+  ) => {
+    const prevRef = useRef<null | HTMLButtonElement>(null)
+    const nextRef = useRef<null | HTMLButtonElement>(null)
+    const paginationRef = useRef<null | HTMLDivElement>(null)
 
-  return (
-    <div className={`${styles.base} ${styles[theme]} ${className}`}>
-      <Swiper {...mergedOptions}>
-        {Children.map(children, (child) => {
-          return <SwiperSlide>{child}</SwiperSlide>
-        })}
-      </Swiper>
-      <div className='swiper-controls'>
-        <button ref={prevRef} type='button' className='swiper-button-prev'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='7'
-            height='12'
-            fill='none'
-            viewBox='0 0 7 12'>
-            <path
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='M5.75 10.5 1 5.75 5.75 1'
-            />
-          </svg>
-        </button>
-        <div ref={paginationRef} className='swiper-pagination' />
-        <button ref={nextRef} type='button' className='swiper-button-next'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='7'
-            height='12'
-            fill='none'
-            viewBox='0 0 7 12'>
-            <path
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='m1 10.5 4.75-4.75L1 1'
-            />
-          </svg>
-        </button>
+    const mergedOptions: SwiperProps = {
+      // User options
+      ...swiperOptions,
+
+      // Our strict options override user defined
+      modules: [Pagination, Navigation, ...(swiperOptions.modules || [])],
+      pagination: {
+        enabled: true,
+        clickable: true,
+        dynamicBullets: true,
+        el: paginationRef.current
+      },
+      navigation: {
+        enabled: true,
+        prevEl: prevRef.current,
+        nextEl: nextRef.current
+      },
+      className: carouselClass
+    }
+
+    return (
+      <div ref={ref} className={`${styles.base} ${styles[theme]} ${className}`}>
+        <Swiper {...mergedOptions}>
+          {Children.map(children, (child) => {
+            return <SwiperSlide>{child}</SwiperSlide>
+          })}
+        </Swiper>
+        <div className='swiper-controls'>
+          <button ref={prevRef} type='button' className='swiper-button-prev'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='7'
+              height='12'
+              fill='none'
+              viewBox='0 0 7 12'>
+              <path
+                stroke='currentColor'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M5.75 10.5 1 5.75 5.75 1'
+              />
+            </svg>
+          </button>
+          <div ref={paginationRef} className='swiper-pagination' />
+          <button ref={nextRef} type='button' className='swiper-button-next'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='7'
+              height='12'
+              fill='none'
+              viewBox='0 0 7 12'>
+              <path
+                stroke='currentColor'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='m1 10.5 4.75-4.75L1 1'
+              />
+            </svg>
+          </button>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
+
+export default CarouselPaginated
