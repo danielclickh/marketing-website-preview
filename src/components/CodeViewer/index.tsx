fbc20c0e9d05c8b177733b2656a77df2540e396e
Levi Cole <levi.cole@clickhouse.com>
2025-11-25 12:10:08 +0000

    Initial work cleaning up css

--- src/components/CodeViewer/index.tsx
+++ src/components/CodeViewer/index.tsx
@@ -1,5 +1,6 @@
 import CodeInterpreter from './CodeInterpreter'
 import { DefaultView } from './CodeResults'
+import './highlightjs.scss'
 import { ChartConfig, ChartType } from './types'
 import { base64Decode } from './utils'
 import { CodeBlock } from '@clickhouse/click-ui'
