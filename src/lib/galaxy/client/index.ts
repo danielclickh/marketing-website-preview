d072ac78b1fc7a41dfc1b0c8ee8a427c2983314b
Levi Cole <levi.cole@clickhouse.com>
2025-12-15 09:53:32 +0000

    Restored private methods since they are no longer needed

--- src/lib/galaxy/client/index.ts
+++ src/lib/galaxy/client/index.ts
@@ -82,9 +82,9 @@ export class GalaxyClient {
   private errorHandler?: ErrorHandler
   private application: string
   private apiHost: string
-  getContext: () => Record<string, unknown>
-  getUserId: () => string
-  getSessionId: () => string
+  private getContext: () => Record<string, unknown>
+  private getUserId: () => string
+  private getSessionId: () => string
 
   constructor({
     httpClient,
