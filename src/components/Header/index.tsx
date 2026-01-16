92fc214dca4aa99a9132af5bc740850077b204a8
Levi Cole <levi.cole@clickhouse.com>
2026-01-16 13:37:32 +0000

    Re-added postgres global announcement

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -23,15 +23,15 @@ export default function Header({ github, eyebrow }: HeaderProps) {
   const [isScrolled, setIsScrolled] = useState<boolean>(false)
 
   // Eyebrow default settings
-  const [headerBannerEnabled, setHeaderBannerEnabled] = useState(false)
+  const [headerBannerEnabled, setHeaderBannerEnabled] = useState(true)
   const [headerBannerArrow, setHeaderBannerArrow] = useState(true)
   const [headerBannerText, setHeaderBannerText] = useState<
     string | React.ReactNode
   >(
-    'ClickHouse raises Series D and Enters LLM Observability with Langfuse Acquisition. More here'
+    'Postgres managed by ClickHouse is in private preview. One stack for transactions and analytics'
   )
   const [headerBannerUrl, setHeaderBannerUrl] = useState(
-    '/blog/clickhouse-raises-400-million-series-d-acquires-langfuse-launches-postgres?loc=banner'
+    '/cloud/postgres?loc=banner'
   )
   const [headerBannerExpires, setHeaderBannerExpires] = useState<
     undefined | Date
