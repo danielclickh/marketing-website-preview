30ebbc2e2e468afd2d670385a4b9c123cf36666c
Levi Cole <levi.cole@clickhouse.com>
2026-01-16 14:34:02 +0000

    Series D announcement banner

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -28,10 +28,10 @@ export default function Header({ github, eyebrow }: HeaderProps) {
   const [headerBannerText, setHeaderBannerText] = useState<
     string | React.ReactNode
   >(
-    'Postgres managed by ClickHouse is in private preview. One stack for transactions and analytics'
+    'ClickHouse raises Series D and Enters LLM Observability with Langfuse Acquisition. More here'
   )
   const [headerBannerUrl, setHeaderBannerUrl] = useState(
-    '/cloud/postgres?loc=banner'
+    '/blog/clickhouse-raises-400-million-series-d-acquires-langfuse-launches-postgres?loc=banner'
   )
   const [headerBannerExpires, setHeaderBannerExpires] = useState<
     undefined | Date
