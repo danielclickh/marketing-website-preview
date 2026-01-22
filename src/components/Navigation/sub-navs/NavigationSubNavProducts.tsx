c7234238ca30d75040b160c84abab3d5a02e52fc
Gareth Jones <github@garethdjones.com>
2026-01-22 15:13:15 -0800

    Adding postgres to the menu

--- src/components/Navigation/sub-navs/NavigationSubNavProducts.tsx
+++ src/components/Navigation/sub-navs/NavigationSubNavProducts.tsx
@@ -59,29 +59,28 @@ export default function NavigationSubNavProducts() {
                 </NavigationLink>
               </li>
               
-              {/*<li className='flex items-center'>
+              <li className='flex items-center'>
                 <NavigationLink
                   href='#'
                   onClick={useGalaxyOnClick(
-                    'topNav.productMenu.clickstackCloudSelect'
+                    'topNav.productMenu.postgresSelect'
                   )}
                   className='group/nav-with-icon !flex w-full !flex-nowrap items-center gap-3'>
                   <Image
-                    src='/images/nav/icon-clickstack-cloud.svg'
-                    alt='ClickStack Cloud'
+                    src='/images/nav/icon-postgres.svg'
+                    alt='Postgres ClickHouse Cloud'
                     width={24}
                     height={24}
                     className='flex-grow-1 size-6 flex-shrink-0 object-scale-down'
                   />
                   <span>
-                    ClickStack Cloud
+                    Managed Postgres
                     <div className='text-xs text-slate-300 transition-colors group-hover/nav-with-icon:text-white'>
-                      Unified observability UI for logs,
-                      <br /> metrics, and traces
+                    Fast, scalable, enterprise-grade Postgres, <br />natively integrated with ClickHouse.
                     </div>
                   </span>
                 </NavigationLink>
-              </li>*/}
+              </li>
             </ul>
           </div>
         </div>
