621aa6a4603c638c8f5ab08b43925d4b186ba527
Levi Cole <levi.cole@clickhouse.com>
2026-01-26 10:06:28 +0000

    Fixed nav errors with changed link props

--- src/components/Navigation/parts/NavigationLink.tsx
+++ src/components/Navigation/parts/NavigationLink.tsx
@@ -2,10 +2,8 @@ import LinkWithArrow from '@/components/LinkWithArrow'
 import Link, { LinkProps } from 'next/link'
 import React, { forwardRef } from 'react'
 
-export interface NavigationLinkProps extends Omit<LinkProps, 'href'> {
-  href?: string // Restrict href to be a string
-  className?: string
-  children: React.ReactNode
+export interface NavigationLinkProps
+  extends Omit<LinkProps & React.HTMLProps<HTMLAnchorElement>, 'ref'> {
   isHovered?: boolean
   target?: string
   arrow?: boolean
@@ -30,7 +28,7 @@ const NavigationLink = forwardRef<HTMLAnchorElement, NavigationLinkProps>(
     return (
       <LinkComponent
         ref={ref}
-        href={href || '#'} // Ensure href is a string
+        href={href}
         prefetch={prefetch}
         target={target}
         {...props}
--- src/components/jp/Navigation/parts/index.ts
+++ src/components/jp/Navigation/parts/index.ts
@@ -1,15 +1,12 @@
 import NavigationChevron, { NavigationChevronProps } from './NavigationChevron'
 import NavigationItem, { NavigationItemProps } from './NavigationItem'
 import NavigationLink, { NavigationLinkProps } from './NavigationLink'
-import NavigationQuote, { NavigationQuoteProps } from './NavigationQuote'
 
 export {
   NavigationChevron,
   NavigationItem,
   NavigationLink,
-  NavigationQuote,
   type NavigationChevronProps,
   type NavigationItemProps,
-  type NavigationLinkProps,
-  type NavigationQuoteProps
+  type NavigationLinkProps
 }
